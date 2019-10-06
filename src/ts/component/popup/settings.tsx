e3d4671d4fb03b80ecddf3cf89938e896474f207
Razor <andrew.simachev@gmail.com>
2019-10-06 12:26:04 +0300

    settings wallpaper section

--- src/ts/component/popup/settings.tsx
+++ src/ts/component/popup/settings.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Icon, IconUser, Button, Title, Label } from 'ts/component';
+import { Icon, IconUser, Button, Title, Label, Cover } from 'ts/component';
 import { I, Storage } from 'ts/lib';
 import { observer, inject } from 'mobx-react';
 
@@ -19,13 +19,15 @@ interface State {
 class PopupSettings extends React.Component<Props, {}> {
 
 	state = {
-		page: ''
+		page: 'index'
 	};
 	
 	constructor (props: any) {
 		super(props);
 		
 		this.onClose = this.onClose.bind(this);
+		this.onPage = this.onPage.bind(this);
+		this.onCover = this.onCover.bind(this);
 		this.onLogout = this.onLogout.bind(this);
 	};
 	
@@ -37,42 +39,81 @@ class PopupSettings extends React.Component<Props, {}> {
 		let content = null;
 		
 		switch (page) {
+			
 			default:
+			case 'index':
 				content = (
-					<div className="index">
+					<div>
 						<Icon className="close" onClick={this.onClose} />
 						<Title text="Settings" />
+						
 						<div className="rows">
-							<div className="row">
+							<div className="row" onClick={() => { this.onPage('wallpaper'); }}>
 								<Icon className="wallpaper" />
 								<Label text="Wallpaper" />
 								<Icon className="arrow" />
 							</div>
-							<div className="row">
+							<div className="row" onClick={() => { this.onPage('phrase'); }}>
 								<Icon className="phrase" />
 								<Label text="Keychain phrase" />
 								<Icon className="arrow" />
 							</div>
-							<div className="row">
+							<div className="row" onClick={() => { this.onPage('pin'); }}>
 								<Icon className="pin" />
 								<Label text="Pin code" />
 								<Icon className="arrow" />
 							</div>
-							<div className="row">
+							<div className="row" onClick={() => { this.onPage('notify'); }}>
 								<Icon className="notify" />
 								<Label text="Notifications" />
 								<div className="switches">
 								</div>
 							</div>
 						</div>
+						
 						<div className="logout" onClick={this.onLogout}>Log out</div>
 					</div>
 				);
 				break;
+				
+			case 'wallpaper':
+				let covers = [];
+				for (let i = 1; i <= 7; ++i) {
+					covers.push({ id: i });
+				};
+				
+				const Item = (item: any) => (
+					<div className="item" onClick={() => { this.onCover(item.id); }}>
+						<Cover num={item.id} />
+					</div>
+				);
+				
+				content = (
+					<div>
+						<Icon className="back" onClick={() => { this.onPage('index'); }} />
+						<Title text="Wallpaper" />
+						
+						<div className="covers">
+							{covers.map((item: any, i: number) => (
+								<Item key={i} {...item} />
+							))}
+						</div>
+					</div>
+				);
+				break;
+				
+			case 'phrase':
+				break;
+				
+			case 'pin':
+				break;
+				
+			case 'notify':
+				break;
 		};
 		
 		return (
-			<div>
+			<div className={page}>
 				{content}
 			</div>
 		);
@@ -80,8 +121,16 @@ class PopupSettings extends React.Component<Props, {}> {
 	
 	onClose () {
 		const { commonStore, id } = this.props;
-		
 		commonStore.popupClose(id);
+	};
+	
+	onPage (id: string) {
+		this.setState({ page: id });
+	};
+	
+	onCover (id: number) {
+		const { commonStore } = this.props;
+		commonStore.coverSet(id);
 	};
 	
 	onLogout (e: any) {
