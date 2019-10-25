294e73d8468dba50636a70de70ae286a71896ef3
Razor <andrew.simachev@gmail.com>
2019-10-25 13:09:07 +0300

    gallery view

--- src/ts/component/block/dataview/view/gallery.tsx
+++ src/ts/component/block/dataview/view/gallery.tsx
@@ -10,8 +10,25 @@ class ViewGallery extends React.Component<Props, {}> {
 		const { header, content } = this.props;
 		const { data, properties } = content;
 		
+		const Cell = (item: any) => (
+			<div className={'cell c' + item.property.type}>
+				{item.data}
+			</div>
+		);
+		
+		const Card = (item: any) => (
+			<div className="card">
+				{properties.map((property: any, i: number) => (
+					<Cell key={property.id} property={...property} data={data[item.index][property.id]} />
+				))}
+			</div>
+		);
+		
 		return (
 			<div className="view viewGallery">
+				{data.map((item: any, i: number) => (
+					<Card key={i} index={i} {...item} />
+				))}
 			</div>
 		);
 	};
