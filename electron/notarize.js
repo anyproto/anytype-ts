8576937644bc6b964512c5c4f64fd7f1e7afb94e
requilence <requilence@gmail.com>
2020-01-22 15:16:38 +0200

    fix max os sign and notarize & change app schema

---
+++ electron/notarize.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+	if (process.env.ELECTRON_SKIP_NOTARIZE) {
+		return;
+	}
+	
+	const { electronPlatformName, appOutDir } = context;
+	if (electronPlatformName !== 'darwin') {
+		return;
+	}
+	
+	const appName = context.packager.appInfo.productFilename;
+	
+	return await notarize({
+		appBundleId: 'com.anytype.anytype2',
+		appPath: `${appOutDir}/${appName}.app`,
+		appleId: process.env.APPLEID,
+		appleIdPassword: process.env.APPLEIDPASS,
+	});
+};
