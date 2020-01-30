fdc0e4cb0dbf4d12323a7006fc9b23f850b18010
Andrew Simachev <andrew.simachev@gmail.com>
2020-01-30 16:57:03 +0300

    workflow update for linux

--- update.sh
+++ update.sh
@@ -5,15 +5,35 @@ FILE="addon.tar.gz"
 GITHUB="api.github.com"
 
 token=$1;
+platform=$2;
+arch="";
+
+#js_v0.1.1_darwin-amd64.tar.gz
+#js_v0.1.1_linux-amd64.tar.gz
+
+if [ "$platform" = "ubuntu-latest" ]; then
+	arch="linux";
+fi;
+if [ "$platform" = "macos-latest" ]; then
+	arch="darwin";
+fi;
+if [ "$platform" = "windows-latest" ]; then
+	arch="windows";
+fi;
 
 if [ "$token" = "" ]; then
   echo "ERROR: token is empty"
   exit 1
 fi;
 
+if [ "$arch" = "" ]; then
+  echo "ERROR: arch not found"
+  exit 1
+fi;
+
 version=`curl -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases | jq ".[0]"`
 tag=`echo $version | jq ".tag_name"`
-asset_id=`echo $version | jq ".assets | map(select(.name | match(\"js_\";\"i\")))[0].id"`
+asset_id=`echo $version | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+_$arch\";\"i\")))[0].id"`
 
 if [ "$asset_id" = "" ]; then
   echo "ERROR: version not found"
@@ -22,8 +42,8 @@ fi;
 
 printf "Version: $tag\n"
 printf "Found asset: $asset_id\n"
-echo -n "Downloading file... "
-curl -sL -H 'Accept: application/octet-stream' https://$token:@$GITHUB/repos/$REPO/releases/assets/$asset_id > $FILE
+echo -n "Downloading file..."
+curl -sL -H 'Accept: application/octet-stream' "https://$GITHUB/repos/$REPO/releases/assets/$asset_id?access_token=$token" > $FILE
 printf "Done\n"
 
 echo -n "Uncompressing... "
@@ -38,5 +58,5 @@ rm -rf addon
 
 mv -fv protobuf/commands.js src/proto/commands.js
 rm -rf protobuf
-rm -rf $FILE
+#rm -rf $FILE
 printf "Done\n"
