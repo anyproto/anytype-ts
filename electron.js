2db8f7efea32534f3a3644e652df87799b18c7f1
Andrew Simachev <andrew.simachev@gmail.com>
2020-02-10 21:19:10 +0300

    forgot to switch off react devtools

--- electron.js
+++ electron.js
@@ -24,9 +24,11 @@ function createWindow () {
 	
 	let win = new BrowserWindow(param);
 	
+	/*
 	BrowserWindow.addDevToolsExtension(
 		path.join(os.homedir(), '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.4.0_0')
 	);
+	*/
 	
 	if (is.development) {
 		win.loadURL('http://localhost:8080');
