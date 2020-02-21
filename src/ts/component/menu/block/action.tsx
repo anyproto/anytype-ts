b85bf0847fe8a7c08f45cdedf131bafc68639ee6
Andrew Simachev <andrew.simachev@gmail.com>
2020-02-21 18:25:26 +0300

    #142

--- src/ts/component/menu/block/action.tsx
+++ src/ts/component/menu/block/action.tsx
@@ -137,7 +137,7 @@ class MenuBlockAction extends React.Component<Props, {}> {
 			{ 
 				children: [
 					//{ id: 'move', icon: 'move', name: 'Move to' },
-					//{ id: 'copy', icon: 'copy', name: 'Duplicate' },
+					{ id: 'copy', icon: 'copy', name: 'Duplicate' },
 					{ id: 'remove', icon: 'remove', name: 'Delete' },
 					{ id: 'turn', icon: 'turn', name: 'Turn into', arrow: true },
 				] 
@@ -410,23 +410,15 @@ class MenuBlockAction extends React.Component<Props, {}> {
 				break;
 				
 			case 'copy':
-				commonStore.popupOpen('tree', { 
-					data: { 
-						type: 'copy', 
-						rootId: root,
-						onConfirm: (id: string) => {
-							const lastId = blockIds[blockIds.length - 1];
-							const last = blocks[rootId].find((it: I.Block) => { return it.id == lastId; });
+				const lastId = blockIds[blockIds.length - 1];
+				const last = blocks[rootId].find((it: I.Block) => { return it.id == lastId; });
 							
-							C.BlockListDuplicate(rootId, blockIds, blockIds[blockIds.length - 1], I.BlockPosition.Bottom, (message: any) => {
-								if (last) {
-									const length = String(last.content.text || '').length;
-									focus.set(last.id, { from: length, to: length });
-									focus.apply();									
-								};
-							});
-						}
-					}, 
+				C.BlockListDuplicate(rootId, blockIds, blockIds[blockIds.length - 1], I.BlockPosition.Bottom, (message: any) => {
+					if (last) {
+						const length = String(last.content.text || '').length;
+						focus.set(last.id, { from: length, to: length });
+						focus.apply();
+					};
 				});
 				break;
 				
