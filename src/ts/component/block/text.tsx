ff4c9bf72211ebfe36d051e007f8c86e20c0d027
Andrew Simachev <andrew.simachev@gmail.com>
2020-02-25 20:02:20 +0300

    code lang selectors fix

--- src/ts/component/block/text.tsx
+++ src/ts/component/block/text.tsx
@@ -80,7 +80,7 @@ class BlockText extends React.Component<Props, {}> {
 				};
 				
 				additional = (
-					<Select initial="Language" id="lang" value={fields.lang} ref={(ref: any) => { this.refLang = ref; }} options={options} onChange={this.onLang} />
+					<Select initial="Language" id={'lang-' + id} value={fields.lang} ref={(ref: any) => { this.refLang = ref; }} options={options} onChange={this.onLang} />
 				);
 				break;
 				
