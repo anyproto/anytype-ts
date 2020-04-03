1e59701508c1bddcd491a6143c3c40ad14a0066b
Vladimir Ermolaev <v@anytype.io>
2020-04-03 18:53:40 +0300

    adding first what's new

--- src/ts/docs/help/index.tsx
+++ src/ts/docs/help/index.tsx
@@ -4,6 +4,7 @@ const Index = [
 	{ type: I.BlockType.Icon, icon: ':question:' },
 	{ type: I.BlockType.Title, text: 'Help' },
 	{ type: I.BlockType.Link, icon: ':keyboard:', name: 'Keyboard & Shortcuts', contentId: 'shortcuts' },
+	// { type: I.BlockType.Link, icon: ':wave:', name: 'What\'s new', contentId: '../new' },
 ];
 
 import Shortcuts from './shortcuts';
--- src/ts/docs/new.tsx
+++ src/ts/docs/new.tsx
@@ -3,5 +3,17 @@ import { I } from 'ts/lib';
 export default [
 	{ type: I.BlockType.Icon, icon: ':wave:' },
 	{ type: I.BlockType.Title, text: 'What\'s new' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'trololo' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Header1, text: 'Introducing Anytype' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'We are so thrilled to announce that the first Anytype version is ready! 🔥' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'We fully reworked the design and baked it with cutting edge stack of technologies to make your experience safe, secure and convenient.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'It’s completely ready for your personal usage, so fill free to use it how you want.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Header3, text: 'Available features' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Dashboard with your favorite documents that could be added with «Plus» sign, sorted and archived;' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Editor with all kind of blocks to create notes, ideas, bookmarks collection, wikis, receipts, diaries, to-do lists, and many more. You can use the clipboard and paste any content you want;' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Drop everything into editor. E v e r y t h i n g. Folders will be converted into pages, files there will be stored in pages. Easiest way to export and organise media from your computer.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '     …Many other cool features we want you to find, try and we hope enjoy!' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Don’t forget to <font color="red">save your seed phrase</font> in case you want to log out and get all the data from another device.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Please, write as a review, feature or bug request and join the discussion in Telegram. We would love to hear your voice.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'All the great new features will come in automatic updates, so don`t be worry about app getting outdated.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '<font color="grey"><i>03 March 2020</i></font>' },
 ];
