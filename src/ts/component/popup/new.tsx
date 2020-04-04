0bc84c598ff03ca600b89e6b053a6342ad9f051d
Andrew Simachev <andrew.simachev@gmail.com>
2020-04-04 12:07:44 +0300

    #232

--- src/ts/component/popup/new.tsx
+++ src/ts/component/popup/new.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { Label, Icon } from 'ts/component';
 import { I, Docs } from 'ts/lib';
 import { commonStore } from 'ts/store';
 import { observer } from 'mobx-react';
@@ -13,12 +14,18 @@ interface Props extends I.Popup, RouteComponentProps<any> {
 class PopupNew extends React.Component<Props, {}> {
 	
 	render () {
-		const path: any[] = [
-			{ icon: ':question:', name: 'Help', contentId: 'index' }
-		];
-
 		return (
 			<div className="wrapper">
+				<div className="head">
+					<div className="side left">
+						<Label text="What’s new?" />
+					</div>
+					<div className="side right">
+						<Label text="Stay tuned for Anytype’s news " />
+						<Icon className="telegram" />
+						<Icon className="twitter" />
+					</div>
+				</div>
 				<div className="editor">
 					<div className="blocks">
 						{Docs.New.map((item: any, i: number) => (
