0f9171569592943c66f4edb352acf9c33f611f5b
Vladimir Ermolaev <v@anytype.io>
2020-04-07 19:13:10 +0300

    Change the title and styling

--- src/ts/docs/new.tsx
+++ src/ts/docs/new.tsx
@@ -2,10 +2,9 @@ import { I } from 'ts/lib';
 
 export default [
 	{ type: I.BlockType.Icon, icon: ':wave:' },
-	{ type: I.BlockType.Title, text: 'What\'s new' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Header1, text: 'Introducing Anytype' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '</br>We fully reworked the design and baked it with cutting edge technologies to make your experience safe, secure and convenient.' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Anytype now is <font style="background-color: #fef9cc;">ready for your personal usage</font>. 🔥' },
+	{ type: I.BlockType.Title, text: 'Introducing Anytype' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'We fully reworked the design and baked it with cutting edge technologies to make your experience safe, secure and convenient.' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Anytype now is <font style="background-color: #fef9cc;">ready for your personal usage</font>.' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Header3, text: 'Available features' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Editor with all kinds of blocks. It is ready for any notes, ideas, collections, knowledge books, receipts, diaries, to-do lists, travel plans, galleries... whatever you want;' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Media content. Organize your space with playable videos, photos, and web bookmarks;' },
@@ -13,7 +12,7 @@ export default [
 	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Page styling. Cover the page with photo, use emoji and use all the text formatting;' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Bulleted, text: 'Dashboard. Add your favorite documents, sort, and archive.' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '</br><b>Create a new home for your information — private and free</b>.</br></br>' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Thank you for building a new web together. Writing us a review, bug or feature request moving all of us forward. 🙏' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Thank you for building a new web together. Writing us a review, bug or feature request </br> moving all of us forward. 🙏' },
 	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: 'Don’t forget to <font style="color: #f55522;">save your seed phrase</font> in case you want to log out. All the new features will come in automatic updates, so don`t be worry about the outdated app.' },
-	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '</br><font style="text-align: right; color:#aca996"><i>03 March 2020</i></font>' },
+	{ type: I.BlockType.Text, style: I.TextStyle.Paragraph, text: '</br><font style="text-align: right; color:#aca996">03 March 2020</font>' },
 ];
