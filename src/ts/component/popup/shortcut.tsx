4dc4522589d00516b74dbdaf899d69846815d0ea
Andrew Simachev <andrew.simachev@gmail.com>
2020-08-28 13:18:31 +0300

    #614: section structure

--- src/ts/component/popup/shortcut.tsx
+++ src/ts/component/popup/shortcut.tsx
@@ -1,26 +1,45 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { RouteComponentProps } from 'react-router';
-import { I } from 'ts/lib';
+import { I, Util } from 'ts/lib';
 
 interface Props extends I.Popup, RouteComponentProps<any> {
 	history: any;
 };
 
+interface State {
+	page: string;
+};
+
 const $ = require('jquery');
 
-class PopupShortcut extends React.Component<Props, {}> {
+class PopupShortcut extends React.Component<Props, State> {
+
+	state = {
+		page: 'main',
+	};
 	
 	render () {
+		const { page } = this.state;
+		const platform = Util.getPlatform();
 		const tabs = [
 			{ id: 'main', name: 'Main' },
 			{ id: 'navigation', name: 'Navigation' },
 			{ id: 'markdown', name: 'Markdown' },
 			{ id: 'command', name: 'Commands' },
 		];
+		const sections = this.getSections(page);
 
 		const Tab = (item: any) => (
-			<div className="item">{item.name}</div>
+			<div className={[ 'item', (item.id == page ? 'active' : '') ].join(' ')} onClick={(e: any) => { this.onPage(item.id); }}>
+				{item.name}
+			</div>
+		);
+
+		const Section = (item: any) => (
+			<div className="section">
+				<div className="name">{item.name}</div>
+			</div>
 		);
 
 		return (
@@ -34,6 +53,9 @@ class PopupShortcut extends React.Component<Props, {}> {
 				</div>
 
 				<div className="body">
+					{sections.map((item: any, i: number) => (
+						<Section key={i} {...item} />
+					))}
 				</div>
 			</div>
 		);
@@ -44,16 +66,52 @@ class PopupShortcut extends React.Component<Props, {}> {
 		this.props.position();
 	};
 
+	onPage (id: string) {
+		this.setState({ page: id });
+	};
+
+	getSections (id: string) {
+		const sections = {
+
+			main: [
+				{ 
+					name: 'Basics', children: [
+
+					]
+				},
+				{ 
+					name: 'Structuring', children: [
+
+					] 
+				},
+				{ 
+					name: 'Selection', children: [
+
+					]
+				},
+				{ 
+					name: 'Actions', children: [
+
+					]
+				},
+				{ 
+					name: 'Text style', children: [
+
+					]
+				},
+			]
+
+		};
+
+		return sections[id] || [];
+	};
+
 	resize () {
 		const win = $(window);
 		const node = $(ReactDOM.findDOMNode(this));
-		const head = node.find('.head');
 		const body = node.find('.body');
-		const height = win.height() - 100;
 
-		console.log(win.height(), head.outerHeight());
-
-		body.css({ height: height });
+		body.css({ height: win.height() - 100 });
 	};
 	
 };
