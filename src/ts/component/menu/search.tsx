593490b567c9a53e989f30af916f5cf51d7432c9
Andrew Simachev <andrew.simachev@gmail.com>
2020-09-04 14:14:32 +0300

    #688: search refactoring

--- src/ts/component/menu/search.tsx
+++ src/ts/component/menu/search.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
 import { Input } from 'ts/component';
-import { I, keyboard, Util } from 'ts/lib';
+import { I, keyboard } from 'ts/lib';
 
 interface Props extends I.Menu {};
 
+const $ = require('jquery');
+const Constant = require('json/constant.json');
+const findAndReplaceDOMText = require('findandreplacedomtext');
+
 class MenuSearch extends React.Component<Props, {}> {
 	
-	ref: any = null; 
+	ref: any = null;
+	last: string = '';
+	n: number = 0;
 	
 	constructor (props: any) {
 		super(props);
 		
-		this.onChange = this.onChange.bind(this);
+		this.onKeyUp = this.onKeyUp.bind(this);
+		this.onKeyDown = this.onKeyDown.bind(this);
 	};
 
 	render () {
@@ -20,12 +27,12 @@ class MenuSearch extends React.Component<Props, {}> {
 		const { value } = data;
 		
 		return (
-			<form className="flex" onSubmit={this.onChange}>
-				<Input ref={(ref: any) => { this.ref = ref; }} value={value} placeHolder="Search..." />
+			<div className="flex">
+				<Input ref={(ref: any) => { this.ref = ref; }} value={value} placeHolder="Search..." onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp} />
 				<div className="buttons">
-					<div className="btn" onClick={this.onChange}>Search</div>
+					<div className="btn" onClick={this.onKeyUp}>Search</div>
 				</div>
-			</form>
+			</div>
 		);
 	};
 	
@@ -38,17 +45,99 @@ class MenuSearch extends React.Component<Props, {}> {
 	};
 
 	componentWillUnmount () {
+		this.clear();
 		keyboard.setFocus(false);
 	};
+
+	onKeyDown (e: any) {
+		keyboard.shortcut('tab', e, (pressed: string) => {
+			e.preventDefault();
+		});
+	};
 	
-	onChange (e: any) {
+	onKeyUp (e: any) {
 		e.preventDefault();
 		
 		const { param } = this.props;
 		const { data } = param;
-		const { onChange } = data;
+		const { container } = data;
+		const value = this.ref.getValue();
+
+		let ret = false;
+		keyboard.shortcut('arrowup, arrowdown, tab, enter', e, (pressed: string) => {
+			this.focus();
+			this.n += pressed == 'arrowup' ? -1 : 1;
+			ret = true;
+		});
+
+		if (ret) {
+			return;
+		};
+
+		if (this.last != value) {
+			this.n = 0;
+			this.clear();
+			console.log('clear', value);
+		};
+		this.last = value;
+
+		if (!value) {
+			return;
+		};
+
+		findAndReplaceDOMText(container.get(0), {
+			preset: 'prose',
+			find: new RegExp(value, 'gi'),
+			wrap: 'search',
+			portionMode: 'first',
+			filterElements: (el: any) => {
+				const tag = el.nodeName.toLowerCase();
+				if ([ 'span', 'div' ].indexOf(tag) < 0) {
+					return false;
+				};
+
+				const style = window.getComputedStyle(el);
+				if ((style.display == 'none') || (style.opacity == '0') || (style.visibility == 'hidden')) {
+					return false;
+				};
+				return true;
+			},
+		});
+	};
+
+	clear () {
+		const { param } = this.props;
+		const { data } = param;
+		const { container } = data;
+
+		container.find('search').each((i: number, item: any) => {
+			item = $(item);
+			item.replaceWith(item.html());
+		});
+	};
+
+	focus () {
+		const { param } = this.props;
+		const { data } = param;
+		const { container } = data;
+		const win = $(window);
+		const items = container.find('.editor search');
+		const wh = win.height();
+		const offset = Constant.size.lastBlock + Constant.size.header;
+
+		if (this.n > items.length - 1) {
+			this.n = 0;
+		};
+
+		container.find('search.active').removeClass('active');
+
+		const next = $(items.get(this.n));
+		if (next && next.length) {
+			next.addClass('active');
 		
-		onChange(Util.filterFix(this.ref.getValue()));
+			const y = next.offset().top;
+			$('html, body').stop(true, true).animate({ scrollTop: y - wh + offset }, 100);
+		};
 	};
 	
 };
