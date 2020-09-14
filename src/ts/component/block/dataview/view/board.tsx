c306cc889e27f0c9578f161d2bed8a409abb2ce7
Andrew Simachev <andrew.simachev@gmail.com>
2020-09-14 17:53:29 +0300

    #389: board dnd

--- src/ts/component/block/dataview/view/board.tsx
+++ src/ts/component/block/dataview/view/board.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { Icon } from 'ts/component';
 import { I, Util } from 'ts/lib';
 import { observer } from 'mobx-react';
-import { blockStore } from 'ts/store'
+import { blockStore } from 'ts/store';
 
 import Cell from '../cell';
 
@@ -14,12 +15,24 @@ interface Column {
 	list: any[];
 };
 
+const getItemStyle = (snapshot: any, draggableStyle: any) => {
+	return draggableStyle;
+};
+
 const GROUP = 'name';
 const Constant = require('json/constant.json');
 const $ = require('jquery');
 
 @observer
 class ViewBoard extends React.Component<Props, {}> {
+
+	constructor (props: any) {
+		super(props);
+		
+		this.onAdd = this.onAdd.bind(this);
+		this.onDragStart = this.onDragStart.bind(this);
+		this.onDragEnd = this.onDragEnd.bind(this);
+	};
 
 	render () {
 		const { rootId, block, view, readOnly } = this.props;
@@ -34,61 +47,115 @@ class ViewBoard extends React.Component<Props, {}> {
 		const { offset, total } = blockStore.getDbMeta(block.id);
 		const columns = this.getColumns();
 		
-		const Card = (item: any) => (
-			<div className="card">
-				{relations.map((relation: any, i: number) => (
+		const Card = (item: any) => {
+			return (
+				<Draggable key={item.index} draggableId={item.column + '-' + item.index} index={item.index}>
+					{(provided: any, snapshot: any) => (
+						<div 
+							className="card"
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							{...provided.dragHandleProps}
+							style={getItemStyle(snapshot, provided.draggableProps.style)}
+						>
+							{relations.map((relation: any, i: number) => (
+								<Cell 
+									key={'board-cell-' + relation.id} 
+									id={item.index} 
+									rootId={rootId}
+									block={block}
+									view={view} 
+									relation={...relation} 
+									data={item.data} 
+									readOnly={readOnly} 
+								/>
+							))}
+						</div>
+					)}
+				</Draggable>
+			);
+		};
+
+		const Add = (item: any) => (
+			<Draggable draggableId={item.column + '-add'} index={item.index}>
+				{(provided: any, snapshot: any) => (
+					<div 
+						className="card add"
+						ref={provided.innerRef}
+						{...provided.draggableProps}
+						{...provided.dragHandleProps}
+						style={getItemStyle(snapshot, provided.draggableProps.style)}
+						onClick={() => { this.onAdd(item.column); }}
+					>
+						<Icon className="plus" />
+					</div>
+				)}
+			</Draggable>
+		);
+
+		const Head = (item: any) => {
+			const head = {};
+			head[GROUP] = item.value;
+			return (
+				<div className="head">
 					<Cell 
-						key={'board-cell-' + relation.id} 
-						id={item.index} 
+						id="" 
 						rootId={rootId}
 						block={block}
 						view={view} 
-						relation={...relation} 
-						data={item.data} 
-						readOnly={readOnly} 
+						relation={group} 
+						data={head} 
+						readOnly={true} 
 					/>
-				))}
-			</div>
-		);
-
-		const Column = (item: any) => {
-			const head = {};
-			head[GROUP] = item.value;
-
-			return (
-				<div className="column">
-					<div className="head">
-						<Cell 
-							id="" 
-							rootId={rootId}
-							block={block}
-							view={view} 
-							relation={group} 
-							data={head} 
-							readOnly={true} 
-						/>
-					</div>
-					<div className="list">
-						{item.list.map((child: any, i: number) => (
-							<Card key={'board-card-' + i} data={...child} />
-						))}
-						<div className="card add">
-							<Icon className="plus" />
-						</div>
-					</div>
 				</div>
 			);
 		};
-		
+
+		const Column = (item: any) => {
+			return (
+				<Draggable draggableId={'column-' + item.index} index={item.index}>
+					{(provided: any, snapshot: any) => (
+						<div 
+							className="column"
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							{...provided.dragHandleProps}
+							style={getItemStyle(snapshot, provided.draggableProps.style)}
+						>
+							<Head value={item.value} />
+							<Droppable droppableId={'column-' + item.index + '-drop'} direction="vertical" type="row">
+								{(provided: any) => (
+									<div className="list" {...provided.droppableProps} ref={provided.innerRef}>
+										{item.list.map((child: any, i: number) => (
+											<Card key={'board-card-' + i} column={item.index} index={i} data={...child} />
+										))}
+										<Add column={item.index} index={item.list.length + 1} />
+										{provided.placeholder}
+									</div>
+								)}
+							</Droppable>
+						</div>
+					)}
+				</Draggable>
+			);
+		};
+
 		return (
 			<div className="wrap">
 				<div className="scroll">
 					<div className="viewItem viewBoard">
-						<div className="columns">
-							{columns.map((item: any, i: number) => (
-								<Column key={i} index={i} {...item} />
-							))}
-						</div>
+						<DragDropContext onDragStart={this.onDragStart} onDragEnd={this.onDragEnd}>
+							<Droppable droppableId="columns" direction="horizontal" type="column">
+								{(provided: any) => (
+									<div className="columns" {...provided.droppableProps} ref={provided.innerRef}>
+										{columns.map((item: any, i: number) => (
+											<Column key={i} index={i} {...item} />
+										))}
+										{provided.placeholder}
+									</div>
+								)}
+							</Droppable>
+						</DragDropContext>
 					</div>
 				</div>
 			</div>
@@ -99,22 +166,28 @@ class ViewBoard extends React.Component<Props, {}> {
 		this.resize();
 	};
 
+	onAdd (column: number) {
+	};
+
+	onDragStart () {
+	};
+
+	onDragEnd () {
+	};
+
 	resize () {
-		const { view } = this.props;
 		const win = $(window);
 		const node = $(ReactDOM.findDOMNode(this));
 		const scroll = node.find('.scroll');
 		const viewItem = node.find('.viewItem');
+		const columns = this.getColumns();
 		const ww = win.width();
 		const mw = ww - 192;
+		const size = Constant.size.dataview.board;
 		
 		let vw = 0;
 		let margin = 0;
-		let width = 0;
-
-		for (let relation of view.relations) {
-			width += relation.width;
-		};
+		let width = columns.length * (size.card + size.margin);
 
 		if (width < mw) {
 			vw = mw;
