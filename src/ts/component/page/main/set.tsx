d64ef77c0b6efe6da7a7db38c30f375e99705dc9
Andrew Simachev <andrew.simachev@gmail.com>
2020-09-15 18:43:42 +0300

    #722: object selection screen

--- src/ts/component/page/main/set.tsx
+++ src/ts/component/page/main/set.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { observer } from 'mobx-react';
-import { Icon, HeaderMainSet as Header } from 'ts/component';
+import { Icon, Title, Label, Smile, HeaderMainSet as Header } from 'ts/component';
 
 interface Props extends RouteComponentProps<any> {};
 
@@ -13,11 +13,48 @@ class PageMainSet extends React.Component<Props, {}> {
 	};
 
 	render () {
+		const items = [
+			{ id: '', icon: 'page', emoji: '', name: 'Page' },
+			{ id: '', icon: 'contact', emoji: '', name: 'Contact' },
+			{ id: '', icon: 'task', emoji: '', name: 'Task' },
+			{ id: '', icon: '', emoji: '💾', name: 'Doc' },
+			{ id: '', icon: '', emoji: '🎓', name: 'Univercity' },
+			{ id: '', icon: '', emoji: '📅', name: 'Meeting' },
+			{ id: '', icon: '', emoji: '📚', name: 'Book review' },
+		];
+
+		const Item = (item: any) => {
+			let icon = null;
+			if (item.emoji) {
+				icon = <Smile icon={item.emoji} />;
+			} else 
+			if (item.icon) {
+				icon = <Icon className={item.icon} />;
+			};
+			return (
+				<div className="item">
+					{icon}
+					<div className="name">{item.name}</div>
+				</div>
+			);
+		};
+
 		return (
 			<div>
 				<Header {...this.props} rootId="" />
 				<div className="wrapper">
 					<Icon className="new" />
+					<Title text="New set" />
+					<Label text="Choose a object type for this set" />
+					<div className="items">
+						<div className="item add">
+							<Icon className="add" />
+							<div className="name">Create new object type</div>
+						</div>
+						{items.map((item: any, i: number) => (
+							<Item key={i} {...item} />
+						))}
+					</div>
 				</div>
 			</div>
 		);
