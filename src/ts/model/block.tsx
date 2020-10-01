b2cc1fb304aebde4f554309d0ad84b35019fc770
Andrew Simachev <andrew.simachev@gmail.com>
2020-10-01 15:01:05 +0300

    selection fix

--- src/ts/model/block.tsx
+++ src/ts/model/block.tsx
@@ -73,7 +73,7 @@ class Block implements I.Block {
 	};
 	
 	isSelectable (): boolean {
-		return !this.isSystem() && !this.isIcon() && !this.isTextTitle();
+		return !this.isSystem() && !this.isIcon();
 	};
 	
 	isDraggable (): boolean {
