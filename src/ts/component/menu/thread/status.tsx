eb1fa8f8bac963886197536a99b78f2ab99106ed
Andrew Simachev <andrew.simachev@gmail.com>
2021-01-29 01:06:05 +0300

    #995 fix

--- src/ts/component/menu/thread/status.tsx
+++ src/ts/component/menu/thread/status.tsx
@@ -12,7 +12,7 @@ class MenuThreadStatus extends React.Component<Props, {}> {
 		const { rootId, isCafe, accountId } = data;
 		const thread = authStore.threadGet(rootId);
 		const { cafe, accounts } = thread;
-		const { status, files } = cafe;
+		const { status, files } = cafe || {};
 		const account = accounts.find((it: I.ThreadAccount) => { return it.id == accountId; });
 
 		const Item = (item: any) => (
