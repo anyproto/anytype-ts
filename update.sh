355b7cebf58d81ebe79c3e03351e2139857c69dc
Andrew Simachev <andrew.simachev@gmail.com>
2021-02-03 12:46:12 +0300

    fix workflow

--- update.sh
+++ update.sh
@@ -1,73 +1,72 @@
 #!/usr/bin/env bash
-{
-  REPO="anytypeio/go-anytype-middleware"
-  FILE="addon.tar.gz"
-  GITHUB="api.github.com"
 
-  token=$1;
-  platform=$2;
-  arch="";
+REPO="anytypeio/go-anytype-middleware"
+FILE="addon.tar.gz"
+GITHUB="api.github.com"
 
-  if [ "$platform" = "ubuntu-latest" ]; then
-    arch="linux";
-  elif [ "$platform" = "macos-latest" ]; then
-    arch="darwin";
-  elif [ "$platform" = "windows-latest" ]; then
-    arch="windows";
-    FILE="addon.zip"
-  fi;
+token=$1;
+platform=$2;
+arch="";
 
-  if [ "$token" = "" ]; then
-    echo "ERROR: token is empty"
-    exit 1
-  fi;
+if [ "$platform" = "ubuntu-latest" ]; then
+  arch="linux";
+elif [ "$platform" = "macos-latest" ]; then
+  arch="darwin";
+elif [ "$platform" = "windows-latest" ]; then
+  arch="windows";
+  FILE="addon.zip"
+fi;
 
-  if [ "$arch" = "" ]; then
-    echo "ERROR: arch not found"
-    exit 1
-  fi;
+if [ "$token" = "" ]; then
+  echo "ERROR: token is empty"
+  exit 1
+fi;
 
-  version=`curl -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases | jq ".[0]"`
-  tag=`echo $version | jq ".tag_name"`
-  asset_id=`echo $version | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_$arch\";\"i\")))[0].id"`
+if [ "$arch" = "" ]; then
+  echo "ERROR: arch not found"
+  exit 1
+fi;
 
-  if [ "$asset_id" = "" ]; then
-    echo "ERROR: version not found"
-    exit 1
-  fi;
+version=`curl -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases | jq ".[0]"`
+tag=`echo $version | jq ".tag_name"`
+asset_id=`echo $version | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_$arch\";\"i\")))[0].id"`
 
-  printf "Version: $tag\n"
-  printf "Found asset: $asset_id\n"
-  echo -n "Downloading file..."
-  curl -sL -H "Authorization: token $token" -H 'Accept: application/octet-stream' "https://$GITHUB/repos/$REPO/releases/assets/$asset_id" > $FILE
+if [ "$asset_id" = "" ]; then
+  echo "ERROR: version not found"
+  exit 1
+fi;
+
+printf "Version: $tag\n"
+printf "Found asset: $asset_id\n"
+echo -n "Downloading file..."
+curl -sL -H "Authorization: token $token" -H 'Accept: application/octet-stream' "https://$GITHUB/repos/$REPO/releases/assets/$asset_id" > $FILE
+printf "Done\n"
+
+if [ "$platform" = "windows-latest" ]; then
+  echo -n "Uncompressing... "
+  unzip $FILE
+  printf "Done\n"
+  
+  echo "Moving... "
+  mv -fv grpc-server.exe dist/anytypeHelper.exe
+else 
+  echo -n "Uncompressing... "
+  tar -zxf $FILE
   printf "Done\n"
 
-  if [ "$platform" = "windows-latest" ]; then
-    echo -n "Uncompressing... "
-    unzip $FILE
-    printf "Done\n"
-    
-    echo "Moving... "
-    mv -fv grpc-server.exe dist/anytypeHelper.exe
-  else 
-    echo -n "Uncompressing... "
-    tar -zxf $FILE
-    printf "Done\n"
+  echo "Moving... "
+  rm -rf build
+  mkdir -p build
+  mv -fv addon/* build/
+  rm -rf addon
+fi;
 
-    echo "Moving... "
-    rm -rf build
-    mkdir -p build
-    mv -fv addon/* build/
-    rm -rf addon
-  fi;
+rm -rf dist/lib/pb
+rm -rf dist/lib/pkg
+rm -rf dist/lib/protos
+mv -fv protobuf/* dist/lib/
+rm -rf protobuf
+rm -rf $FILE
+printf "Done\n\n"
 
-  rm -rf dist/lib/pb
-  rm -rf dist/lib/pkg
-  rm -rf dist/lib/protos
-  mv -fv protobuf/* dist/lib/
-  rm -rf protobuf
-  rm -rf $FILE
-  printf "Done\n\n"
-
-  du -a dist/lib
-} | tee -a update.log
+du -a dist/lib
