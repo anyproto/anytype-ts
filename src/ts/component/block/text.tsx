b719d7733374994f2c1aa33f7a0c04ae290f38a2
Andrew Simachev <andrew.simachev@gmail.com>
2021-03-27 11:55:56 +0300

    CU-ff14jm: Do not open menu on slash on code blocks

--- src/ts/component/block/text.tsx
+++ src/ts/component/block/text.tsx
@@ -569,7 +569,7 @@ class BlockText extends React.Component<Props, {}> {
 		};
 
 		// Open add menu
-		if ((symbolBefore == '/') && ([ Key.backspace, Key.escape ].indexOf(k) < 0) && !menuOpenAdd) {
+		if ((symbolBefore == '/') && ([ Key.backspace, Key.escape ].indexOf(k) < 0) && !menuOpenAdd && !block.isTextCode()) {
 			value = Util.stringCut(value, range.from - 1, range.from);
 			onMenuAdd(id, value, range);
 		};
