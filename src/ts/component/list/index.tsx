a4649bde2751e276cf403e85e884e7778c4acd1c
Andrew Simachev <andrew.simachev@gmail.com>
2021-05-13 14:26:31 +0300

    fix relationLinks

--- src/ts/component/list/index.tsx
+++ src/ts/component/list/index.tsx
@@ -38,7 +38,7 @@ class ListIndex extends React.Component<Props, {}> {
 		
 		const Item = SortableElement((item: any) => {
 			const content = item.content || {};
-			const object = detailStore.get(root, content.targetBlockId, [ 'relationFormat' ]);
+			const object = detailStore.get(root, content.targetBlockId, []);
 			const { _objectEmpty_, name, layout, iconEmoji, iconImage } = object;
 			const type = dbStore.getObjectType(object.type);
 			const cn = [ 'item' ];
@@ -52,8 +52,6 @@ class ListIndex extends React.Component<Props, {}> {
 					</div>
 				);
 			};
-
-			console.log(object.name, object.layout);
 
 			let icon = null;
 			let showMenu = true;
