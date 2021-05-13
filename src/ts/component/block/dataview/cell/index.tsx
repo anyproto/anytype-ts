1e05d4bf1152d00165548786e4a8847ad883dc5d
Andrew Simachev <andrew.simachev@gmail.com>
2021-05-13 09:39:43 +0300

    CU-jqc4b1: fix

--- src/ts/component/block/dataview/cell/index.tsx
+++ src/ts/component/block/dataview/cell/index.tsx
@@ -330,9 +330,6 @@ class Cell extends React.Component<Props, {}> {
 			return null;
 		};
 
-		console.log('onChange', relation.relationKey, value);
-		console.trace();
-	
 		const record = getRecord(index);
 		if (record && onCellChange) {
 			onCellChange(record.id, relation.relationKey, DataUtil.formatRelationValue(relation, value, true), callBack);
