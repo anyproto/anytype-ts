1626fa7ba3757e7e7abdb5ea7019b3d844c97344
Andrew Simachev <andrew.simachev@gmail.com>
2021-06-17 22:51:11 +0300

    #mq6p1z: fix cover scale

--- src/ts/component/block/cover.tsx
+++ src/ts/component/block/cover.tsx
@@ -248,7 +248,7 @@ class BlockCover extends React.Component<Props, State> {
 		
 		const { rootId } = this.props;
 		const object = detailStore.get(rootId, rootId, [ 'coverId', 'coverType', 'coverScale' ], true);
-		const { coverId, coverType, coverScale } = object;
+		const { coverId, coverType } = object;
 		const node = $(ReactDOM.findDOMNode(this));
 		const isImage = [ I.CoverType.Upload, I.CoverType.Image ].indexOf(coverType) >= 0;
 		
@@ -264,10 +264,13 @@ class BlockCover extends React.Component<Props, State> {
 		};
 
 		const cb = () => {
+			const object = detailStore.get(rootId, rootId, [ 'coverScale' ], true);
+			const { coverScale } = object;
+
 			if (this.refDrag) {
 				this.refDrag.setValue(coverScale);
 			};
-			
+
 			this.rect = (node.get(0) as Element).getBoundingClientRect();
 			this.onScaleMove(coverScale);
 			this.cover.css({ opacity: 1 });
