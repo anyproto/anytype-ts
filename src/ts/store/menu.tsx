01d306ed27b5f8e81de6db0451f829e3b6ac4881
Andrew Simachev <andrew.simachev@gmail.com>
2021-07-08 17:33:36 +0300

    fixes

--- src/ts/store/menu.tsx
+++ src/ts/store/menu.tsx
@@ -1,5 +1,5 @@
 import { observable, action, computed, set } from 'mobx';
-import { I, Storage, Util, analytics } from 'ts/lib';
+import { I, Util, analytics } from 'ts/lib';
 
 const Constant = require('json/constant.json');
 const $ = require('jquery');
