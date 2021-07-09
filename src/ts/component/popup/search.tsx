8b972f432cea30cef5f79faf2f925dc2bf335da6
Andrew Simachev <andrew.simachev@gmail.com>
2021-07-09 11:15:56 +0300

    fix

--- src/ts/component/popup/search.tsx
+++ src/ts/component/popup/search.tsx
@@ -384,7 +384,7 @@ class PopupSearch extends React.Component<Props, State> {
 		const { config } = commonStore;
 		const { filter } = this.state;
 		
-		let skipLayouts = [ I.ObjectLayout.File, I.ObjectLayout.Image ];
+		let skipLayouts = [ I.ObjectLayout.File, I.ObjectLayout.Image, I.ObjectLayout.Video ];
 		if (!config.allowDataview) {
 			skipLayouts = skipLayouts.concat(I.ObjectLayout.Human, I.ObjectLayout.Set, I.ObjectLayout.ObjectType, I.ObjectLayout.Relation);
 		};
