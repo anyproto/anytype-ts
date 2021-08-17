a154020b7fbccc5eae6df4313070be06a8469bdf
Andrew Simachev <andrew.simachev@gmail.com>
2021-08-17 20:25:27 +0300

    graph tweaks

--- src/ts/component/popup/graph.tsx
+++ src/ts/component/popup/graph.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { I, C, Util, DataUtil, SmileUtil } from 'ts/lib';
-import { commonStore } from 'ts/store';
+import { I, C, Util, DataUtil, SmileUtil, translate } from 'ts/lib';
+import { commonStore, blockStore } from 'ts/store';
 import { observer } from 'mobx-react';
 import * as d3 from 'd3';
 
@@ -10,7 +10,6 @@ interface Props extends I.Popup {};
 const fs = window.require('fs');
 const { app } = window.require('electron').remote;
 const $ = require('jquery');
-const data = require('json/graph.json');
 const path = window.require('path');
 const userPath = app.getPath('userData');
 
@@ -19,11 +18,11 @@ const BG1 = '#f0efe9';
 
 const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> {
 
-	weights: any = {};
-
 	render () {
 		return (
-			<div />
+			<div>
+				<div id="graph" />
+			</div>
 		);
 	};
 
@@ -40,36 +39,49 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 			let data = { edges: [], nodes: [] };
 
 			try { data = JSON.parse(content); } catch (e) {};
-			this.init(data);
 
+			this.init(data);
 			Util.deleteFolderRecursive(message.path);
 		});
 	};
 
-	init (data: any) {
+	init ({ edges, nodes }) {
+		edges = edges.map(d => Object.create(d));
+  		nodes = nodes.map(d => Object.create(d));
+
 		const { getId } = this.props;
+		const { root } = blockStore;
 		const obj = $(`#${getId()} #innerWrap`);
 		const node = $(ReactDOM.findDOMNode(this));
-		const edges = data.edges.map(d => Object.create(d));
-  		const nodes = data.nodes.map(d => Object.create(d));
 		const width = obj.width();
 		const height = obj.height();
 		const transform = d3.zoomIdentity;
 
+		let weights: any = {};
+
 		for (let item of nodes) {
-			this.weights[item.id] = {
+			weights[item.id] = {
 				source: edges.filter((it: any) => { return it.source == item.id; }).length,
 				target: edges.filter((it: any) => { return it.target == item.id; }).length,
 			};
 		};
 
-  		const simulation = d3.forceSimulation(nodes)
-		.force('link', d3.forceLink(edges).id(d => d.id).distance(20))
-		.force('charge', d3.forceManyBody())
-		.force('center', d3.forceCenter(width / 2, height / 2))
-		.force('collision', d3.forceCollide(nodes).radius(d => this.radius(d)));
+		nodes = nodes.map((d: any) => {
+			d.name = d.name || translate('defaultNamePage');
+			d.radius = Math.max(5, Math.min(10, weights[d.id].source));
+			if (d.id == root) {
+				d.radius = 15;
+			};
+			return d;
+		});
 
-		const svg = d3.create('svg')
+  		const simulation = d3.forceSimulation(nodes)
+		.force('link', d3.forceLink(edges).id(d => d.id).distance(10))
+		.force('charge', d3.forceManyBody().strength(-20))
+		.force('center', d3.forceCenter(width / 2, height / 2))
+		.force('collision', d3.forceCollide(nodes).radius(d => d.radius * 1.5));
+
+		const svg = d3.select("#graph").append('svg')
 		.attr('viewBox', [ 0, 0, width, height ])
 		.attr('width', width)
     	.attr('height', height)
@@ -93,28 +105,28 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 		.append('svg:image')
 		.attr('preserveAspectRatio', 'xMidYMid slice')
 		.attr('width', (d: any) => {
-			let r = this.radius(d);
+			let r = d.radius;
 			if (d.iconImage) {
 				r *= 2;
 			};
 			return r;
 		})
 		.attr('height', (d: any) => {
-			let r = this.radius(d);
+			let r = d.radius;
 			if (d.iconImage) {
 				r *= 2;
 			};
 			return r;
 		})
 		.attr('x', (d: any) => {
-			let r = this.radius(d) / 2;
+			let r = d.radius / 2;
 			if (d.iconImage) {
 				r = 0;
 			};
 			return r;
 		})
 		.attr('y', (d: any) => {
-			let r = this.radius(d) / 2;
+			let r = d.radius / 2;
 			if (d.iconImage) {
 				r = 0;
 			};
@@ -131,26 +143,27 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 				src = src.replace(/^.\//, '');
 			};
 			if (d.iconImage) {
-				src = commonStore.imageUrl(d.iconImage, this.radius(d) * 2);
+				src = commonStore.imageUrl(d.iconImage, d.radius * 2);
 			};
 			if (!src) {
-				console.log(d.id, d.name, d.type, d.layout);
-				src = 'img/space.svg';
+				src = 'img/icon/page.svg';
 			};
 			return src;
-		})
+		});
+
+		const tooltip = d3.select("#graph")
+  		.append('div')
+		.attr('class', 'tooltip');
 
 		const link = group.append('g')
 		.attr('stroke', '#eae9e0')
 		.attr('stroke-opacity', 1)
-		.attr('stroke-width', 1)
+		.attr('stroke-width', 0.5)
 		.selectAll('line')
 		.data(edges)
-		.join('line')
+		.join('line');
 
 		const el = group.append('g')
-		.attr('stroke', '#fff')
-		.attr('stroke-width', 1)
 		.selectAll('g')
 		.data(nodes)
 		.join('g')
@@ -160,9 +173,17 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 		})
 		.on('mouseenter', function (e: any, d: any) {
 			d3.select(this).select('#bg').style('fill', BG1);
+			
+			tooltip.style('display', 'block').text(Util.shorten(d.name, 24));
+		})
+		.on('mousemove', (e: any) => {
+			tooltip.
+			style('top', (e.pageY + 10) + 'px').
+			style('left', (e.pageX + 10) + 'px');
 		})
 		.on('mouseleave', function (e: any, d: any) {
 			d3.select(this).select('#bg').style('fill', BG0);
+			tooltip.style('display', 'none');
 		})
 		.call(this.drag(simulation));
 
@@ -173,10 +194,10 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 		const bg = el.append('circle')
 		.style('fill', BG0)
 		.attr('id', 'bg')
-		.attr('r', d => this.radius(d));
+		.attr('r', d => d.radius);
 
 		const img = el.append('circle')
-		.attr('r', d => this.radius(d))
+		.attr('r', d => d.radius)
 		.style('fill', (d: any) => { return `url(#${d.id})`; });
 
 		simulation.on('tick', () => {
@@ -195,12 +216,7 @@ const PopupGraph = observer(class PopupGraph extends React.Component<Props, {}> 
 			.attr('cy', d => d.y);
 		});
 
-		node.append(svg.node());
-	};
-
-	radius (d: any) {
-		const r = Math.max(8, Math.min(20, (this.weights[d.id].source + this.weights[d.id].target) / 2));
-		return r;
+		node.find('#graph').append(svg.node());
 	};
 
 	drag (simulation: any) {
