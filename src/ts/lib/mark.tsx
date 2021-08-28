43b032cf1e5c4f7ff19368713742f79d3ae4af3d
Andrew Simachev <andrew.simachev@gmail.com>
2021-08-28 11:12:09 +0300

    #1403vh8: Remove duplicate move code and fix link markup adjustments

--- src/ts/lib/mark.tsx
+++ src/ts/lib/mark.tsx
@@ -131,20 +131,6 @@ class Mark {
 		return Util.unmap(map).sort(this.sort);
 	};
 	
-	move (marks: I.Mark[], start: number, diff: number) {
-		marks = marks || [];
-		for (let mark of marks) {
-			if ((mark.range.from < start && mark.range.to >= start) || (!start && !mark.range.from)) {
-				mark.range.to += diff;
-			} else
-			if (mark.range.from >= start) {
-				mark.range.from += diff;
-				mark.range.to += diff;
-			};
-		};
-		return marks;
-	};
-	
 	sort (c1: I.Mark, c2: I.Mark) {
 		if (c1.type > c2.type) return 1;
 		if (c1.type < c2.type) return -1;
@@ -231,6 +217,7 @@ class Mark {
 				mark.range.from += length;
 				mark.range.to += length;
 			};
+			mark.range.from = Math.max(0, mark.range.from);
 		};
 		return marks;
 	};
@@ -441,17 +428,6 @@ class Mark {
 			return { marks, text };
 		};
 
-		// Marks should be moved by replacement lengths
-		let move = (from: number, offset: number) => {
-			for (let i in marks) {
-				let m = marks[i];
-				if (m.range.from >= from) {
-					m.range.from = Math.max(0, m.range.from - offset);
-					m.range.to = Math.max(0, m.range.to - offset);
-				};
-			};
-		};
-
 		// Markdown
 		for (let item of this.regexpMarkdown) {
 			html = text;
@@ -466,7 +442,7 @@ class Mark {
 				let to = from + p3.length;
 				let replace = p1 + p3 + ' ';
 
-				move(from, p2.length * 2);
+				this.adjust(marks, from, -p2.length * 2);
 				marks.push({ type: item.type, range: { from: from, to: to }, param: '' });
 				text = text.replace(s, replace);
 				return s;
@@ -478,11 +454,34 @@ class Mark {
 		html.replace(/\[([^\[\]]+)\]\(([^\(\)]+)\)(\s|$)/g, (s: string, p1: string, p2: string, p3: string) => {
 			p1 = String(p1 || '');
 			p2 = String(p2 || '');
+			p3 = String(p3 || '');
 
 			let from = (Number(text.indexOf(s)) || 0);
 			let to = from + p1.length;
+			let innerIdx = [];
 
-			move(from, p2.length + 4);
+			// Remove inner links and adjust other marks to new range
+			for (let i = 0; i < marks.length; ++i) {
+				let mark = marks[i];
+
+				if (mark.type == I.MarkType.Link) {
+					marks.splice(i, 1);
+					i--;
+					continue;
+				};
+
+				if ((mark.range.from >= from) && (mark.range.to <= from + p1.length + p2.length + 4)) {
+					innerIdx.push(i);
+				};
+			};
+
+			this.adjust(marks, from, -(p2.length + 4));
+
+			for (let i of innerIdx) {
+				marks[i].range.from = from;
+				marks[i].range.to = to;
+			};
+
 			marks.push({ type: I.MarkType.Link, range: { from: from, to: to }, param: p2 });
 			text = text.replace(s, p1 + ' ');
 			return s;
