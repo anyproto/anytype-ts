687bfecc28f283f68628efc7126c68ed041f2641
Andrew Simachev <andrew.simachev@gmail.com>
2021-08-28 16:40:38 +0300

    masonry in gallery

--- src/ts/component/block/dataview/view/gallery/card.tsx
+++ src/ts/component/block/dataview/view/gallery/card.tsx
@@ -5,18 +5,19 @@ import { Cell } from 'ts/component';
 
 interface Props extends I.ViewComponent {
 	index: number;
-}
+	style?: any;
+};
 
 const Card = observer(class Card extends React.Component<Props, {}> {
 
 	render () {
-		const { index, getView, onCellClick, onRef } = this.props;
+		const { index, getView, onCellClick, onRef, style } = this.props;
 		const view = getView();
 		const relations = view.relations.filter((it: any) => { return it.isVisible; });
 		const idPrefix = 'dataviewCell';
 
 		return (
-			<div className="card">
+			<div className="card" style={style}>
 				{relations.map((relation: any, i: number) => {
 					const id = DataUtil.cellId(idPrefix, relation.relationKey, index);
 					return (
