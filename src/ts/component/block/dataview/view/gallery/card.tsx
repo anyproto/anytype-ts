b704700a9f1a28b023f040bc9357b825d78f6e25
Andrew Simachev <andrew.simachev@gmail.com>
2021-08-29 11:46:12 +0300

    gallery covers

--- src/ts/component/block/dataview/view/gallery/card.tsx
+++ src/ts/component/block/dataview/view/gallery/card.tsx
@@ -1,38 +1,63 @@
 import * as React from 'react';
 import { I, DataUtil } from 'ts/lib';
 import { observer } from 'mobx-react';
-import { Cell } from 'ts/component';
+import { Cell, Cover } from 'ts/component';
+import { commonStore, detailStore, dbStore } from 'ts/store';
 
 interface Props extends I.ViewComponent {
 	index: number;
 	style?: any;
 };
 
+const $ = require('jquery');
+const Constant = require('json/constant.json');
+
 const Card = observer(class Card extends React.Component<Props, {}> {
 
 	render () {
-		const { index, getView, onCellClick, onRef, style } = this.props;
+		const { rootId, block, index, getView, getRecord, onCellClick, onRef, style } = this.props;
 		const view = getView();
-		const relations = view.relations.filter((it: any) => { return it.isVisible; });
+		const viewRelations = view.relations.filter((it: any) => { return it.isVisible; });
+		const relations = dbStore.getRelations(rootId, block.id);
 		const idPrefix = 'dataviewCell';
+		const record = getRecord(index);
+		const file = relations.find((it: any) => { 	
+			return !it.isHidden && (it.format == I.RelationType.File); 
+		});
+
+		// Find any file relation with picture
+		let picture = '';
+		if (file) {
+			const value = DataUtil.getRelationArrayValue(record[file.relationKey]);
+			for (let id of value) {
+				const f = detailStore.get(rootId, id, []);
+				if (f && (f.type == Constant.typeId.image)) {
+					picture = commonStore.imageUrl(f.id, 600);
+					break;
+				};
+			};
+		};
 
 		return (
 			<div className="card" style={style}>
-				{relations.map((relation: any, i: number) => {
-					const id = DataUtil.cellId(idPrefix, relation.relationKey, index);
-					return (
-						<Cell 
-							key={'list-cell-' + view.id + relation.relationKey} 
-							{...this.props}
-							ref={(ref: any) => { onRef(ref, id); }} 
-							relationKey={relation.relationKey}
-							viewType={view.type}
-							idPrefix={idPrefix}
-							onClick={(e: any) => { onCellClick(e, relation.relationKey, index); }}
-							index={index}
-						/>
-					);
-				})}
+				{picture ? <Cover src={picture} /> : ''}
+				<div className="inner">
+					{viewRelations.map((relation: any, i: number) => {
+						const id = DataUtil.cellId(idPrefix, relation.relationKey, index);
+						return (
+							<Cell 
+								key={'list-cell-' + view.id + relation.relationKey} 
+								{...this.props}
+								ref={(ref: any) => { onRef(ref, id); }} 
+								relationKey={relation.relationKey}
+								viewType={view.type}
+								idPrefix={idPrefix}
+								onClick={(e: any) => { onCellClick(e, relation.relationKey, index); }}
+								index={index}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		);
 	};
