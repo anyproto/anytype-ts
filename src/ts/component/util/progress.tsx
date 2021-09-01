96f8a2233a310d3c860fe977209bb0f770d2dbda
Andrew Simachev <andrew.simachev@gmail.com>
2021-09-01 09:13:12 +0300

    progress dnd

--- src/ts/component/util/progress.tsx
+++ src/ts/component/util/progress.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Icon, Label } from 'ts/component';
-import { Util, C } from 'ts/lib';
+import { Util, C, Storage } from 'ts/lib';
 import { commonStore } from 'ts/store';
 import { observer } from 'mobx-react';
 
@@ -11,10 +11,17 @@ const $ = require('jquery');
 
 const Progress = observer(class Progress extends React.Component<Props, {}> {
 	
+	obj: any = null;
+	dx: number = 0;
+	dy: number = 0;
+	width: number = 0;
+	height: number = 0;
+
 	constructor (props: any) {
 		super(props);
 		
 		this.onCancel = this.onCancel.bind(this);
+		this.onDragStart = this.onDragStart.bind(this);
 	};
 	
 	render () {
@@ -30,7 +37,7 @@ const Progress = observer(class Progress extends React.Component<Props, {}> {
 		
 		return (
 			<div className={cn.join(' ')}>
-				<div className="inner">
+				<div id="inner" className="inner" onMouseDown={this.onDragStart}>
 					<Label text={text} />
 					{canCancel ? <Icon className="close" onClick={this.onCancel} /> : ''}
 					<div className="bar">
@@ -40,6 +47,7 @@ const Progress = observer(class Progress extends React.Component<Props, {}> {
 			</div>
 		);
 	};
+
 	
 	componentDidUpdate () {
 		const { progress } = commonStore;
@@ -49,6 +57,13 @@ const Progress = observer(class Progress extends React.Component<Props, {}> {
 
 		const { current, total } = progress;
 		const node = $(ReactDOM.findDOMNode(this));
+		const obj = node.find('#inner');
+		const coords = Storage.get('progress');
+
+		if (coords) {
+			const { x, y } = this.checkCoords(coords.x, coords.y);
+			this.setStyle(obj, x, y);
+		};
 		
 		node.removeClass('hide');
 		
@@ -63,6 +78,49 @@ const Progress = observer(class Progress extends React.Component<Props, {}> {
 		const { id } = progress;
 		
 		C.ProcessCancel(id);
+	};
+
+	onDragStart (e: any) {
+		const win = $(window);
+		const node = $(ReactDOM.findDOMNode(this));
+		const obj = node.find('#inner');
+		const offset = obj.offset();
+
+		this.obj = obj;
+		this.dx = e.pageX - offset.left;
+		this.dy = e.pageY - offset.top;
+		this.width = obj.width();
+		this.height = obj.height();
+
+		win.unbind('mousemove.progress mouseup.progress');
+		win.on('mousemove.progress', (e: any) => { this.onDragMove(e); });
+		win.on('mouseup.progress', (e: any) => { this.onDragEnd(e); });
+	};
+
+	onDragMove (e: any) {
+		const { x, y } = this.checkCoords(e.pageX - this.dx, e.pageY - this.dy);
+
+		this.setStyle(this.obj, x, y);
+		Storage.set('progress', { x, y });
+	};
+
+	onDragEnd (e: any) {
+		$(window).unbind('mousemove.progress mouseup.progress');
+	};
+
+	checkCoords (x: number, y: number): { x: number, y: number } {
+		const win = $(window);
+
+		x = Math.max(0, x);
+		x = Math.min(win.width() - this.width, x);
+		y = Math.max(Util.sizeHeader(), y);
+		y = Math.min(win.height() - this.height, y);
+
+		return { x, y };
+	};
+
+	setStyle (obj: any, x: number, y: number) {
+		obj.css({ margin: 0, left: x, top: y });
 	};
 	
 });
