84687cd9b20bc4e258923e84602d6e5d4bc4517e
Andrew Simachev <andrew.simachev@gmail.com>
2021-09-03 14:04:19 +0300

    experimental flag

--- src/ts/lib/datautil.tsx
+++ src/ts/lib/datautil.tsx
@@ -604,12 +604,15 @@ class DataUtil {
 			{ type: I.BlockType.File, id: I.FileType.File, icon: 'file', lang: 'File' },
 			{ type: I.BlockType.File, id: I.FileType.Image, icon: 'image', lang: 'Image' },
 			{ type: I.BlockType.File, id: I.FileType.Video, icon: 'video', lang: 'Video' },
-			{ type: I.BlockType.File, id: I.FileType.Audio, icon: 'audio', lang: 'Audio' },
 			{ type: I.BlockType.Bookmark, id: 'bookmark', icon: 'bookmark', lang: 'Bookmark' },
 			{ type: I.BlockType.Text, id: I.TextStyle.Code, icon: 'code', lang: 'Code' },
 		];
+
 		if (config.experimental) {
-			ret.push({ type: I.BlockType.Latex, id: I.BlockType.Latex, icon: 'latex', lang: 'Latex' });
+			ret = ret.concat([
+				{ type: I.BlockType.File, id: I.FileType.Audio, icon: 'audio', lang: 'Audio' },
+				{ type: I.BlockType.Latex, id: I.BlockType.Latex, icon: 'latex', lang: 'Latex' }
+			]);
 		};
 
 		return ret.map(this.menuMapperBlock);
