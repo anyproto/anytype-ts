2cdfdd9c5cd7fc0f42e02cc771781e51681d870f
Andrew Simachev <andrew.simachev@gmail.com>
2021-09-14 14:28:24 +0300

    #mxd9d9: Code snippet enter

--- src/ts/component/block/text.tsx
+++ src/ts/component/block/text.tsx
@@ -470,7 +470,7 @@ const BlockText = observer(class BlockText extends React.Component<Props, {}> {
 		const menuOpenSmile = menuStore.isOpen('smile');
 
 		keyboard.shortcut('enter', e, (pressed: string) => {
-			if (block.isTextCode() || menuOpen) {
+			if (menuOpen) {
 				return;
 			};
 
