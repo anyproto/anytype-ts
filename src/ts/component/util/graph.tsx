8f7dc3002e092d8c26a97e7e95ae0e6b184626af
Andrew Simachev <andrew.simachev@gmail.com>
2021-10-08 19:02:47 +0300

    fix

--- src/ts/component/util/graph.tsx
+++ src/ts/component/util/graph.tsx
@@ -123,7 +123,7 @@ const Graph = observer(class Graph extends React.Component<Props, {}> {
 			d.layout = Number(d.layout) || 0;
 			d.name = d.name || translate('defaultNamePage');
 			d.shortName = Util.shorten(d.name, 16);
-			d.radius = Math.max(3, Math.min(8, sourceCnt));
+			d.radius = Math.max(3, Math.min(10, sourceCnt + targetCnt));
 			d.isRoot = d.id == rootId;
 			d.isOrphan = !targetCnt && !sourceCnt;
 			d.src = this.imageSrc(d);
