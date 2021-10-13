e876975151129d8470d0abd294cb0f54f7c076a4
Andrew Simachev <andrew.simachev@gmail.com>
2021-10-13 17:53:08 +0300

    fix media

--- src/ts/lib/datautil.tsx
+++ src/ts/lib/datautil.tsx
@@ -699,6 +699,7 @@ class DataUtil {
 		
 		if (hasFile) {
 			items.push({ id: 'download', icon: 'download', name: 'Download' });
+			items.push({ id: 'openFileAsObject', icon: 'expand', name: 'Open as object' });
 			//items.push({ id: 'rename', icon: 'rename', name: 'Rename' });
 			//items.push({ id: 'replace', icon: 'replace', name: 'Replace' });
 		};
