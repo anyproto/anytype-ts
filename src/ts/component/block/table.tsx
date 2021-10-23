6fe04902cbd4ac2be57d50e26d9c82124a92b2d2
Andrew Simachev <andrew.simachev@gmail.com>
2021-10-23 17:15:00 +0300

    adding columns

--- src/ts/component/block/table.tsx
+++ src/ts/component/block/table.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { RouteComponentProps } from 'react-router';
 import { I, C, keyboard } from 'ts/lib';
 import { observer } from 'mobx-react';
+import { Icon } from 'ts/component';
 import { getRange, setRange } from 'selection-ranges';
 
 interface Props extends I.BlockComponent, RouteComponentProps<any> {};
@@ -40,9 +41,10 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 
 	render () {
 		const { readonly, block } = this.props;
-		const { content } = block;
-		const { columns, rows } = content;
+		const { columns, rows } = block.content;
 		const cn = [ 'wrap', 'focusable', 'c' + block.id ];
+		const cl = columns.length;
+		const cr = rows.length;
 
 		let Editor = null;
 		if (readonly) {
@@ -78,11 +80,12 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 			<thead>
 				<tr>
 					{columns.map((item: any, i: number) => (
-						<th key={i}>
+						<th key={i} style={{ width: (1 / cl) * 100 + '%' }}>
 							<Editor 
 								id={[ Key.Column, 0, i ].join('-')} 
 								value={item.name} 
 							/>
+							<Icon className="plus" onClick={() => { this.columnAdd(i); }} />
 						</th>
 					))}
 				</tr>
@@ -188,7 +191,14 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 	};
 
 	onBlur () {
+		const { key, row, column } = this.focusObj;
+		const target = this.getTarget(key, row, column);
+		const value = this.getValue(target);
+
+		window.clearTimeout(this.timeout);
 		keyboard.setFocus(false);
+
+		this.saveValue(key, row, column, value);
 	};
 
 	onSelect (e: any) {
@@ -201,9 +211,9 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 	onKeyDown (e: any) {
 		const { block } = this.props;
 		const { columns, rows } = block.content;
-		const target = $(e.currentTarget);
-		const value = this.getValue(target);
 		const { key, row, column, range } = this.focusObj;
+		const target = this.getTarget(key, row, column);
+		const value = this.getValue(target);
 
 		const isFirstCol = column == 0;
 		const isLastCol = column == columns.length - 1;
@@ -288,18 +298,16 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 			keyboard.shortcut('enter', e, (pressed: string) => {
 				e.preventDefault();
 
-				this.save(key, row, column, value);
+				this.saveValue(key, row, column, value);
 				this.rowAdd(row);
 			});
 		};
 	};
 
 	onKeyUp (e: any) {
-		const target = $(e.currentTarget);
+		const { key, row, column } = this.focusObj;
+		const target = this.getTarget(key, row, column);
 		const value = this.getValue(target);
-		const { key, row, column, range } = this.focusObj;
-
-		window.clearTimeout(this.timeout);
 
 		let ret = false;
 
@@ -307,17 +315,26 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 			ret = true;
 		});
 
-		if (ret) {
-			return;
+		window.clearTimeout(this.timeout);
+		if (!ret) {
+			this.timeout = window.setTimeout(() => { this.saveValue(key, row, column, value); }, 500);
 		};
-		
-		this.timeout = window.setTimeout(() => { this.save(key, row, column, value); }, 500);
 	};
 
-	addColumn (index: number) {
+	columnAdd (index: number) {
+		const { block } = this.props;
+		const { columns } = block.content;
+
+		columns.splice(index + 1, 0, { name: '' });
+
+		console.log(columns);
+
+		this.saveContent();
 	};
 
-	removeColumn (index: number) {
+	columnRemove (index: number) {
+
+		this.saveContent();
 	};
 
 	rowAdd (index: number) {
@@ -327,6 +344,8 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 
 		this.focusSet(Key.Row, index + 1, 0, { from: 0, to: 0 });
 		rows.splice(index + 1, 0, row);
+
+		this.saveContent();
 	};
 
 	rowRemove (index: number) {
@@ -342,6 +361,7 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 		};
 
 		rows.splice(index, 1);
+		this.saveContent();
 	};
 
 	fillRow (row: I.TableRow) {
@@ -356,7 +376,7 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 		return row;
 	};
 
-	save (key: string, row: number, column: number, value: string) {
+	saveValue (key: string, row: number, column: number, value: string) {
 		const { rootId, block } = this.props;
 		const { content } = block;
 
@@ -370,10 +390,14 @@ const BlockTable = observer(class BlockTable extends React.Component<Props, {}> 
 			content[key][row].data[column].value = value;
 		};
 
-		C.BlockUpdateContent({
-			...block,
-			content,
-		}, rootId, block.id);
+		C.BlockUpdateContent({ ...block, content }, rootId, block.id);
+	};
+
+	saveContent () {
+		const { rootId, block } = this.props;
+		const { content } = block;
+
+		C.BlockUpdateContent({ ...block }, rootId, block.id);
 	};
 
 	getValue (obj: any): string {
