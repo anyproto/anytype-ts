7a30743738feac6168f39c8acef0785721ae0468
Andrew Simachev <andrew.simachev@gmail.com>
2021-11-30 14:27:04 +0300

    PopupPrompt updates

--- src/ts/component/menu/block/more.tsx
+++ src/ts/component/menu/block/more.tsx
@@ -563,6 +563,10 @@ class MenuBlockMore extends React.Component<Props, {}> {
 							value: message.link,
 							readonly: true,
 							select: true,
+							textConfirm: 'Copy',
+							onChange: (v: string) => {
+								Util.clipboardCopy({ text: v });
+							}
 						}
 					});
 				});
--- src/ts/component/popup/confirm.tsx
+++ src/ts/component/popup/confirm.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Title, Icon, Label, Button } from 'ts/component';
 import { I, keyboard } from 'ts/lib';
 import { observer } from 'mobx-react';
+import { translate } from '../../lib';
 
 interface Props extends I.Popup {}
 
@@ -21,8 +22,8 @@ const PopupConfirm = observer(class PopupConfirm extends React.Component<Props, 
 		
 		const canConfirm = undefined === data.canConfirm ? true : data.canConfirm;
 		const canCancel = undefined === data.canCancel ? true : data.canCancel;
-		const textConfirm = data.textConfirm || 'Ok';
-		const textCancel = data.textCancel || 'Cancel';
+		const textConfirm = data.textConfirm || translate('commonOk');
+		const textCancel = data.textCancel || translate('commonCancle');
 		
 		return (
 			<React.Fragment>
