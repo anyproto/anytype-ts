0e4fdaa300dc996aaf5ca91cd3417c268dd84476
Andrew Simachev <andrew.simachev@gmail.com>
2022-01-14 12:21:25 +0300

    fix user icons font size in graph

--- dist/workers/graph.js
+++ dist/workers/graph.js
@@ -520,7 +520,7 @@ const isIconCircle = (d) => {
 
 const nameCircleIcon = (d) => {
 	ctx.save();
-	ctx.font = font;  
+	ctx.font = d.radius / 2 + ' ' + fontFamily;  
 	ctx.fillStyle = Color.iconText;
 	ctx.textAlign = 'center';
 	ctx.textBaseline = 'middle';
