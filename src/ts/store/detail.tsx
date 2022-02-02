63754fab1b9d3d2c80c97b67820d918bd4718485
Andrew Simachev <andrew.simachev@gmail.com>
2022-02-02 15:12:09 +0300

    optimization

--- src/ts/store/detail.tsx
+++ src/ts/store/detail.tsx
@@ -118,7 +118,7 @@ class DetailStore {
 			list = list.filter((it: Detail) => { return keys.includes(it.relationKey); });
 		};
 
-		let object: any = Object.fromEntries(list.map(it => [ it.relationKey, it.value ]));;
+		let object: any = Object.fromEntries(list.map(it => [ it.relationKey, it.value ]));
 		let layout = Number(object.layout) || I.ObjectLayout.Page;
 		let name = String(object.name || DataUtil.defaultName('page'));
 		let snippet = String(object.snippet || '').replace(/\n/g, ' ');
