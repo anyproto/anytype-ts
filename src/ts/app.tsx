66cc6c89eaea49fc2f16d7a23801e49f184ce94f
Andrew Simachev <andrew.simachev@gmail.com>
2022-03-11 11:21:24 +0300

    debounce writing event

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -15,6 +15,8 @@ configure({ enforceActions: 'never' });
 import 'react-virtualized/styles.css';
 import 'katex/dist/katex.min.css';
 
+import 'prismjs/themes/prism.css';
+
 import 'scss/font.scss';
 import 'scss/common.scss';
 import 'scss/debug.scss';
