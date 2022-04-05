8a0905cdd614b843845a14eacec739af2cc98400
Andrew Simachev <andrew.simachev@gmail.com>
2022-04-05 11:54:58 +0300

    toggles

--- dist/js/export.js
+++ dist/js/export.js
@@ -2,16 +2,27 @@ document.body.onload = function () {
     $(function () {
         var link = $('lnk');
         var a = $('a');
+		var block = $('.block');
     
-        link.unbind('click').on('click', function (e) {
+        link.off('click').on('click', function (e) {
             e.preventDefault();
             window.open($(this).attr('href'));
         });
     
-        a.unbind('click').on('click', function (e) {
+        a.off('click').on('click', function (e) {
             e.preventDefault();
             window.open($(this).attr('href'));
         });
+
+		block.each(function (i, item) {
+			item = $(item);
+
+			const toggle = item.find('.marker.toggle');
+
+			toggle.off('click').on('click', function (e) {
+				item.hasClass('isToggled') ? item.removeClass('isToggled') : item.addClass('isToggled');
+			});
+		});
     
     });
 };
