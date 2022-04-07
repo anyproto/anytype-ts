5686fe8f19e283c38b138c3fd0eca972ca5a6818
Andrew Simachev <andrew.simachev@gmail.com>
2022-04-07 13:16:29 +0300

    description logic changes

--- src/ts/component/block/link/card.tsx
+++ src/ts/component/block/link/card.tsx
@@ -8,7 +8,7 @@ interface Props extends I.BlockComponent, RouteComponentProps<any> {
     withName?: boolean;
     withIcon?: boolean;
     withCover?: boolean;
-    withDescription?: boolean;
+    description?: number;
     iconSize: number;
     object: any;
     className?: string;
@@ -26,13 +26,13 @@ Size[I.LinkIconSize.Medium] = 48;
 const LinkCard = observer(class LinkCard extends React.Component<Props, {}> {
 
 	render () {
-        const { rootId, block, withName, withIcon, withDescription, iconSize, object, className, canEdit, onClick, onSelect, onUpload, onCheckbox } = this.props;
+        const { rootId, block, withName, withIcon, description, iconSize, object, className, canEdit, onClick, onSelect, onUpload, onCheckbox } = this.props;
         const { id, layout, coverType, coverId, coverX, coverY, coverScale, snippet } = object;
         const { bgColor } = block;
         const cn = [ 'linkCard', DataUtil.layoutClass(id, layout), 'c' + Size[iconSize] ];
         const cns = [ 'sides' ];
-        const featured: any = new M.Block({ id: rootId + '-featured', type: I.BlockType.Featured, childrenIds: [], fields: {}, content: {} });
         const withCover = this.props.withCover && coverId && coverType;
+		const canDescription = ![ I.ObjectLayout.Note ].includes(object.layout);
 
         if (className) {
             cn.push(className);
@@ -44,9 +44,17 @@ const LinkCard = observer(class LinkCard extends React.Component<Props, {}> {
         if (bgColor) {
 			cns.push('bgColor bgColor-' + bgColor);
 		};
-        if (!withIcon && !withName && !withDescription) {
+        if (!withIcon && !withName && (description == I.LinkDescription.None)) {
             cns.push('hidden');
         };
+
+		let descr = '';
+		if (description == I.LinkDescription.Added) {
+			descr = canDescription ? object.description : '';
+		};
+		if (description == I.LinkDescription.Content) {
+			descr = canDescription ? (object.description || object.snippet) : '';
+		};
 
 		return (
 			<div className={cn.join(' ')} onMouseDown={onClick}>
@@ -67,10 +75,8 @@ const LinkCard = observer(class LinkCard extends React.Component<Props, {}> {
 								) : ''}
 								{withName ? <ObjectName object={object} /> : ''}
 							</div>
-							{withDescription ? <ObjectDescription className="cardDescription" object={object} /> : ''}
+							{descr ? <div className="cardDescription">{descr}</div> : ''}
 							<div className="archive">{translate('blockLinkArchived')}</div>
-
-							{/*<Block {...this.props} rootId={block.content.targetBlockId} iconSize={18} block={featured} readonly={true} className="noPlus" />*/}
 						</div>
 					</div>
 					{withCover ? (
