418f075e6f5277d94ae4076d43b68326bf9efbeb
Andrew Simachev <andrew.simachev@gmail.com>
2022-07-11 16:04:49 +0300

    fix

--- src/ts/lib/command.tsx
+++ src/ts/lib/command.tsx
@@ -888,7 +888,7 @@ const HistoryShowVersion = (pageId: string, versionId: string, callBack?: (messa
 };
 
 const HistorySetVersion = (pageId: string, versionId: string, callBack?: (message: any) => void) => {
-	const request = new Rpc.History.Show.Request();
+	const request = new Rpc.History.SetVersion.Request();
 	
 	request.setPageid(pageId);
 	request.setVersionid(versionId);
