94bcbff3d607cecdcddc5fa7ef82f0c95fccb994
Andrew Simachev <andrew.simachev@gmail.com>
2022-07-23 13:51:08 +0300

    fix block cover menu logic

--- src/ts/component/menu/block/cover.tsx
+++ src/ts/component/menu/block/cover.tsx
@@ -199,7 +199,7 @@ const MenuBlockCover = observer(class MenuBlockCover extends React.Component<Pro
 				const filters: I.Filter[] = [
 					{ operator: I.FilterOperator.And, relationKey: 'workspaceId', condition: I.FilterCondition.Empty, value: null },
 					{ operator: I.FilterOperator.And, relationKey: 'type', condition: I.FilterCondition.Equal, value: Constant.typeId.image },
-					{ operator: I.FilterOperator.And, relationKey: 'widthInPixels', condition: I.FilterCondition.GreaterOrEqual, value: Constant.size.cover },
+					{ operator: I.FilterOperator.And, relationKey: 'widthInPixels', condition: I.FilterCondition.GreaterOrEqual, value: 1600 },
 					{ operator: I.FilterOperator.And, relationKey: 'isHidden', condition: I.FilterCondition.Equal, value: false },
 					{ operator: I.FilterOperator.And, relationKey: 'isArchived', condition: I.FilterCondition.Equal, value: false },
 					{ operator: I.FilterOperator.And, relationKey: 'isDeleted', condition: I.FilterCondition.Equal, value: false },
