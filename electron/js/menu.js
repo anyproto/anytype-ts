d131da23acff3a275c21203ae44b7c1fc8347721
Andrew Simachev <andrew.simachev@gmail.com>
2022-09-23 15:06:07 +0300

    JS-597

--- electron/js/menu.js
+++ electron/js/menu.js
@@ -74,8 +74,8 @@ class MenuManager {
 
 					Separator,
 
-					{ label: 'Object diagnostics', click: () => { Util.send(this.win, 'command', 'debugSync'); } },
-					{ label: 'Tree diagnostics', click: () => { this.win.show(); Util.send(this.win, 'command', 'debugTree'); } },
+					{ label: 'Profile debug', click: () => { Util.send(this.win, 'command', 'debugSync'); } },
+					{ label: 'Current object debug', click: () => { this.win.show(); Util.send(this.win, 'command', 'debugTree'); } },
 
 					Separator,
 
