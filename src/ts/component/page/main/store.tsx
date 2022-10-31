ed14e07426017a43978a3a087425837ec80c3384
Andrew Simachev <andrew.simachev@gmail.com>
2022-10-31 13:06:43 +0300

    store scroll fixes

--- src/ts/component/page/main/store.tsx
+++ src/ts/component/page/main/store.tsx
@@ -467,7 +467,7 @@ const PageMainStore = observer(class PageMainStore extends React.Component<Props
 		let wh = isPopup ? container.height() - hh : win.height();
 
 		if (platform == I.Platform.Windows) {
-			wh += 30;
+			wh -= 30;
 		};
 
 		wrapper.css({ height: wh });
