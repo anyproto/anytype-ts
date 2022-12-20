9a63aac798827f79ad02e4ce0040bf6d6e1696e6
Andrew Simachev <andrew.simachev@gmail.com>
2022-12-20 13:09:26 +0300

    JS-1034: export options

--- src/ts/component/popup/page/settings/export/markdown.tsx
+++ src/ts/component/popup/page/settings/export/markdown.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Title, Label, Button, Switch, Storage } from 'Component';
-import { I, translate } from 'Lib';
+import { Title, Label, Button, Switch } from 'Component';
+import { I, translate, Storage } from 'Lib';
 import { observer } from 'mobx-react';
 
 import Head from '../head';
