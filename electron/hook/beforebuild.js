f767c0fa02499917fd7ec091aee2fda419026c6d
Roman Khafizianov <requilence@gmail.com>
2022-12-22 20:40:52 +0100

    linux arm support

--- electron/hook/beforebuild.js
+++ electron/hook/beforebuild.js
@@ -4,12 +4,15 @@ exports.default = async function(context){
     const { platform, arch } = context;
 
     console.log('Build BeforeBuild', platform.name, arch);
+	let folder = "";
+	if (platform.name == 'mac') {
+		folder = arch == 'arm64' ? 'darwin-arm' : 'darwin-amd';
+	} else if (platform.name == 'linux') {
+		folder = arch == 'arm64' ? 'linux-arm' : 'linux-amd';
+	} else {
+		return
+	}
 
-	if (platform.name !== 'mac') {
-		return;
-	};
-
-    const folder = arch == 'arm64' ? 'darwin-arm' : 'darwin-amd';
 	if (fs.existsSync('./' + folder)) {
 		fs.renameSync('./' + folder, './build', function (err) {
 		});
