55ee7063685abf672e146b8e38e407313dba2e43
Mike Mhlv <mhlv@anytype.io>
2023-01-11 14:29:32 +0100

    JS-1075: settings username

--- src/ts/component/popup/page/settings/account.tsx
+++ src/ts/component/popup/page/settings/account.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import {Icon, Title, Label, IconObject} from 'Component';
-import {I, C, Storage, translate, Util, analytics, ObjectUtil} from 'Lib';
-import {authStore, blockStore, commonStore, detailStore, menuStore, popupStore} from 'Store';
+import { Icon, Title, Label } from 'Component';
+import { I, C, Storage, translate, Util, analytics } from 'Lib';
+import { authStore, blockStore, commonStore, detailStore, popupStore } from 'Store';
 import { observer } from 'mobx-react';
 
 import Head from './head';
+import UserInfo from './userinfo';
 import Constant from "json/constant.json";
 
 interface Props extends I.Popup, RouteComponentProps<any> {
@@ -32,8 +33,6 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 	constructor (props: any) {
 		super(props);
 
-		this.onUserpic = this.onUserpic.bind(this);
-		this.onUserpicUpload = this.onUserpicUpload.bind(this);
 		this.onLogout = this.onLogout.bind(this);
 		this.onFileOffload = this.onFileOffload.bind(this);
 		this.onDelete = this.onDelete.bind(this);
@@ -58,9 +57,7 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 
 				{error ? <div className="message">{error}</div> : ''}
 
-				<div className="userInfo">
-					<IconObject id="userInfoUserpic" object={profile} size={80} onClick={this.onUserpic} />
-				</div>
+				<UserInfo {...this.props} profile={profile} />
 
 				<div className="rows">
 					<Label className="sectionName" text="Access" />
@@ -107,62 +104,6 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 				</div>
 			</div>
 		);
-	};
-
-	onUserpic () {
-		const object = detailStore.get(Constant.subId.profile, blockStore.profile);
-		const options = [
-			{ id: 'upload', name: 'Change' },
-		];
-
-		if (object.iconImage) {
-			options.push({ id: 'remove', name: 'Remove' });
-		};
-
-		menuStore.open('select', {
-			element: `#userInfoUserpic`,
-			horizontal: I.MenuDirection.Center,
-			data: {
-				value: '',
-				options: options,
-				onSelect: (event: any, item: any) => {
-					if (item.id == 'remove') {
-						ObjectUtil.setIcon(object.id, '', '');
-					};
-					if (item.id == 'upload') {
-						this.onUserpicUpload(object.id);
-					};
-				},
-			}
-		});
-	};
-
-	onUserpicUpload (rootId) {
-		const { setLoading } = this.props;
-
-		const options: any = {
-			properties: [ 'openFile' ],
-			filters: [ { name: '', extensions: Constant.extension.cover } ]
-		};
-
-		window.Electron.showOpenDialog(options).then((result: any) => {
-			const files = result.filePaths;
-			if ((files == undefined) || !files.length) {
-				return;
-			};
-
-			setLoading(true);
-
-			C.FileUpload('', files[0], I.FileType.Image, (message: any) => {
-				if (message.error.code) {
-					return;
-				};
-
-				ObjectUtil.setIcon(rootId, '', message.hash, () => {
-					setLoading(false);
-				});
-			});
-		});
 	};
 
 	onLogout (e: any) {
