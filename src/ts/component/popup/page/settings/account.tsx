41449a82048dc0c38a0354d24f5d4bf0457ee3b4
Mike Mhlv <mhlv@anytype.io>
2023-01-11 13:07:14 +0100

    JS-1075: settings userpic

--- src/ts/component/popup/page/settings/account.tsx
+++ src/ts/component/popup/page/settings/account.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Icon, Title, Label } from 'Component';
-import { I, C, Storage, translate, Util, analytics } from 'Lib';
-import { authStore, commonStore, popupStore } from 'Store';
+import {Icon, Title, Label, IconObject} from 'Component';
+import {I, C, Storage, translate, Util, analytics, ObjectUtil} from 'Lib';
+import {authStore, blockStore, commonStore, detailStore, menuStore, popupStore} from 'Store';
 import { observer } from 'mobx-react';
 
 import Head from './head';
+import Constant from "json/constant.json";
 
 interface Props extends I.Popup, RouteComponentProps<any> {
 	prevPage: string;
@@ -31,6 +32,8 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 	constructor (props: any) {
 		super(props);
 
+		this.onUserpic = this.onUserpic.bind(this);
+		this.onUserpicUpload = this.onUserpicUpload.bind(this);
 		this.onLogout = this.onLogout.bind(this);
 		this.onFileOffload = this.onFileOffload.bind(this);
 		this.onDelete = this.onDelete.bind(this);
@@ -46,6 +49,7 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 		const pin = Storage.get('pin');
 		const canDelete = account.status.type == I.AccountStatusType.Active;
 		const canMove = config.experimental;
+		const profile = detailStore.get(Constant.subId.profile, blockStore.profile);
 
 		return (
 			<div>
@@ -54,7 +58,13 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 
 				{error ? <div className="message">{error}</div> : ''}
 
+				<div className="userInfo">
+					<IconObject id="userInfoUserpic" object={profile} size={80} onClick={this.onUserpic} />
+				</div>
+
 				<div className="rows">
+					<Label className="sectionName" text="Access" />
+
 					<div className="row" onClick={() => { onPage('phrase'); }}>
 						<Icon className="phrase" />
 						<Label text={translate('popupSettingsPhraseTitle')} />
@@ -65,7 +75,7 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 						<Icon className="pin" />
 						<Label text={translate('popupSettingsPinTitle')} />
 						<div className="status">
-							{pin ? 'On' : 'Off'}
+							{pin ? 'on' : 'off'}
 						</div>
 						<Icon className="arrow" />
 					</div>
@@ -97,6 +107,62 @@ const PopupSettingsPageAccount = observer(class PopupSettingsPageAccount extends
 				</div>
 			</div>
 		);
+	};
+
+	onUserpic () {
+		const object = detailStore.get(Constant.subId.profile, blockStore.profile);
+		const options = [
+			{ id: 'upload', name: 'Change' },
+		];
+
+		if (object.iconImage) {
+			options.push({ id: 'remove', name: 'Remove' });
+		};
+
+		menuStore.open('select', {
+			element: `#userInfoUserpic`,
+			horizontal: I.MenuDirection.Center,
+			data: {
+				value: '',
+				options: options,
+				onSelect: (event: any, item: any) => {
+					if (item.id == 'remove') {
+						ObjectUtil.setIcon(object.id, '', '');
+					};
+					if (item.id == 'upload') {
+						this.onUserpicUpload(object.id);
+					};
+				},
+			}
+		});
+	};
+
+	onUserpicUpload (rootId) {
+		const { setLoading } = this.props;
+
+		const options: any = {
+			properties: [ 'openFile' ],
+			filters: [ { name: '', extensions: Constant.extension.cover } ]
+		};
+
+		window.Electron.showOpenDialog(options).then((result: any) => {
+			const files = result.filePaths;
+			if ((files == undefined) || !files.length) {
+				return;
+			};
+
+			setLoading(true);
+
+			C.FileUpload('', files[0], I.FileType.Image, (message: any) => {
+				if (message.error.code) {
+					return;
+				};
+
+				ObjectUtil.setIcon(rootId, '', message.hash, () => {
+					setLoading(false);
+				});
+			});
+		});
 	};
 
 	onLogout (e: any) {
