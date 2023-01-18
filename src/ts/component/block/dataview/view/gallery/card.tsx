8af5eeb0a34c31ec93e083ebbab9613624211010
Mike Mhlv <mhlv@anytype.io>
2023-01-18 15:58:21 +0100

    JS-1151: interaction with controls in a card cover

--- src/ts/component/block/dataview/view/gallery/card.tsx
+++ src/ts/component/block/dataview/view/gallery/card.tsx
@@ -157,7 +157,7 @@ const Card = observer(class Card extends React.Component<Props> {
 			return;
 		};
 
-		if ($(e.target).hasClass('play')) {
+		if ($(e.target).parents('.audioControls').length) {
 			return;
 		};
 
