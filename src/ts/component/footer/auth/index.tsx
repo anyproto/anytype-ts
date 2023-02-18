39db05dc88bc8aae3980d3c52c96f359cb82f48b
Andrew Simachev <andrew.simachev@gmail.com>
2023-02-18 13:52:55 +0300

    onboarding styles

--- src/ts/component/footer/auth/index.tsx
+++ src/ts/component/footer/auth/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import { Icon } from 'Component';
 import { I, Util } from 'Lib';
 
 class FooterAuthIndex extends React.Component<I.FooterComponent> {
 	
 	render () {
-		const { onHelp } = this.props;
-
 		return (
 			<React.Fragment>
 				<div className="copy">{Util.date('Y', Util.time())}, Anytype</div>
-				<Icon id="button-help" className="help big" tooltip="Help" tooltipY={I.MenuDirection.Top} onClick={onHelp} />
 			</React.Fragment>
 		);
 	};
