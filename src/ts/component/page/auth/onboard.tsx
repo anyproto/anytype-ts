c1076d36c9962b7f84ee1a01f56f9af456a3d271
Dragos Rotaru <dragos@rotaru.co>
2023-02-24 17:57:05 +0700

    completed basic flow

--- src/ts/component/page/auth/onboard.tsx
+++ src/ts/component/page/auth/onboard.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
 import { Frame, Title, Label, Button, Header, Footer, DotIndicator } from 'Component';
-import { I, translate, Animation, Util } from 'Lib';
+import { I, translate, Animation, } from 'Lib';
 import { observer } from 'mobx-react';
 
+enum OnboardStage {
+	VOID = 0,
+	KEY_PHRASE = 1,
+	OFFLINE = 2,
+	SOUL = 3,
+	SOUL_CREATING = 4,
+	SPACE_CREATING = 5,
+
+}
+
 type State = {
-	index: number;
+	stage: OnboardStage;
+	keyPhraseCopied: boolean;
 }
 
 const PageAuthOnboard = observer(class PageAuthOnboard extends React.Component<I.PageComponent, State> {
 	state: State = {
-		index: 0,
+		stage: OnboardStage.VOID,
+		keyPhraseCopied: false,
 	}
 
 	constructor (props: I.PageComponent) {
@@ -18,18 +30,43 @@ const PageAuthOnboard = observer(class PageAuthOnboard extends React.Component<I
 	};
 	
 	render () {
+		const { stage } = this.state;
+
+		// Mapping the Stages to text.json keys
+		const stageNameMap = {
+			[OnboardStage.VOID]: "Void",
+			[OnboardStage.KEY_PHRASE]: "KeyPhrase",
+			[OnboardStage.OFFLINE]: "Offline",
+			[OnboardStage.SOUL]: "Soul",
+			[OnboardStage.SOUL_CREATING]: "SoulCreating",
+			[OnboardStage.SPACE_CREATING]: "SpaceCreating",
+		}
+
+		const title = <Title className="animation" text={translate(`authOnboard${stageNameMap[stage]}Title`)} />;
+		let label = <Label className="animation" text={translate(`authOnboard${stageNameMap[stage]}Label`)} />;
+		let submit = <Button text={translate(`authOnboard${stageNameMap[stage]}Submit`)} onClick={this.onNext} />;
+		let dotIndicator = <DotIndicator activeIndex={this.state.stage} count={4} />;
+
+		if (stage === OnboardStage.VOID || (stage === OnboardStage.KEY_PHRASE && this.state.keyPhraseCopied)) {
+			submit = <Button text={translate(`authOnboardSubmit`)} onClick={this.onNext} />;
+		}
+		if (stage === OnboardStage.SOUL_CREATING || stage === OnboardStage.SPACE_CREATING) {
+			label = null;
+			submit = null;
+			dotIndicator = null;
+		}
+
         return (
 			<div>
 				<Header {...this.props} component="authIndex" />
 				<Footer {...this.props} component="authIndex" />
 				<Frame>
-					<DotIndicator activeIndex={0} count={4} />
-					<Title className="animation" text={translate('authOnboardVoidTitle')} />
-					<Label className="animation" text={translate('authOnboardVoidLabel')} />
-								
+					{dotIndicator}
+					{title}
+					{label}	
 					<div className="buttons">
 						<div className="animation">
-							<Button text={translate('authOnboardNext')} onClick={this.onNext} />
+							{submit}
 						</div>
 					</div>
 				</Frame>
@@ -41,8 +78,12 @@ const PageAuthOnboard = observer(class PageAuthOnboard extends React.Component<I
 		Animation.to();
 	};
 
+	componentDidUpdate(): void {
+		Animation.to();
+	}
+
 	onNext () {
-		Animation.from(() => { Util.route('/auth/onboard/encrypt') });
+		Animation.from(() => { this.setState(prev => ({ ...prev, stage: prev.stage + 1 })) });
 	}
 });
 
