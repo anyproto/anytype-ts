a06ebfcaf010e72049275108ad7b9bff14cd497e
Dragos Rotaru <dragos@rotaru.co>
2023-03-14 21:13:03 +0800

    removed success page

--- src/ts/component/page/index.tsx
+++ src/ts/component/page/index.tsx
@@ -14,7 +14,6 @@ import PageAuthLogin from './auth/login';
 import PageAuthPinCheck from './auth/pinCheck';
 import PageAuthSetup from './auth/setup';
 import PageAuthAccountSelect from './auth/accountSelect';
-import PageAuthSuccess from './auth/success';
 import PageAuthOnboard from './auth/onboard';
 import PageAuthShare from './auth/share';
 import PageAuthDeleted from './auth/deleted';
@@ -43,7 +42,6 @@ const Components: { [key: string]: any } = {
 	'auth/pin-check':		 PageAuthPinCheck,
 	'auth/setup':			 PageAuthSetup,
 	'auth/account-select':	 PageAuthAccountSelect,
-	'auth/success':			 PageAuthSuccess,
 	'auth/onboard':			 PageAuthOnboard,
 	'auth/share':			 PageAuthShare,
 	'auth/deleted':			 PageAuthDeleted,
