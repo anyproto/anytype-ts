8b3fb013bd39c24c9ecebfff1b30c915f4de5610
Andrew Simachev <andrew.simachev@gmail.com>
2023-03-28 11:55:42 +0300

    JS-1690: fix

--- src/ts/component/block/index.tsx
+++ src/ts/component/block/index.tsx
@@ -67,7 +67,7 @@ const Block = observer(class Block extends React.Component<Props> {
 	};
 
 	render () {
-		const { rootId, css, className, block, readonly, isInsideTable } = this.props;
+		const { rootId, css, className, block, readonly, isInsideTable, index } = this.props;
 		const { id, type, fields, content, hAlign, bgColor } = block;
 
 		if (!id) {
@@ -80,7 +80,7 @@ const Block = observer(class Block extends React.Component<Props> {
 		let canSelect = !isInsideTable;
 		let canDrop = !readonly && !isInsideTable;
 		let canDropMiddle = false;
-		let cn: string[] = [ 'block', DataUtil.blockClass(block), 'align' + hAlign ];
+		let cn: string[] = [ 'block', DataUtil.blockClass(block), 'align' + hAlign, 'index' + index ];
 		let cd: string[] = [ 'wrapContent' ];
 		let blockComponent = null;
 		let empty = null;
