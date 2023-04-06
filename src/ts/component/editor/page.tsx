e5dda1f01cdde0fcb1cce207800af983dfbe2771
Andrew Simachev <andrew.simachev@gmail.com>
2023-04-06 18:08:16 +0300

    JS-1795: fix

--- src/ts/component/editor/page.tsx
+++ src/ts/component/editor/page.tsx
@@ -1593,7 +1593,7 @@ const EditorPage = observer(class EditorPage extends React.Component<Props> {
 		blocks = blocks.map((it: I.Block) => {
 			const element = blockStore.getMapElement(rootId, it.id);
 
-			if (it.isText()) {
+			if (it.type == I.BlockType.Text) {
 				text.push(String(it.content.text || ''));
 			};
 
