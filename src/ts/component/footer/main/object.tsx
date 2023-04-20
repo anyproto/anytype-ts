360d2351910b35ce59a5f0ab1abf93f8a1e6c176
Andrew Simachev <andrew.simachev@gmail.com>
2023-04-20 15:36:15 +0200

    JS-1874: fix

--- src/ts/component/footer/main/object.tsx
+++ src/ts/component/footer/main/object.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Icon } from 'Component';
-import { I, keyboard } from 'Lib';
+import { I, keyboard, Preview } from 'Lib';
 
 const FooterMainEdit = class FooterMainEdit extends React.Component<I.FooterComponent> {
 	
@@ -10,23 +10,34 @@ const FooterMainEdit = class FooterMainEdit extends React.Component<I.FooterComp
 
 		return (
 			<React.Fragment>
-				<Icon
-					id="button-add"
-					className="big"
-					tooltip="Add new object"
-					tooltipY={I.MenuDirection.Top} 
-					tooltipCaption={`${cmd} + N`}
+				<div 
+					id="button-add" 
+					className="iconWrap" 
 					onClick={onAdd}
-				/>
-				<Icon 
+					onMouseEnter={e => this.onTooltipShow(e, 'Add new object', `${cmd} + N`)}
+				>
+					<Icon />
+					<div className="bg" />
+				</div>
+
+				<div 
 					id="button-help" 
-					className="big help" 
-					tooltip="Help" 
-					tooltipY={I.MenuDirection.Top} 
-					onClick={onHelp} 
-				/>
+					className="iconWrap" 
+					onClick={onAdd}
+					onMouseEnter={e => this.onTooltipShow(e, 'Help')}
+				>
+					<Icon />
+					<div className="bg" />
+				</div>
 			</React.Fragment>
 		);
+	};
+
+	onTooltipShow (e: any, text: string, caption?: string) {
+		const t = Preview.tooltipCaption(text, caption);
+		if (t) {
+			Preview.tooltipShow({ text: t, element: $(e.currentTarget), typeY: I.MenuDirection.Top });
+		};
 	};
 
 };
