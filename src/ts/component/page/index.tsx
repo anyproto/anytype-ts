8f55c320044a6ce43ac96b537bdd94c4e7ab7b0c
Mike Mhlv <mhlv@anytype.io>
2023-04-26 15:09:48 +0100

    migration popup hidden

--- src/ts/component/page/index.tsx
+++ src/ts/component/page/index.tsx
@@ -255,7 +255,8 @@ const Page = observer(class Page extends React.Component<I.PageComponent> {
 			return;
 		};
 
-		popupStore.open('migration', { data: { type: 'onboarding' } });
+		// popupStore.open('migration', { data: { type: 'onboarding' } });
+		Onboarding.start('dashboard', false);
 	};
 
 	unbind () {
