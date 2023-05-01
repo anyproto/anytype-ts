a937b06833a8b0a10ef327fe287501dd7b584186
Mike Mhlv <mhlv@anytype.io>
2023-05-01 16:35:26 +0100

    JS-1945: progress bar and file offload

--- src/ts/component/popup/page/settings/storage/index.tsx
+++ src/ts/component/popup/page/settings/storage/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import { Title, Label, IconObject, ObjectName, Button } from 'Component';
-import { analytics, I, translate } from 'Lib';
+import { analytics, C, I, translate } from 'Lib';
 import { observer } from 'mobx-react';
-import { commonStore, detailStore } from 'Store';
+import { commonStore, detailStore, popupStore } from 'Store';
 import Constant from "json/constant.json";
 
-const PopupSettingsPageStorageIndex = observer(class PopupSettingsPageStorageIndex extends React.Component<I.PopupSettings> {
+interface Props extends I.PopupSettings {
+    onPage: (id: string) => void;
+    setLoading: (v: boolean) => void;
+};
+
+const PopupSettingsPageStorageIndex = observer(class PopupSettingsPageStorageIndex extends React.Component<Props, {}> {
+
+    constructor (props: Props) {
+        super(props);
+
+        this.onManageFiles = this.onManageFiles.bind(this);
+        this.onFileOffload = this.onFileOffload.bind(this);
+    };
 
     render () {
         const space = detailStore.get(Constant.subId.space, commonStore.workspace);
@@ -26,6 +38,8 @@ const PopupSettingsPageStorageIndex = observer(class PopupSettingsPageStorageInd
                     <Button className="c28 outlined" text={translate('popupSettingsStorageIndexManageFiles')} onClick={this.onManageFiles} />
                 </div>
 
+                <div className="progressBar"><div className="progressBarFill" style={{ width: '25%' }} /></div>
+
                 <Title className="sub" text={translate('popupSettingsStorageIndexLocalStorageTitle')} />
                 <Label className="description" text={translate('popupSettingsStorageIndexLocalStorageText')} />
 
@@ -37,21 +51,53 @@ const PopupSettingsPageStorageIndex = observer(class PopupSettingsPageStorageInd
                             <div className="type">{translate(`popupSettingsStorageIndexLocalStorageUsage`)}</div>
                         </div>
                     </div>
-                    <Button className="c28 outlined" text={translate('popupSettingsStorageIndexOffloadFiles')} onClick={this.onOffloadFiles} />
+                    <Button className="c28 outlined" text={translate('popupSettingsStorageIndexOffloadFiles')} onClick={this.onFileOffload} />
                 </div>
 
             </React.Fragment>
         );
     };
 
-    onManageFiles = () => {
+    onManageFiles () {
         const { onPage } = this.props;
 
         onPage('storageManager');
     };
 
-    onOffloadFiles () {
-        console.log('OFFLOAD')
+    onFileOffload (e: any) {
+        const { setLoading } = this.props;
+
+        analytics.event('ScreenFileOffloadWarning');
+
+        popupStore.open('confirm',{
+            data: {
+                title: 'Are you sure?',
+                text: 'All media files will be deleted from your current device. They can be downloaded again from a backup node or another device.',
+                textConfirm: 'Yes',
+                onConfirm: () => {
+                    setLoading(true);
+
+                    C.FileListOffload([], false, (message: any) => {
+                        setLoading(false);
+
+                        if (message.error.code) {
+                            return;
+                        };
+
+                        popupStore.open('confirm',{
+                            data: {
+                                title: 'Files offloaded',
+                                //text: Util.sprintf('Files: %s, Size: %s', message.files, FileUtil.size(message.bytes)),
+                                textConfirm: 'Ok',
+                                canCancel: false,
+                            }
+                        });
+
+                        analytics.event('FileOffload', { middleTime: message.middleTime });
+                    });
+                },
+            }
+        });
     };
 
 });
