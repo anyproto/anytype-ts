949f272796e9ac292a4e8722998c55d36ae8d27c
Andrew Simachev <andrew.simachev@gmail.com>
2023-05-03 13:27:51 +0200

    fix dataview creation flag

--- src/ts/component/block/dataview.tsx
+++ src/ts/component/block/dataview.tsx
@@ -594,6 +594,9 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 				element,
 				vertical: dir > 0 ? I.MenuDirection.Top : I.MenuDirection.Bottom,
 				horizontal: dir > 0 ? I.MenuDirection.Left : I.MenuDirection.Right,
+				onClose: () => {
+					this.creating = false;
+				},
 				data: {
 					details,
 				},
