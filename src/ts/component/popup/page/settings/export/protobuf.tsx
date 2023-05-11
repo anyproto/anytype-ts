0695a2f8a380eb818284822377840fba58111bc2
Roman Khafizianov <requilence@gmail.com>
2023-05-11 17:25:13 +0200

    do not include files in pb export

--- src/ts/component/popup/page/settings/export/protobuf.tsx
+++ src/ts/component/popup/page/settings/export/protobuf.tsx
@@ -37,7 +37,7 @@ const PopupSettingsPageExportProtobuf = observer(class PopupSettingsPageExportPr
 		Action.openDir({
 			buttonLabel: 'Export',
 		}, paths => {
-			C.ObjectListExport(paths[0], [], I.ExportFormat.Protobuf, true, true, true, true, true, (message: any) => {
+			C.ObjectListExport(paths[0], [], I.ExportFormat.Protobuf, true, true, false, true, true, (message: any) => {
 				if (!message.error.code) {
 					Renderer.send('pathOpen', paths[0]);
 				};
