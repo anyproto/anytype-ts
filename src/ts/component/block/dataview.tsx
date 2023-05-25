c11724d6945f77091d35bc80b03e25999f99046f
Andrew Simachev <andrew.simachev@gmail.com>
2023-05-25 12:32:21 +0200

    record details for dates

--- src/ts/component/block/dataview.tsx
+++ src/ts/component/block/dataview.tsx
@@ -514,6 +514,7 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 			I.FilterCondition.In,
 			I.FilterCondition.AllIn,
 		]; 
+		const time = Util.time();
 
 		const types = Relation.getSetOfObjects(rootId, objectId, Constant.typeId.type);
 		const relations = Relation.getSetOfObjects(rootId, objectId, Constant.typeId.relation);
@@ -547,13 +548,72 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 		};
 
 		for (let filter of view.filters) {
-			if (!conditions.includes(filter.condition) || !filter.value) {
+			console.log(filter);
+
+			if (!conditions.includes(filter.condition)) {
 				continue;
 			};
-			
+
+			let value = filter.value;
+
+			switch (filter.quickOption) {
+				case I.FilterQuickOption.Yesterday: {
+					value = time - 86400;
+					break;
+				};
+
+				case I.FilterQuickOption.CurrentWeek:
+				case I.FilterQuickOption.CurrentMonth:
+				case I.FilterQuickOption.Today: {
+					value = time;
+					break;
+				};
+
+				case I.FilterQuickOption.Tomorrow: {
+					value = time + 86400;
+					break;
+				};
+
+				case I.FilterQuickOption.LastWeek: {
+					value = time - 86400 * 7;
+					break;
+				};
+
+				case I.FilterQuickOption.LastMonth: {
+					value = time - 86400 * 30;
+					break;
+				};
+
+				case I.FilterQuickOption.NextWeek: {
+					value = time + 86400 * 7;
+					break;
+				};
+
+				case I.FilterQuickOption.NextMonth: {
+					value = time + 86400 * 30;
+					break;
+				};
+
+				case I.FilterQuickOption.NumberOfDaysAgo: {
+					value = time - 86400 * value;
+					break;
+				};
+
+				case I.FilterQuickOption.NumberOfDaysNow: {
+					value = time + 86400 * value;
+					break;
+				};
+			};
+
+			if (!value) {
+				continue;
+			};
+
+			console.log(value);
+
 			const relation = dbStore.getRelationByKey(filter.relationKey);
 			if (relation && !relation.isReadonlyValue) {
-				details[filter.relationKey] = Relation.formatValue(relation, filter.value, true);
+				details[filter.relationKey] = Relation.formatValue(relation, value, true);
 			};
 		};
 
