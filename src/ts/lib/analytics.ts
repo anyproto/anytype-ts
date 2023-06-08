6f64a134c3b21795ae4674c099ca03ea725c5c05
Andrew Simachev <andrew.simachev@gmail.com>
2023-06-08 20:46:32 +0200

    fix limit in analytics

--- src/ts/lib/analytics.ts
+++ src/ts/lib/analytics.ts
@@ -6,7 +6,7 @@ import Constant from 'json/constant.json';
 const KEYS = [ 
 	'method', 'id', 'action', 'style', 'code', 'route', 'format', 'color',
 	'type', 'objectType', 'linkType', 'embedType', 'relationKey', 'layout', 'align', 'template', 'index', 'condition',
-	'tab', 'document', 'page', 'count', 'context', 'originalId', 'length', 'group', 'view', 'widgetLayout',
+	'tab', 'document', 'page', 'count', 'context', 'originalId', 'length', 'group', 'view', 'widgetLayout', 'limit',
 ];
 const KEY_CONTEXT = 'analyticsContext';
 const KEY_ORIGINAL_ID = 'analyticsOriginalId';
