1ab04cc86cdbed42c1fdcfd4ba5131e0a836af98
Andrew Simachev <andrew.simachev@gmail.com>
2023-06-19 12:26:32 +0200

    windows afterbuild hook

--- electron/hook/afterbuild.js
+++ electron/hook/afterbuild.js
@@ -22,7 +22,8 @@ exports.default = async function (context) {
 	const { packager, file } = context;
 
 	if (packager.platform.name == 'windows') {
-		let cmd = `AzureSignTool sign -kvu "${process.env.AZURE_KEY_VAULT_URI}" -kvi "${process.env.AZURE_CLIENT_ID}" -kvt "${process.env.AZURE_TENANT_ID}" -kvs "${process.env.AZURE_CLIENT_SECRET}" -kvc ${process.env.AZURE_CERT_NAME} -tr http://timestamp.digicert.com -v "${file}"`;
+		const fileName = file.replace('.blockmap', '');
+		const cmd = `AzureSignTool sign -kvu "${process.env.AZURE_KEY_VAULT_URI}" -kvi "${process.env.AZURE_CLIENT_ID}" -kvt "${process.env.AZURE_TENANT_ID}" -kvs "${process.env.AZURE_CLIENT_SECRET}" -kvc ${process.env.AZURE_CERT_NAME} -tr http://timestamp.digicert.com -v "${fileName}"`;
 
 		console.log('[afterSign]: ', cmd);
 
