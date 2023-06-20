e96cad9fa08e42415a6d5fc9d31cd92d6adae232
Andrew Simachev <andrew.simachev@gmail.com>
2023-06-20 14:09:42 +0200

    fix hook

--- electron/hook/afterbuild.js
+++ electron/hook/afterbuild.js
@@ -24,22 +24,22 @@ exports.default = async function (context) {
 
 	if (packager.platform.name == 'windows') {
 		const fileName = file.replace('.blockmap', '');
-		const cmd = `
-			AzureSignTool.exe sign \
-			-du "https://anytype.io" \
-			-fd sha384 \
-			-td sha384 \
-			-tr http://timestamp.digicert.com \
-			--azure-key-vault-url "${process.env.AZURE_KEY_VAULT_URI}" \
-			--azure-key-vault-client-id "${process.env.AZURE_CLIENT_ID}" \
-			--azure-key-vault-tenant-id "${process.env.AZURE_TENANT_ID}" \
-			--azure-key-vault-client-secret "${process.env.AZURE_CLIENT_SECRET}" \
-			--azure-key-vault-certificate "${process.env.AZURE_CERT_NAME}" \
-			-v \
-			-ifl "${fileName}"
-		`
-		console.log(cmd);
+		const cmd = [
+			`AzureSignTool.exe sign`
+			`-du "https://anytype.io"`,
+			`-fd sha384`,
+			`-td sha384`,
+			`-tr http://timestamp.digicert.com`,
+			`--azure-key-vault-url "${process.env.AZURE_KEY_VAULT_URI}"`,
+			`--azure-key-vault-client-id "${process.env.AZURE_CLIENT_ID}"`,
+			`--azure-key-vault-tenant-id "${process.env.AZURE_TENANT_ID}"`,
+			`--azure-key-vault-client-secret "${process.env.AZURE_CLIENT_SECRET}"`,
+			`--azure-key-vault-certificate "${process.env.AZURE_CERT_NAME}"`,
+			`-v`
+			`"${fileName}"`,
+		].join(' ');
 
+		console.log(cmd);
 		return await execPromise(cmd);
 	};
 
