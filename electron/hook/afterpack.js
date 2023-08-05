12d7267380e0a964d57843bdcc5215cc5586a278
Andrew Simachev <andrew.simachev@gmail.com>
2023-08-06 00:25:06 +0200

    add ELECTRON_SKIP_SENTRY flag

--- electron/hook/afterpack.js
+++ electron/hook/afterpack.js
@@ -2,6 +2,10 @@ const SentryCli = require('@sentry/cli');
 const cli = new SentryCli();
 
 exports.default = async function (context) {
+	if (process.env.ELECTRON_SKIP_SENTRY) {
+		return;
+	};
+
 	cli.releases.options = {
 		url: 'https://sentry.anytype.io',
 		authToken: process.env.SENTRY_AUTH_TOKEN,
