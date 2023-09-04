3fbe66b08ba9ba9846244fd0a6d548e803f55a90
Andrew Simachev <andrew.simachev@gmail.com>
2023-09-04 19:40:00 +0200

    JS-2912: fix

--- src/ts/component/editor/page.tsx
+++ src/ts/component/editor/page.tsx
@@ -1849,6 +1849,10 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 				event.params.fileType = param.content.type;
 			};
 
+			if (param.type == I.BlockType.Dataview) {
+				event.id = param.content.isCollection ? 'Collection' : 'Set';
+			};
+
 			analytics.event('CreateBlock', event);
 		});
 	};
