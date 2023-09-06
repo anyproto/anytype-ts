9c97ce1a4c78def7900d19e35ae52360a4ea002e
Mike Mhlv <mhlv@anytype.io>
2023-09-06 09:54:39 +0100

    JS-2851: code review

--- src/ts/component/footer/auth/disclaimer.tsx
+++ src/ts/component/footer/auth/disclaimer.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Label } from 'Component';
-import { I, translate } from 'Lib';
+import { I, translate, UtilCommon } from 'Lib';
+import Url from 'json/url.json';
 
 class FooterAuthDisclaimer extends React.Component<I.FooterComponent> {
 	
 	render () {
-		return <Label className="disclaimer" text={translate('authDisclaimer')} />;
+		return <Label className="disclaimer" text={UtilCommon.sprintf(translate('authDisclaimer'), Url.terms, Url.privacy)} />;
 	};
 
 };
