082e3e7116dd665d027e81f6c93163a9b3437459
Andrew Simachev <andrew.simachev@gmail.com>
2023-10-20 13:34:02 +0200

    JS-3088: fix

--- src/ts/component/menu/space.tsx
+++ src/ts/component/menu/space.tsx
@@ -161,11 +161,7 @@ const MenuSpace = observer(class MenuSpace extends React.Component<I.Menu> {
 			this.n = 0;
 		};
 
-		if (items[this.n].isActive) {
-			this.onArrow(dir);
-		} else {
-			this.props.setActive();
-		};
+		this.props.setActive();
 	};
 
 	getItems () {
