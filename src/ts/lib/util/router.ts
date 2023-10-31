54e951c4812ac15c928f6e1cc782a0b4ad8d5f13
Andrew Simachev <andrew.simachev@gmail.com>
2023-10-31 10:17:19 +0100

    get spaceId from routing

--- src/ts/lib/util/router.ts
+++ src/ts/lib/util/router.ts
@@ -1,4 +1,4 @@
-import { C, UtilData, Preview, analytics, Storage } from 'Lib';
+import { C, UtilData, Preview, analytics, Storage, keyboard } from 'Lib';
 import { commonStore, authStore, blockStore, menuStore, popupStore } from 'Store';
 import Constant from 'json/constant.json';
 
@@ -153,7 +153,8 @@ class UtilRouter {
 	};
 
 	getRouteSpaceId () {
-		return commonStore.space;
+		const param = this.getParam(this.history.location.pathname);
+		return param.spaceId || commonStore.space;
 	};
 
 };
