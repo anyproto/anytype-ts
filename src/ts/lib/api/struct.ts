ef05264c9cc9c1083f06aaf35a5972f770039d55
Andrew Simachev <andrew.simachev@gmail.com>
2023-11-14 16:59:15 +0100

    fix Encode.struct undefined error

--- src/ts/lib/api/struct.ts
+++ src/ts/lib/api/struct.ts
@@ -2,31 +2,42 @@ import Struct from 'google-protobuf/google/protobuf/struct_pb.js';
 
 export class Encode {
 
-	static encodeStruct (obj: any) {
-		if (typeof obj === 'undefined') {
-			obj = null;
-		};
-		return Struct.Struct.fromJavaScript(obj);
+	public static struct (obj: any) {
+		return Struct.Struct.fromJavaScript(this.prepare(obj));
 	};
 
-	static encodeValue (value: any) {
-		if (typeof value === 'undefined') {
-			value = null;
+	public static value (value: any) {
+		return Struct.Value.fromJavaScript(this.prepare(value));
+	};
+
+	private static prepare (o: any) {
+		if (typeof o === 'undefined') {
+			o = null;
+		} else 
+		if (typeof o === 'object') {
+			for (const k in o) {
+				if (typeof o[k] === 'object') {
+					o[k] = this.prepare(o[k]);
+				} else 
+				if (typeof o[k] === 'undefined') {
+					o[k] = null;
+				};
+			};
 		};
-		return Struct.Value.fromJavaScript(value);
+		return o;
 	};
 	
 };
 
 export class Decode {
 
-	static decodeValue (value: any) {
+	public static value (value: any) {
 		let data = null;
 		try { data = value ? value.toJavaScript() : null; } catch (e) { /**/ };
 		return data;
 	};
 
-	static decodeStruct (struct: any) {
+	public static struct (struct: any) {
 		let data = {};
 		try { data = struct ? struct.toJavaScript() : {}; } catch (e) { /**/ };
 		return data;
