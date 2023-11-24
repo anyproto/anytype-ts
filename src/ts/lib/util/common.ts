9d2ff912e6a2ace44226919ea53289c6f2b9d487
Andrew Simachev <andrew.simachev@gmail.com>
2023-11-24 23:37:19 +0100

    fix sanitize

--- src/ts/lib/util/common.ts
+++ src/ts/lib/util/common.ts
@@ -774,7 +774,7 @@ class UtilCommon {
 		return DOMPurify.sanitize(String(s || ''), { 
 			ALLOWED_TAGS: [ 
 				'b', 'br', 'a', 'ul', 'li', 'h1', 'strike', 'kbd', 'italic', 'bold', 'underline', 'lnk', 'color',
-				'bgcolor', 'mention', 'emoji', 'obj', 'span', 'p', 'name', 'smile', 'img'
+				'bgcolor', 'mention', 'emoji', 'obj', 'span', 'p', 'name', 'smile', 'img', 'search'
 			] 
 		});
 	};
