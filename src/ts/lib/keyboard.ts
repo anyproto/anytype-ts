b665bf9cee1cf21e5ccd987d3ff4e708da02aebf
Andrew Simachev <andrew.simachev@gmail.com>
2023-12-19 16:52:45 +0100

    JS-3594: remove device name

--- src/ts/lib/keyboard.ts
+++ src/ts/lib/keyboard.ts
@@ -601,7 +601,6 @@ class Keyboard {
 		C.AppGetVersion((message: any) => {
 			let url = Url.contact;
 
-			url = url.replace(/\%25device\%25/g, window.Electron.version.device);
 			url = url.replace(/\%25os\%25/g, window.Electron.version.os);
 			url = url.replace(/\%25version\%25/g, window.Electron.version.app);
 			url = url.replace(/\%25build\%25/g, message.details);
