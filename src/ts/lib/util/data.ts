f2481d4a402b12a9dfe0fa56624c6971366dae0e
Andrew Simachev <andrew.simachev@gmail.com>
2024-01-04 11:18:47 +0100

    fix title update

--- src/ts/lib/util/data.ts
+++ src/ts/lib/util/data.ts
@@ -482,7 +482,7 @@ class UtilData {
 	checkDetails (rootId: string, blockId?: string) {
 		blockId = blockId || rootId;
 
-		const object = detailStore.get(rootId, blockId, [ 'layoutAlign', 'templateIsBundled' ].concat(Constant.coverRelationKeys));
+		const object = detailStore.get(rootId, blockId, [ 'layoutAlign', 'templateIsBundled' ].concat(Constant.coverRelationKeys), true);
 		const childrenIds = blockStore.getChildrenIds(rootId, blockId);
 		const checkType = blockStore.checkBlockTypeExists(rootId);
 		const { iconEmoji, iconImage, coverType, coverId } = object;
