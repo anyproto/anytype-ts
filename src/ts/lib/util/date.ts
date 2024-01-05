62e7c34f4873c5d4777aa4b2bfe9edd0f041528f
Andrew Simachev <andrew.simachev@gmail.com>
2024-01-05 23:43:39 +0100

    code review fixes

--- src/ts/lib/util/date.ts
+++ src/ts/lib/util/date.ts
@@ -335,13 +335,13 @@ class UtilDate {
 	isLeapYear (year: number): boolean {
 		if (year % 4 !== 0) {
 			return false;
-		}
+		};
 		if (year % 400 === 0) {
 			return true;
-		}
+		};
 		if (year % 100 === 0) {
 			return false;
-		}
+		};
 		return true;
 	};
 
