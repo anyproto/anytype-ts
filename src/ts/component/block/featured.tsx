462f52d13291c9f3f2218394ecf9dec154784bf5
Andrew Simachev <andrew.simachev@gmail.com>
2024-01-19 12:08:13 +0100

    JS-3406: fix

--- src/ts/component/block/featured.tsx
+++ src/ts/component/block/featured.tsx
@@ -209,7 +209,7 @@ const BlockFeatured = observer(class BlockFeatured extends React.Component<Props
 								arrayLimit={relation.format == I.RelationType.Object ? 1 : 2}
 								textLimit={150}
 								onMouseLeave={this.onMouseLeave}
-								withLabel={true}
+								withName={true}
 							/>
 						</span>
 					);
