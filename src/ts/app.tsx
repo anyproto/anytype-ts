4b8bdd3601a43b197cf2c2201280edf2b23c555a
Mike Mhlv <mhlv@anytype.io>
2024-01-24 11:38:30 +0000

    JS-3489: wip

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -233,6 +233,10 @@ class App extends React.Component<object, State> {
 
 	componentDidMount () {
 		this.init();
+
+		window.setTimeout(() => {
+			popupStore.open('settings', { data: { page: 'spacesList' }});
+		}, 1000);
 	};
 
 	init () {
--- src/ts/component/popup/page/settings/spacesList.tsx
+++ src/ts/component/popup/page/settings/spacesList.tsx
@@ -2,25 +2,61 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { Title, Label, IconObject, ObjectName, Button } from 'Component';
 import { analytics, C, UtilRouter, UtilFile, I, translate, UtilCommon } from 'Lib';
-import { authStore, commonStore, popupStore } from 'Store';
+import { authStore, commonStore, dbStore, popupStore } from 'Store';
 
-interface Props extends I.PopupSettings {
-	onPage: (id: string) => void;
-	setLoading: (v: boolean) => void;
-};
+const PopupSettingsPageSpacesList = observer(class PopupSettingsPageSpacesList extends React.Component<{}, {}> {
 
-const PopupSettingsPageSpacesList = observer(class PopupSettingsPageSpacesList extends React.Component<Props, {}> {
-
-	constructor (props: Props) {
+	constructor (props) {
 		super(props);
 	};
 
 	render () {
+		const spaces = dbStore.getSpaces();
+
+		const Row = (space) => {
+			console.log('SPACE: ', space)
+			return (
+				<tr>
+					<td>
+						<div className="spaceNameWrapper">
+							<IconObject object={space} />
+							<div className="spaceName">
+								<ObjectName object={space} />
+							</div>
+						</div>
+					</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			);
+		};
 
 		return (
 			<React.Fragment>
 				<Title text={translate('popupSettingsSpacesListTitle')} />
 
+				<div className="spacesList">
+					<table>
+						<thead>
+							<tr>
+								<th>{translate('popupSettingsSpacesListSpace')}</th>
+								<th>{translate('popupSettingsSpacesListAccess')}</th>
+								<th>{translate('popupSettingsSpacesListSize')}</th>
+								<th>{translate('popupSettingsSpacesListNetwork')}</th>
+								<th>{translate('popupSettingsSpacesListDevice')}</th>
+								<th> </th>
+							</tr>
+						</thead>
+						<tbody>
+							{spaces.map((item: any, i: number) => (
+								<Row key={i} {...item} />
+							))}
+						</tbody>
+					</table>
+				</div>
 			</React.Fragment>
 		);
 	};
