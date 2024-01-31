5edf96ee055137f86ab7f4641623989f7d9c90ee
Mike Mhlv <mhlv@anytype.io>
2024-01-31 16:54:36 +0000

    JS-3222: fix

--- src/ts/component/widget/list/index.tsx
+++ src/ts/component/widget/list/index.tsx
@@ -237,10 +237,12 @@ const WidgetList = observer(class WidgetList extends React.Component<Props, Stat
 		if (!this.cache) {
 			const items = this.getItems();
 
+			console.log('ITEMS: ', items)
+
 			this.cache = new CellMeasurerCache({
 				fixedWidth: true,
 				defaultHeight: i => this.getRowHeight(items[i]),
-				keyMapper: i => items[i].id,
+				keyMapper: i => items[i],
 			});
 		};
 	};
