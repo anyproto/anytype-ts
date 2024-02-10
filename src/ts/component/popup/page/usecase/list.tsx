cf503557ecf848d271521e8fb10b17c1635654ad
Andrew Simachev <andrew.simachev@gmail.com>
2024-02-10 19:15:39 +0100

    empty state

--- src/ts/component/popup/page/usecase/list.tsx
+++ src/ts/component/popup/page/usecase/list.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Loader, Title, Label, EmptySearch } from 'Component';
+import { Loader, Title, Label, EmptySearch, Icon, Filter } from 'Component';
 import { I, C, translate, UtilCommon } from 'Lib';
 import { AutoSizer, CellMeasurer, CellMeasurerCache, List, WindowScroller } from 'react-virtualized';
 import { commonStore } from 'Store';
@@ -15,8 +15,10 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 
 	node = null;
 	refList = null;
+	refFilter = null;
 	cache: any = {};
-	timeout = 0;
+	timeoutResize = 0;
+	timeoutFilter = 0;
 	category = null;
 	state = {
 		isLoading: false
@@ -32,6 +34,8 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 
 		this.onResize = this.onResize.bind(this);
 		this.onCategory = this.onCategory.bind(this);
+		this.onFilterChange = this.onFilterChange.bind(this);
+		this.onFilterClear = this.onFilterClear.bind(this);
 	};
 	
 	render () {
@@ -46,7 +50,7 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 
 		let textEmpty = '';
 		if (this.category) {
-			textEmpty = UtilCommon.sprintf(translate('popupUsecaseListCategoryEmpty'), this.category.name);
+			textEmpty = UtilCommon.sprintf(translate('popupUsecaseListEmptyCategory'), this.category.name);
 		};
 
 		const Category = (item: any) => (
@@ -95,11 +99,26 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 							<Category key={i} {...item} />
 						))}
 					</div>
+
+					<div className="gradient left" />
+					<div className="gradient right" />
+
+					<Icon className="arrow left" />
+					<Icon className="arrow right" />
 				</div>
 
 				<div className="mid">
 					<Title text={translate('popupUsecaseListTitle')} />
 					<Label text={translate('popupUsecaseListText')} />
+
+					<Filter 
+						ref={ref => this.refFilter = ref}
+						id="store-filter"
+						icon="search"
+						placeholder={translate('commonSearchPlaceholder')}
+						onChange={this.onFilterChange}
+						onClear={this.onFilterClear}
+					/>
 				</div>
 
 				<div className="items">
@@ -155,7 +174,8 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 	};
 
 	componentWillUnmount(): void {
-		window.clearTimeout(this.timeout);
+		window.clearTimeout(this.timeoutResize);
+		window.clearTimeout(this.timeoutFilter);
 	};
 
 	reset () {
@@ -175,12 +195,27 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 		this.forceUpdate();
 	};
 
+	onFilterChange (v: string) {
+		window.clearTimeout(this.timeoutFilter);
+		this.timeoutFilter = window.setTimeout(() => this.forceUpdate(), 500);
+	};
+
+	onFilterClear () {
+		this.forceUpdate();
+	};
+
 	getItems () {
 		const ret: any[] = [];
+		const filter = this.refFilter ? this.refFilter.getValue() : '';
 		
 		let items = commonStore.gallery.list || [];
 		if (this.category) {
 			items = items.filter(it => this.category.list.includes(it.name));
+		};
+
+		if (filter) {
+			const reg = new RegExp(UtilCommon.regexEscape(filter), 'gi');
+			items = items.filter(it => reg.test(it.title) || reg.test(it.description));
 		};
 
 		let n = 0;
@@ -205,8 +240,8 @@ class PopupUsecasePageList extends React.Component<I.PopupUsecase, State> {
 	};
 
 	onResize ({ width }) {
-		window.clearTimeout(this.timeout);
-		this.timeout = window.setTimeout(() => this.forceUpdate(), 10);
+		window.clearTimeout(this.timeoutResize);
+		this.timeoutResize = window.setTimeout(() => this.forceUpdate(), 10);
 	};
 
 };
