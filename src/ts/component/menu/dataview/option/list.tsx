2264255b2a4aa558cc831ae2d767c5496e2c586f
Andrew Simachev <andrew.simachev@gmail.com>
2024-02-28 18:09:06 +0100

    relation option order

--- src/ts/component/menu/dataview/option/list.tsx
+++ src/ts/component/menu/dataview/option/list.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import $ from 'jquery';
 import { observer } from 'mobx-react';
-import { AutoSizer, CellMeasurer, InfiniteLoader, List, CellMeasurerCache } from 'react-virtualized';
+import { AutoSizer, CellMeasurer, InfiniteLoader, List as VList, CellMeasurerCache } from 'react-virtualized';
 import { Icon, Tag, Filter, DragBox } from 'Component';
 import { I, C, UtilCommon, UtilMenu, keyboard, Relation, translate } from 'Lib';
 import { menuStore, dbStore, commonStore } from 'Store';
 import Constant from 'json/constant.json';
+import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
 const HEIGHT = 28;
@@ -25,65 +26,74 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 		
 		this.rebind = this.rebind.bind(this);
 		this.onFilterChange = this.onFilterChange.bind(this);
+		this.onSortStart = this.onSortStart.bind(this);
+		this.onSortEnd = this.onSortEnd.bind(this);
 	};
 	
 	render () {
-		const { param } = this.props;
+		if (!this.cache) {
+			return null;
+		};
+
+		const { param, getId } = this.props;
 		const { data } = param;
 		const { filter, canAdd, noFilter } = data;
 		const relation = data.relation.get();
 		const value = data.value || [];
 		const items = this.getItems();
+		const placeholder = canAdd ? translate('menuDataviewOptionListFilterOrCreateOptions') : translate('menuDataviewOptionListFilterOptions');
+		const empty = canAdd ? translate('menuDataviewOptionListTypeToCreate') : translate('menuDataviewOptionListTypeToSearch');
 
-		let placeholder;
-		let empty;
+		const Handle = SortableHandle(() => (
+			<Icon className="dnd" />
+		));
 
-		if (canAdd) {
-			placeholder = translate('menuDataviewOptionListFilterOrCreateOptions');
-			empty = translate('menuDataviewOptionListTypeToCreate');
-		}
-		else {
-			placeholder = translate('menuDataviewOptionListFilterOptions');
-			empty = translate('menuDataviewOptionListTypeToSearch');
-		};
+		const Item = SortableElement((item: any) => {
+			const active = value.indexOf(item.id) >= 0;
 
-		if (!this.cache) {
-			return null;
-		};
+			return (
+				<div 
+					id={`item-${item.id}`} 
+					className="item" 
+					style={item.style} 
+					onMouseEnter={e => this.onOver(e, item)}
+				>
+					<Handle />
+					<div className="clickable" onClick={e => this.onClick(e, item)}>
+						<Tag text={item.name} color={item.color} className={Relation.selectClassName(relation.format)} />
+					</div>
+					<div className="buttons">
+						<Icon className="more" onClick={e => this.onEdit(e, item)} />
+					</div>
+					{active ? <Icon className="chk" /> : ''}
+				</div>
+			);
+		});
+
+		const ItemAdd = (item: any) => (
+			<div 
+				id="item-add" 
+				className="item add" 
+				onMouseEnter={e => this.onOver(e, item)} 
+				onClick={e => this.onClick(e, item)}
+				style={item.style}
+			>
+				<Icon className="plus" />
+				<div className="name">{item.name}</div>
+			</div>
+		);
 
 		const rowRenderer = (param: any) => {
 			const item: any = items[param.index];
-			const active = value.indexOf(item.id) >= 0;
 			
 			let content = null;
 			if (item.id == 'add') {
-				content = (
-					<div 
-						id="item-add" 
-						className="item add" 
-						style={param.style}
-						onClick={e => this.onClick(e, item)} 
-						onMouseEnter={e => this.onOver(e, item)}
-					>
-						<Icon className="plus" />
-						<div className="name">{item.name}</div>
-					</div>
-				);
+				content = <ItemAdd key={item.id} {...item} index={param.index} disabled={true} style={param.style} />;
 			} else 
 			if (item.isSection) {
-				content = (<div className="sectionName" style={param.style}>{item.name}</div>);
+				content = <div className="sectionName" style={param.style}>{item.name}</div>;
 			} else {
-				content = (
-					<div id={'item-' + item.id} className="item" style={param.style} onMouseEnter={e => this.onOver(e, item)}>
-						<div className="clickable" onClick={e => this.onClick(e, item)}>
-							<Tag text={item.name} color={item.color} className={Relation.selectClassName(relation.format)} />
-						</div>
-						<div className="buttons">
-							<Icon className="more" onClick={e => this.onEdit(e, item)} />
-						</div>
-						{active ? <Icon className="chk" /> : ''}
-					</div>
-				);
+				content = <Item key={item.id} {...item} index={param.index} style={param.style} />;
 			};
 
 			return (
@@ -99,6 +109,38 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 			);
 		};
 
+		const List = SortableContainer(() => (
+			<div className="items">
+				{items.length ? (
+					<InfiniteLoader
+						rowCount={items.length}
+						loadMoreRows={() => {}}
+						isRowLoaded={() => true}
+						threshold={LIMIT}
+					>
+						{({ onRowsRendered }) => (
+							<AutoSizer className="scrollArea">
+								{({ width, height }) => (
+									<VList
+										ref={ref => this.refList = ref}
+										width={width}
+										height={height}
+										deferredMeasurmentCache={this.cache}
+										rowCount={items.length}
+										rowHeight={HEIGHT}
+										rowRenderer={rowRenderer}
+										onRowsRendered={onRowsRendered}
+										overscanRowCount={10}
+										scrollToAlignment="center"
+									/>
+								)}
+							</AutoSizer>
+						)}
+					</InfiniteLoader>
+				) : <div className="item empty">{empty}</div>}
+			</div>
+		));
+
 		return (
 			<div className={[ 'wrap', (noFilter ? 'noFilter' : '') ].join(' ')}>
 				{!noFilter ? (
@@ -113,37 +155,16 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 					/>
 				) : ''}
 
-				<div className="items">
-					{items.length ? (
-						<InfiniteLoader
-							rowCount={items.length}
-							loadMoreRows={() => {}}
-							isRowLoaded={() => true}
-							threshold={LIMIT}
-						>
-							{({ onRowsRendered }) => (
-								<AutoSizer className="scrollArea">
-									{({ width, height }) => (
-										<List
-											ref={ref => this.refList = ref}
-											width={width}
-											height={height}
-											deferredMeasurmentCache={this.cache}
-											rowCount={items.length}
-											rowHeight={HEIGHT}
-											rowRenderer={rowRenderer}
-											onRowsRendered={onRowsRendered}
-											overscanRowCount={10}
-											scrollToAlignment="center"
-										/>
-									)}
-								</AutoSizer>
-							)}
-						</InfiniteLoader>
-					) : (
-						<div className="item empty">{empty}</div>
-					)}
-				</div>
+				<List 
+					axis="y" 
+					transitionDuration={150}
+					distance={10}
+					useDragHandle={true}
+					onSortStart={this.onSortStart}
+					onSortEnd={this.onSortEnd}
+					helperClass="isDragging"
+					helperContainer={() => $(`#${getId()} .items`).get(0)}
+				/>
 			</div>
 		);
 	};
@@ -351,6 +372,7 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 		const { data } = param;
 		const { canAdd, filterMapper } = data;
 		const relation = data.relation.get();
+		const { optionOrder } = relation;
 		const isSelect = relation.format == I.RelationType.Select;
 		const value = Relation.getArrayValue(data.value);
 		const ret = [];
@@ -364,12 +386,19 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 			items = items.filter(filterMapper);
 		};
 
+		console.log('optionOrder', JSON.stringify(optionOrder, null, 3));
+
 		items.sort((c1, c2) => {
 			const isSelected1 = value.includes(c1.id);
 			const isSelected2 = value.includes(c2.id);
+			const idx1 = optionOrder.indexOf(c1.id);
+			const idx2 = optionOrder.indexOf(c2.id);
 
-			if (isSelected1 && !isSelected2) return -1;
-			if (!isSelected1 && isSelected2) return 1;
+			//if (isSelected1 && !isSelected2) return -1;
+			//if (!isSelected1 && isSelected2) return 1;
+
+			if (idx1 > idx2) return 1;
+			if (idx1 < idx2) return -1;
 
 			return 0;
 		});
@@ -389,6 +418,24 @@ const MenuOptionList = observer(class MenuOptionList extends React.Component<I.M
 		};
 
 		return items.concat(ret);
+	};
+
+	onSortStart () {
+		keyboard.disableSelection(true);
+	};
+	
+	onSortEnd (result: any) {
+		const { oldIndex, newIndex } = result;
+		const { param } = this.props;
+		const { data } = param;
+		const relation = data.relation.get();
+		
+		let ids = this.getItems().map(it => it.id);
+		ids = arrayMove(ids, oldIndex, newIndex);
+
+		console.log('ORDER', ids);
+
+		C.ObjectSetDetails(relation.id, [ { key: 'relationOptionOrder', value: ids } ]);
 	};
 
 	resize () {
