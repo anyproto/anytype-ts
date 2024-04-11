1d1d6ac12538f942c16406160de2ba370c762d4b
Andrew Simachev <andrew.simachev@gmail.com>
2024-04-11 12:20:39 +0200

    fix email regexp

--- src/ts/lib/util/common.ts
+++ src/ts/lib/util/common.ts
@@ -454,7 +454,11 @@ class UtilCommon {
 	};
 	
 	emailCheck (v: string) {
-		return /^\p{L}+(?:[\.-]?\p{L}+)*@\p{L}+(?:[\.-]?\p{L}+)*(?:\.\p{L}{2,5})+$/.test(String(v || ''));
+		v = String(v || '');
+
+		const uc = '\\P{Script_Extensions=Latin}';
+		const reg = new RegExp(`^[-\\.\\w${uc}]+@([-\\.\\w${uc}]+\\.)+[-\\w${uc}]{2,5}$`, 'gu');
+		return reg.test(v);
 	};
 
 	getSelectionRange (): Range {
