991bc1b3ce16110c68e77532d9ad0a4cf9660aa0
Andrew Simachev <andrew.simachev@gmail.com>
2024-04-20 11:28:25 +0200

    JS-4421: library usecase banner

--- src/ts/component/header/main/store.tsx
+++ src/ts/component/header/main/store.tsx
@@ -1,16 +1,41 @@
 import * as React from 'react';
-import { I } from 'Lib';
+import { Title, Button, Icon } from 'Component';
+import { I, translate } from 'Lib';
 
 class HeaderMainStore extends React.Component<I.HeaderComponent> {
 
 	render () {
-		const { renderLeftIcons, renderTabs } = this.props;
+		const { renderLeftIcons, renderTabs, withBanner, onBanner, onBannerClose } = this.props;
+
+		let banner = null;
+		if (withBanner) {
+			banner = (
+				<div className="bannerWrapper">
+					<div className="banner" onClick={onBanner}>
+						<Icon className="close" onClick={onBannerClose} />
+
+						<div className="side left">
+							<Title text={translate('pageMainStoreBannerTitle')} />
+							<Button text={translate('pageMainStoreBannerButton')} className="c28" />
+						</div>
+
+						<div className="side right">
+							<div className="pic" />
+						</div>
+					</div>
+				</div>
+			);
+		};
 		
 		return (
 			<React.Fragment>
-				<div className="side left">{renderLeftIcons()}</div>
-				<div className="side center">{renderTabs()}</div>
-				<div className="side right" />
+				{banner}
+
+				<div className="sides">
+					<div className="side left">{renderLeftIcons()}</div>
+					<div className="side center">{renderTabs()}</div>
+					<div className="side right" />
+				</div>
 			</React.Fragment>
 		);
 	};
