114ec8dd1b6053d52948667b0c1d723bad4510d8
Andrew Simachev <andrew.simachev@gmail.com>
2024-05-13 12:37:03 +0200

    header updates

--- src/ts/component/header/main/history.tsx
+++ src/ts/component/header/main/history.tsx
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react';
 import { Icon } from 'Component';
 import { C, UtilDate, UtilObject, I, translate, analytics, UtilSpace, keyboard } from 'Lib';
 import { detailStore, popupStore } from 'Store';
+import Constant from 'json/constant.json';
 
 interface State {
 	version: I.HistoryVersion;
@@ -20,11 +21,15 @@ const HeaderMainHistory = observer(class HeaderMainHistory extends React.Compone
 		this.onBack = this.onBack.bind(this);
 		this.onRestore = this.onRestore.bind(this);
 		this.onOpen = this.onOpen.bind(this);
+		this.onRelation = this.onRelation.bind(this);
 	};
 
 	render () {
-		const { renderLeftIcons } = this.props;
+		const { rootId, renderLeftIcons } = this.props;
 		const { version } = this.state;
+		const cmd = keyboard.cmdSymbol();
+		const object = detailStore.get(rootId, rootId, []);
+		const showMenu = !UtilObject.isTypeOrRelationLayout(object.layout);
 		const canWrite = UtilSpace.canMyParticipantWrite();
 
 		return (
@@ -39,8 +44,9 @@ const HeaderMainHistory = observer(class HeaderMainHistory extends React.Compone
 					</div>
 				</div>
 
-				<div className="side right" onClick={this.onRestore}>
-					{canWrite ? <div className="item orange">{translate('headerHistoryRestore')}</div> : ''}
+				<div className="side right">
+					{canWrite ? <div className="item orange" onClick={this.onRestore}>{translate('headerHistoryRestore')}</div> : ''}
+					{showMenu ? <Icon id="button-header-relation" tooltip="Relations" tooltipCaption={`${cmd} + Shift + R`} className="relation" onClick={this.onRelation} /> : ''}
 				</div>
 			</React.Fragment>
 		);
@@ -76,6 +82,28 @@ const HeaderMainHistory = observer(class HeaderMainHistory extends React.Compone
 			UtilObject.openEvent(e, object);
 
 			analytics.event('RestoreFromHistory');
+		});
+	};
+
+	onRelation () {
+		const { isPopup, rootId, menuOpen } = this.props;
+		const cnw = [ 'fixed' ];
+
+		if (!isPopup) {
+			cnw.push('fromHeader');
+		};
+
+		menuOpen('blockRelationView', '#button-header-relation', {
+			noFlipX: true,
+			noFlipY: true,
+			horizontal: I.MenuDirection.Right,
+			subIds: Constant.menuIds.cell,
+			classNameWrap: cnw.join(' '),
+			data: {
+				isPopup,
+				rootId,
+				readonly: true
+			},
 		});
 	};
 
