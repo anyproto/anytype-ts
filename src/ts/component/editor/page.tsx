96937c9d74fed6cea947ddafd605105e58811c67
Andrew Simachev <andrew.simachev@gmail.com>
2024-05-22 16:50:28 +0200

    JS-4630: fix onPaste not clearing keyboard.isFocused flag

--- src/ts/component/editor/page.tsx
+++ src/ts/component/editor/page.tsx
@@ -1726,6 +1726,8 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 		let from = 0;
 		let to = 0;
 
+		keyboard.setFocus(false);
+
 		C.BlockPaste(rootId, focused, range, selection.get(I.SelectType.Block, true), data.anytype.range.to > 0, { ...data, anytype: data.anytype.blocks }, '', (message: any) => {
 			if (message.error.code) {
 				return;
