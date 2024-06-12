b9bab68bee6a0f919d451717591b2bade779e9df
Andrew Simachev <andrew.simachev@gmail.com>
2024-06-12 13:48:28 +0200

    remove unused code

--- src/ts/lib/api/command.ts
+++ src/ts/lib/api/command.ts
@@ -2,8 +2,8 @@ import Commands from 'dist/lib/pb/protos/commands_pb';
 import Model from 'dist/lib/pkg/lib/pb/model/protos/models_pb';
 import { detailStore } from 'Store';
 import { I, UtilCommon, Mark, Storage, dispatcher, Encode, Mapper } from 'Lib';
+
 const Constant = require('json/constant.json');
-import { MembershipTier } from 'Interface';
 
 const Rpc = Commands.Rpc;
 
