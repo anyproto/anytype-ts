1016461c1c75a581ca8ced54525e59ca1e8e3c58
Andrew Simachev <andrew.simachev@gmail.com>
2024-06-24 16:34:36 +0200

    fix date object

--- src/ts/lib/util/object.ts
+++ src/ts/lib/util/object.ts
@@ -16,7 +16,7 @@ class UtilObject {
 		let r = '';
 		switch (v) {
 			default:						 r = 'edit'; break;
-			case I.ObjectLayout.Date:
+			case I.ObjectLayout.Date: 		 r = 'set'; break;
 			case I.ObjectLayout.Type:		 r = 'type'; break;
 			case I.ObjectLayout.Relation:	 r = 'relation'; break;
 			case I.ObjectLayout.Navigation:	 r = 'navigation'; break;
@@ -319,6 +319,7 @@ class UtilObject {
 		return [ 
 			I.ObjectLayout.Set,
 			I.ObjectLayout.Collection,
+			I.ObjectLayout.Date,
 		];
 	};
 
