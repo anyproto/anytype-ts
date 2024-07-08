6e543f8d2ab89628e50aeffbc06263fd0b0292b8
Andrew Simachev <andrew.simachev@gmail.com>
2024-07-08 14:18:27 +0200

    fix sentry crash

--- src/ts/component/widget/index.tsx
+++ src/ts/component/widget/index.tsx
@@ -112,6 +112,8 @@ const WidgetIndex = observer(class WidgetIndex extends React.Component<Props> {
 					}}
 				/>
 			);
+
+			isDraggable = false;
 		} else {
 			buttons = (
 				<div className="buttons">
