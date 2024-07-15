e456b023356b19a2e5b8e9c67a100156ac0cc236
mcrakhman <rakhmanov.m@gmail.com>
2024-07-15 19:18:38 +0200

    JS-4904: add custom sort

--- src/ts/component/menu/syncStatus.tsx
+++ src/ts/component/menu/syncStatus.tsx
@@ -238,6 +238,7 @@ const MenuSyncStatus = observer(class MenuSyncStatus extends React.Component<I.M
 			{ operator: I.FilterOperator.And, relationKey: 'layout', condition: I.FilterCondition.NotIn, value: U.Object.getSystemLayouts() },
 		];
 		const sorts = [
+			{ relationKey: 'syncStatus', type: I.SortType.Custom, customOrder: [I.SyncStatusObject.Syncing, I.SyncStatusObject.Synced] },
 			{ relationKey: 'syncDate', type: I.SortType.Desc },
 		];
 
