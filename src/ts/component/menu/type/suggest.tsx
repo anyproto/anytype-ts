5ce5f2374930d9c26e74ee68096aa1b96aacb6ab
Andrew Simachev <andrew.simachev@gmail.com>
2024-07-18 10:25:50 +0200

    JS-4917

--- src/ts/component/menu/type/suggest.tsx
+++ src/ts/component/menu/type/suggest.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import $ from 'jquery';
 import { observer } from 'mobx-react';
 import { AutoSizer, CellMeasurer, InfiniteLoader, List, CellMeasurerCache } from 'react-virtualized';
-import { Filter, Icon, MenuItemVertical, Loader } from 'Component';
+import { Filter, Icon, MenuItemVertical, Loader, EmptySearch } from 'Component';
 import { I, C, S, U, J, analytics, keyboard, Action, translate } from 'Lib';
 
 interface State {
-	loading: boolean;
+	isLoading: boolean;
 };
 
 const HEIGHT_ITEM = 28;
@@ -16,7 +16,7 @@ const LIMIT = 20;
 const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I.Menu, State> {
 
 	state = {
-		loading: false,
+		isLoading: false,
 	};
 
 	_isMounted = false;
@@ -39,7 +39,7 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 	};
 	
 	render () {
-		const { loading } = this.state;
+		const { isLoading } = this.state;
 		const { param } = this.props;
 		const { data } = param;
 		const { filter, noFilter } = data;
@@ -114,35 +114,41 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 					/>
 				) : ''}
 
-				{loading ? <Loader /> : ''}
+				{isLoading ? <Loader /> : ''}
 
-				<div className="items">
-					<InfiniteLoader
-						rowCount={items.length + 1}
-						loadMoreRows={this.loadMoreRows}
-						isRowLoaded={({ index }) => !!items[index]}
-						threshold={LIMIT}
-					>
-						{({ onRowsRendered, registerChild }) => (
-							<AutoSizer className="scrollArea">
-								{({ width, height }) => (
-									<List
-										ref={ref => this.refList = ref}
-										width={width}
-										height={height}
-										deferredMeasurmentCache={this.cache}
-										rowCount={items.length}
-										rowHeight={({ index }) => this.getRowHeight(items[index])}
-										rowRenderer={rowRenderer}
-										onRowsRendered={onRowsRendered}
-										overscanRowCount={LIMIT}
-										scrollToAlignment="center"
-									/>
-								)}
-							</AutoSizer>
-						)}
-					</InfiniteLoader>
-				</div>
+				{!items.length && !isLoading ? (
+					<EmptySearch text={filter ? U.Common.sprintf(translate('popupSearchEmptyFilter'), filter) : translate('popupSearchEmpty')} />
+				) : ''}
+
+				{this.cache && items.length && !isLoading ? (
+					<div className="items">
+						<InfiniteLoader
+							rowCount={items.length + 1}
+							loadMoreRows={this.loadMoreRows}
+							isRowLoaded={({ index }) => !!items[index]}
+							threshold={LIMIT}
+						>
+							{({ onRowsRendered, registerChild }) => (
+								<AutoSizer className="scrollArea">
+									{({ width, height }) => (
+										<List
+											ref={ref => this.refList = ref}
+											width={width}
+											height={height}
+											deferredMeasurmentCache={this.cache}
+											rowCount={items.length}
+											rowHeight={({ index }) => this.getRowHeight(items[index])}
+											rowRenderer={rowRenderer}
+											onRowsRendered={onRowsRendered}
+											overscanRowCount={LIMIT}
+											scrollToAlignment="center"
+										/>
+									)}
+								</AutoSizer>
+							)}
+						</InfiniteLoader>
+					</div>
+				) : ''}
 			</div>
 		);
 	};
@@ -234,7 +240,7 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 		};
 
 		if (clear) {
-			this.setState({ loading: true });
+			this.setState({ isLoading: true });
 		};
 
 		U.Data.search({
@@ -251,7 +257,7 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 			};
 
 			if (message.error.code) {
-				this.setState({ loading: false });
+				this.setState({ isLoading: false });
 				return;
 			};
 
@@ -266,7 +272,7 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 			this.items = this.items.concat(message.records || []);
 
 			if (clear) {
-				this.setState({ loading: false });
+				this.setState({ isLoading: false });
 			} else {
 				this.forceUpdate();
 			};
@@ -465,12 +471,20 @@ const MenuTypeSuggest = observer(class MenuTypeSuggest extends React.Component<I
 	};
 
 	resize () {
+		const { isLoading } = this.state;
 		const { getId, position, param } = this.props;
 		const { data } = param;
 		const { noFilter } = data;
 		const items = this.getItems();
 		const obj = $(`#${getId()} .content`);
-		const height = Math.min(376, items.reduce((res: number, current: any) => { return res + this.getRowHeight(current); }, 16 + (!noFilter ? 42 : 0)));
+
+		let height = 16 + (noFilter ? 0 : 42);
+		if (!items.length) {
+			height = isLoading ? height + 40 : 160;
+		} else {
+			height = items.reduce((res: number, current: any) => res + this.getRowHeight(current), height);
+		};
+		height = Math.min(height, 376);
 
 		obj.css({ height });
 		position();
