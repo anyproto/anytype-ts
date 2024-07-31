c1cae1242f594cdb4d5d30e98a4bfcfcb2cb90e0
Andrew Simachev <andrew.simachev@gmail.com>
2024-07-31 20:34:08 +0200

    fixed regex

--- src/ts/component/block/text.tsx
+++ src/ts/component/block/text.tsx
@@ -600,7 +600,7 @@ const BlockText = observer(class BlockText extends React.Component<Props> {
 			return;
 		};
 
-		const reg = /\$((?:[^$\\]|\\.)*?)\$[^\d]/g;
+		const reg = /\$((?:[^$\\]|\\.)*?)\$(\s|$)/g;
 
 		let value = this.refEditable.getHtmlValue();
 
