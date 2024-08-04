8a54a4fc22b8cf2c4170cb84da33f3cddd82440c
Andrew Simachev <andrew.simachev@gmail.com>
2024-08-04 22:35:49 +0200

    fix toggle icon position

--- src/ts/lib/sidebar.ts
+++ src/ts/lib/sidebar.ts
@@ -220,10 +220,9 @@ class Sidebar {
 		const pageWidth = ww - width - vw;
 		const ho = keyboard.isMainHistory() ? J.Size.history.panel : 0;
 		const navigation = S.Common.getRef('navigation');
-		const platform = U.Common.getPlatform();
 		
 		let toggleX = width ? width - 40 + (showVault ? vw : 0) : 84;
-		if (!width && isFullScreen || !U.Common.isPlatformMac()) {
+		if (!width && (isFullScreen || !U.Common.isPlatformMac())) {
 			toggleX -= 68;
 		};
 
