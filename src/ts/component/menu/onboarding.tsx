22d8a5d420e0aac272956723f97719b0f524fb59
Andrew Simachev <andrew.simachev@gmail.com>
2024-08-06 10:51:00 +0200

    JS-4945: Onboarding tooltips with dimmer

--- src/ts/component/menu/onboarding.tsx
+++ src/ts/component/menu/onboarding.tsx
@@ -155,6 +155,10 @@ const MenuOnboarding = observer(class MenuSelect extends React.Component<I.Menu,
 		};
 	};
 
+	componentWillUnmount (): void {
+		$('.onboardingDimmer').remove();
+	};
+
 	onClose () {
 		const { param, close } = this.props;
 		const { data } = param;
