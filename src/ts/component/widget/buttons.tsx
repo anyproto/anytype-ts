2a916d873ea3cb141395c91dd2669a8b5bf5d8fb
Andrew Simachev <andrew.simachev@gmail.com>
2024-08-19 09:42:15 +0200

    JS-5072: list members

--- src/ts/component/widget/buttons.tsx
+++ src/ts/component/widget/buttons.tsx
@@ -1,26 +1,45 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Icon } from 'Component';
+import { Icon, IconObject } from 'Component';
 import { I, S, U, translate } from 'Lib';
 
 const WidgetButtons = observer(class WidgetSpace extends React.Component<I.WidgetComponent> {
 
 	render (): React.ReactNode {
 		const items = this.getItems();
+		const space = U.Space.getSpaceview();
+		const participants = U.Space.getParticipantsList([ I.ParticipantStatus.Active ]);
 
 		return (
 			<div className="body">
 				{items.map((item, i) => {
 					let members = null;
-					if (item.id === 'member') {
-						members = <div className="btn">Share</div>;
+					let cnt = null;
+
+					if (item.id == 'member') {
+						if (space.isShared) {
+							members = (
+								<div className="members">
+									{participants.slice(0, 5).map((item, i) => (
+										<IconObject key={item.id} object={item} />
+									))}
+								</div>
+							);
+
+							cnt = <div className="cnt">{participants.length}</div>;
+						} else {
+							members = <div className="btn">Share</div>;
+						};
 					};
 
 					return (
 						<div key={i} className="item" onClick={e => this.onClick(e, item)}>
 							<div className="side left">
 								<Icon className={item.id} />
-								<div className="name">{item.name}</div>
+								<div className="name">
+									{item.name}
+									{cnt}
+								</div>
 							</div>
 							<div className="side right">
 								{members}
