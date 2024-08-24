ef7e9f256aa692b4162887f0cca8cef001f6cb4c
Andrew Simachev <andrew.simachev@gmail.com>
2024-08-24 11:50:54 +0200

    fix vault resize

--- src/ts/lib/sidebar.ts
+++ src/ts/lib/sidebar.ts
@@ -204,10 +204,6 @@ class Sidebar {
 	};
 
 	resizePage (width: number, animate: boolean): void {
-		if (!keyboard.isMain()) {
-			return;
-		};
-
 		this.initObjects();
 
 		if ((width === null) && this.obj && this.obj.length) {
@@ -219,7 +215,7 @@ class Sidebar {
 		const { isClosed } = this.data;
 		const { showVault, isFullScreen } = S.Common;
 		const { ww } = U.Common.getWindowDimensions();
-		const vw = isClosed || !showVault ? 0 : J.Size.vault.width;
+		const vw = isClosed || !showVault || !keyboard.isMain() ? 0 : J.Size.vault.width;
 		const pageWidth = ww - width - vw;
 		const ho = keyboard.isMainHistory() ? J.Size.history.panel : 0;
 		const navigation = S.Common.getRef('navigation');
