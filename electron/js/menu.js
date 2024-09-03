24fccbc2db8dbd01439da279344ea43f1b5590c5
Anatolii Smolianinov <zarkonesmall@gmail.com>
2024-09-03 23:35:45 +0200

    linux, fix menu bar toogle

--- electron/js/menu.js
+++ electron/js/menu.js
@@ -404,11 +404,12 @@ class MenuManager {
 				} 
 			},
 
-			(is.windows || is.linux) ? { 
-				label: Util.translate('electronMenuShowMenu'), type: 'checkbox', checked: !config.hideMenuBar, click: () => { 
-					Api.setMenuBarVisibility(this.win, !config.hideMenuBar);
+			(is.windows || is.linux) ? {
+				label: Util.translate('electronMenuShowMenu'), type: 'checkbox', checked: !config.hideMenuBar, click: () => {
+					Api.setConfig(this.win, { hideMenuBar: !config.hideMenuBar });
+					this.win.setMenuBarVisibility(!config.hideMenuBar);
 					this.initTray();
-				} 
+				}
 			} : null,
 
 			Separator,
