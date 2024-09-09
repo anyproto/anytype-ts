a369f8dec1d82a3cc5e2367d9ab47b7a1b7ba4e1
Andrew Simachev <andrew.simachev@gmail.com>
2024-09-09 15:05:03 +0200

    JS-5257: fix

--- src/ts/component/menu/dataview/filter/values.tsx
+++ src/ts/component/menu/dataview/filter/values.tsx
@@ -418,8 +418,9 @@ const MenuDataviewFilterValues = observer(class MenuDataviewFilterValues extends
 		const view = getView();
 		const filter = view.getFilter(itemId);
 		const isReadonly = this.isReadonly();
+		const key = item.id;
 
-		if (isReadonly) {
+		if (isReadonly || S.Menu.isAnimating('select')) {
 			return;
 		};
 
@@ -428,7 +429,6 @@ const MenuDataviewFilterValues = observer(class MenuDataviewFilterValues extends
 		};
 
 		let options = [];
-		let key = item.id;
 
 		if (item.id == 'relation') {
 			const menuParam = {
@@ -448,7 +448,6 @@ const MenuDataviewFilterValues = observer(class MenuDataviewFilterValues extends
 					this.onChange('relationKey', item.relationKey ? item.relationKey : item.id);
 				}
 			});
-
 			return;
 		};
 
@@ -479,10 +478,10 @@ const MenuDataviewFilterValues = observer(class MenuDataviewFilterValues extends
 					noFilter: true,
 					noVirtualisation: true,
 					rebind: this.rebind,
-					value: item[key],
+					value: item[item.id],
 					options,
 					onSelect: (e: any, el: any) => {
-						this.onChange(key, el.id);
+						this.onChange(item.id, el.id);
 					}
 				}
 			});
