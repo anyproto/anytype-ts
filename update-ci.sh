de802c1e34e58d6e9afcdf2644414081f6c1e5fc
Grigory Efimov <grigory.efimov@gmail.com>
2024-09-10 15:37:57 -0300

    added nightly build support

--- update-ci.sh
+++ update-ci.sh
@@ -1,22 +1,58 @@
 #!/usr/bin/env bash
 
-USER="$1"
-TOKEN="$2"
-OS="$3"
-ARCH="$4"
+# initialize variables
+USER=""
+TOKEN=""
+OS=""
+ARCH=""
+MIDDLEWARE_VERSION=""
 
-if [[ -z $USER || -z $TOKEN || -z $OS || -z $ARCH ]]; then
-    echo "Usage: <GITHUB_USER> <GITHUB_TOKEN> <OS> <ARCH>" 1>&2
+do_usage(){
+    cat <<EOF 1>&2
+
+Usage: $0 --user=<GITHUB_USER> --token=<GITHUB_TOKEN> --os=<OS> --middleware-version=<VERSION> [--arch=<ARCH>]
+
+Options:
+    --user  - github user
+    --token - github token
+    --os    - operating system
+    --arch  - architecture (optional for os windows)
+    --middleware-version - set middleware version
+
+EOF
     exit 1
+}
+
+# process named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --user=*)   USER="${1#*=}";;
+        --token=*)  TOKEN="${1#*=}";;
+        --os=*)     OS="${1#*=}";;
+        --arch=*)   ARCH="${1#*=}";;
+        --middleware-version=*) MIDDLEWARE_VERSION="${1#*=}"; shift;;
+        *)
+            echo "Unknown argument: $1" 1>&2
+            do_usage
+        ;;
+    esac
+    shift
+done
+
+# check args
+if [[ -z $USER || -z $TOKEN || -z $OS || -z $MIDDLEWARE_VERSION ]]; then
+    do_usage
 fi
 
 REPO="anyproto/anytype-heart"
 FILE="addon.tar.gz"
 GITHUB="api.github.com"
 if [[ "$OS" == "ubuntu-latest" ]]; then
+    [[ -z "$ARCH" ]] && do_usage # required for this os
     OS_ARCH="linux-$ARCH"
     FOLDER="$OS_ARCH"
 elif [[ "$OS" == "macos-12" ]]; then
+    [[ -z "$ARCH" ]] && do_usage # required for this os
     OS_ARCH="darwin-$ARCH"
     FOLDER="$OS_ARCH"
 elif [[ "$OS" == "windows-latest" ]]; then
@@ -32,20 +68,25 @@ echo "OS-Arch: $OS_ARCH"
 echo "Folder: $FOLDER"
 echo ""
 
-MMV=$(cat middleware.version)
-VERSION=$(curl --silent --location --user "$USER:$TOKEN" --header "Accept: application/vnd.github.v3+json" "https://$GITHUB/repos/$REPO/releases/tags/v$MMV" | jq .)
-ASSET_ID=$(echo $VERSION | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_${OS_ARCH}\";\"i\")))[0].id")
+if [[ $MIDDLEWARE_VERSION == "nightly" ]]; then
+    echo -n "Downloading file..."
+    curl -sL "$PUBLISH_URL/mw/$ASSET" > $FILE
+    echo "Done"
+else
+    VERSION=$(curl --silent --location --user "$USER:$TOKEN" --header "Accept: application/vnd.github.v3+json" "https://$GITHUB/repos/$REPO/releases/tags/v${MIDDLEWARE_VERSION}" | jq .)
+    ASSET_ID=$(echo $VERSION | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_${OS_ARCH}\";\"i\")))[0].id")
 
-if [ "$ASSET_ID" = "" ]; then
-    echo "ERROR: version not found"
-    exit 1
+    if [[ "$ASSET_ID" == "" ]]; then
+        echo "ERROR: ASSET_ID not found in VERSION='$VERSION'" 1>&2
+        exit 1
+    fi
+
+    echo "Version: $VERSION"
+    echo "Found asset: $ASSET_ID"
+    echo -n "Downloading file..."
+    curl --silent --location --header "Authorization: token $TOKEN" --header "Accept: application/octet-stream" "https://$GITHUB/repos/$REPO/releases/assets/$ASSET_ID" > $FILE
+    echo "Done"
 fi
-
-echo "Version: $VERSION"
-echo "Found asset: $ASSET_ID"
-echo -n "Downloading file..."
-curl --silent --location --header "Authorization: token $TOKEN" --header "Accept: application/octet-stream" "https://$GITHUB/repos/$REPO/releases/assets/$ASSET_ID" > $FILE
-echo "Done"
 
 if [[ "$OS" = "windows-latest" ]]; then
     echo -n "Uncompressing... "
