6f1e17ce0dc389d19b5afba474f18ff1cb49629c
Grigory Efimov <grigory.efimov@gmail.com>
2024-09-09 22:29:29 -0300

    update-ci.sh: refactoring

--- update-ci.sh
+++ update-ci.sh
@@ -1,71 +1,69 @@
 #!/usr/bin/env bash
 
+USER="$1"
+TOKEN="$2"
+OS="$3"
+ARCH="$4"
+
+if [[ -z $USER || -z $TOKEN || -z $OS || -z $ARCH ]]; then
+    echo "Usage: <GITHUB_USER> <GITHUB_TOKEN> <OS> <ARCH>" 1>&2
+    exit 1
+fi
+
 REPO="anyproto/anytype-heart"
 FILE="addon.tar.gz"
 GITHUB="api.github.com"
+if [[ "$OS" == "ubuntu-latest" ]]; then
+    OS_ARCH="linux-$ARCH"
+    FOLDER="$OS_ARCH"
+elif [[ "$OS" == "macos-12" ]]; then
+    OS_ARCH="darwin-$ARCH"
+    FOLDER="$OS_ARCH"
+elif [[ "$OS" == "windows-latest" ]]; then
+    OS_ARCH="windows"
+    FOLDER="dist"
+    FILE="addon.zip"
+else
+    echo "Unsupported OS='$OS'" 1>&2
+    exit 1
+fi
 
-user=$1
-token=$2;
-platform=${3:-ubuntu-latest};
-arch=$4;
-folder="build";
-
-if [ "$platform" = "ubuntu-latest" ]; then
-  arch="linux-$arch";
-  folder="$arch";
-elif [ "$platform" = "macos-12" ]; then
-  arch="darwin-$arch";
-  folder="$arch";
-elif [ "$platform" = "windows-latest" ]; then
-  arch="windows";
-  folder="dist";
-  FILE="addon.zip"
-fi;
-
-echo "Arch: $arch"
-echo "Folder: $folder"
+echo "OS-Arch: $OS_ARCH"
+echo "Folder: $FOLDER"
 echo ""
 
-if [ "$arch" = "" ]; then
-  echo "ERROR: arch not found"
-  exit 1
-fi;
+MMV=$(cat middleware.version)
+VERSION=$(curl --silent --location --user "$USER:$TOKEN" --header "Accept: application/vnd.github.v3+json" "https://$GITHUB/repos/$REPO/releases/tags/v$MMV" | jq .)
+ASSET_ID=$(echo $VERSION | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_${OS_ARCH}\";\"i\")))[0].id")
 
-mwv=`cat middleware.version`
+if [ "$ASSET_ID" = "" ]; then
+    echo "ERROR: version not found"
+    exit 1
+fi
 
-version=`curl -u "$user:$token" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases/tags/v$mwv | jq .`
-
-tag=`echo $version | jq ".tag_name"`
-asset_id=`echo $version | jq ".assets | map(select(.name | match(\"js_v[0-9]+.[0-9]+.[0-9]+(-rc[0-9]+)?_$arch\";\"i\")))[0].id"`
-
-if [ "$asset_id" = "" ]; then
-  echo "ERROR: version not found"
-  exit 1
-fi;
-
-printf "Version: $tag\n"
-printf "Found asset: $asset_id\n"
+echo "Version: $VERSION"
+echo "Found asset: $ASSET_ID"
 echo -n "Downloading file..."
-curl -sL -H "Authorization: token $token" -H 'Accept: application/octet-stream' "https://$GITHUB/repos/$REPO/releases/assets/$asset_id" > $FILE
-printf "Done\n"
+curl --silent --location --header "Authorization: token $TOKEN" --header "Accept: application/octet-stream" "https://$GITHUB/repos/$REPO/releases/assets/$ASSET_ID" > $FILE
+echo "Done"
 
-if [ "$platform" = "windows-latest" ]; then
-  echo -n "Uncompressing... "
-  unzip $FILE
-  printf "Done\n"
-  
-  echo "Moving... "
-  mv -fv grpc-server.exe "$folder/anytypeHelper.exe"
-else 
-  echo -n "Uncompressing... " 
-  tar -zxf $FILE
-  printf "Done\n"
+if [[ "$OS" = "windows-latest" ]]; then
+    echo -n "Uncompressing... "
+    unzip $FILE
+    echo "Done"
 
-  echo "Moving... "
-  rm -rf "$folder"
-  mkdir -p "$folder"
-  mv -fv grpc-server "$folder/anytypeHelper"
-fi;
+    echo "Moving... "
+    mv -fv grpc-server.exe "$FOLDER/anytypeHelper.exe"
+else
+    echo -n "Uncompressing... "
+    tar -zxf $FILE
+    echo "Done"
+
+    echo "Moving... "
+    rm -rf "$FOLDER"
+    mkdir -p "$FOLDER"
+    mv -fv grpc-server "$FOLDER/anytypeHelper"
+fi
 
 rm -rf dist/lib/pb
 rm -rf dist/lib/pkg
