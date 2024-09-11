2526349552e77add710cabbf4b4e70073f853e6f
Andrew Simachev <andrew.simachev@gmail.com>
2024-09-11 13:59:00 +0200

    fix preview

--- src/ts/component/popup/preview.tsx
+++ src/ts/component/popup/preview.tsx
@@ -193,11 +193,14 @@ class PopupPreview extends React.Component<I.Popup> {
 		let w = 0, h = 0;
 		if (width > height) {
 			w = Math.min(maxWidth, width);
-			h = Math.min(maxHeight, w / (width / height));
+			h = w / (width / height);
 		} else {
 			h = Math.min(maxHeight, height);
-			w = Math.min(maxWidth, h / (height / width));
+			w = h / (height / width);
 		};
+
+		console.log('mw', maxWidth, 'mh', maxHeight, 'width', width, 'height', height);
+		console.log('w', w, 'h', h);
 
 		wrap.css({ width: w, height: h });
 		position();
