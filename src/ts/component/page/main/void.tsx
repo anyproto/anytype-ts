79da9a87c85723c7466332d58c45104fed1c3325
Mike Mhlv <mhlv@anytype.io>
2024-09-24 12:05:03 +0100

    JS-5433: void layout

--- src/ts/component/page/main/void.tsx
+++ src/ts/component/page/main/void.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Title } from 'Component';
-import { I } from 'Lib';
+import { Icon, Title, Label, Button } from 'Component';
+import { I, S, Storage, translate, U } from 'Lib';
 
 const PageMainVoid = observer(class PageMainVoid extends React.Component<I.PageComponent> {
 
@@ -13,9 +13,29 @@ const PageMainVoid = observer(class PageMainVoid extends React.Component<I.PageC
 				ref={node => this.node = node}
 				className="wrapper"
 			>
-				<Title text="THIS IS VOID!" />
+				<div className="container">
+					<div className="iconWrapper red">
+						<Icon />
+					</div>
+					<Title text={translate('pageMainVoidTitle')} />
+					<Label text={translate('pageMainVoidText')} />
+					<Button onClick={this.onClick} className="c36" text={translate('pageMainVoidCreateSpace')} />
+				</div>
 			</div>
 		);
+	};
+
+	onClick () {
+		S.Popup.open('settings', {
+			className: 'isSpaceCreate',
+			data: {
+				page: 'spaceCreate',
+				isSpace: true,
+				onCreate: (id) => {
+					U.Router.switchSpace(id, '', true, () => Storage.initPinnedTypes());
+				},
+			},
+		});
 	};
 	
 });
