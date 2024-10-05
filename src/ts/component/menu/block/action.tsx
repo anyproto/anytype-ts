e231ba914b18458a2b0382f9b9618a0b19274adc
Andrew Simachev <andrew.simachev@gmail.com>
2024-10-05 10:38:36 +0200

    JS-5551: fix

--- src/ts/component/menu/block/action.tsx
+++ src/ts/component/menu/block/action.tsx
@@ -417,6 +417,7 @@ class MenuBlockAction extends React.Component<I.Menu, State> {
 					filter: '',
 					filters: [
 						{ relationKey: 'recommendedLayout', condition: I.FilterCondition.In, value: U.Object.getPageLayouts() },
+						{ relationKey: 'uniqueKey', condition: I.FilterCondition.NotEqual, value: J.Constant.typeKey.template },
 					],
 					onClick: (item: any) => {
 						this.moveToPage(item.id);
