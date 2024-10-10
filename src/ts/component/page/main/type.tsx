a40d627fde5a94d7a5b746853a2247605d468f50
Andrew Simachev <andrew.simachev@gmail.com>
2024-10-10 11:41:37 +0200

    fix type subscription

--- src/ts/component/page/main/type.tsx
+++ src/ts/component/page/main/type.tsx
@@ -48,7 +48,7 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 		const { config } = S.Common;
 		const rootId = this.getRootId();
 		const check = U.Data.checkDetails(rootId);
-		const object = S.Record.getTypeById(rootId);
+		const object = S.Detail.get(rootId, rootId, J.Relation.type);
 
 		if (!object) {
 			return null;
