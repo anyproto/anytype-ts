b8d15e1f67498bcec12f5da652d2b9c707442af5
Andrew Simachev <andrew.simachev@gmail.com>
2024-10-14 10:40:27 +0200

    make checkbox clickable in tasks

--- src/ts/component/sidebar/object/item.tsx
+++ src/ts/component/sidebar/object/item.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import $ from 'jquery';
 import { observer } from 'mobx-react';
 import { IconObject, ObjectName, ObjectDescription, ObjectType, Icon } from 'Component';
-import { U, S } from 'Lib';
+import { I, U, S } from 'Lib';
 
 interface Props {
 	item: any;
@@ -25,6 +25,7 @@ const ObjectItem = observer(class ObjectItem extends React.Component<Props> {
 		const type = S.Record.getTypeById(item.type);
 		const isFile = U.Object.isInFileLayouts(item.layout);
 		const isTypeOrRelation = U.Object.isTypeOrRelationLayout(item.layout);
+		const canEdit = U.Object.isTaskLayout(item.layout) && S.Block.isAllowed(item.restrictions, [ I.RestrictionObject.Details ]);
 
 		if (compact) {
 			cn.push('isCompact');
@@ -36,7 +37,7 @@ const ObjectItem = observer(class ObjectItem extends React.Component<Props> {
 		let content = null;
 
 		if (compact) {
-			iconSmall = <IconObject object={item} size={18} iconSize={18} />;
+			iconSmall = <IconObject object={item} size={18} iconSize={18} canEdit={canEdit} />;
 		} else {
 			const iconSize = isFile ? 48 : null;
 
