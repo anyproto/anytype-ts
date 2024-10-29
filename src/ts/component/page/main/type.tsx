30998ea2b64a3f3ed06f1db7057220753fc36047
Andrew Simachev <andrew.simachev@gmail.com>
2024-10-29 15:41:09 +0100

    remove redundant code from PageMainType

--- src/ts/component/page/main/type.tsx
+++ src/ts/component/page/main/type.tsx
@@ -32,11 +32,9 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 		
 		this.onTemplateAdd = this.onTemplateAdd.bind(this);
 		this.onObjectAdd = this.onObjectAdd.bind(this);
-		this.onRelationAdd = this.onRelationAdd.bind(this);
 		this.onSetAdd = this.onSetAdd.bind(this);
 		this.onCreate = this.onCreate.bind(this);
 		this.onEdit = this.onEdit.bind(this);
-		this.onLayout = this.onLayout.bind(this);
 	};
 
 	render () {
@@ -46,7 +44,6 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 			return <Deleted {...this.props} />;
 		};
 
-		const { config } = S.Common;
 		const rootId = this.getRootId();
 		const check = U.Data.checkDetails(rootId);
 		const object = S.Detail.get(rootId, rootId, J.Relation.type);
@@ -60,31 +57,16 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 		const canWrite = U.Space.canMyParticipantWrite();
 		const isTemplate = object.uniqueKey == J.Constant.typeKey.template;
 
-		const layout: any = U.Menu.getLayouts().find(it => it.id == object.recommendedLayout) || {};
 		const showTemplates = !U.Object.getLayoutsWithoutTemplates().includes(object.recommendedLayout) && !isTemplate;
 		const recommendedRelations = Relation.getArrayValue(object.recommendedRelations);
 		const recommendedKeys = recommendedRelations.map(id => S.Record.getRelationById(id)).map(it => it && it.relationKey);
 
 		const allowedObject = object.isInstalled && U.Object.isInPageLayouts(object.recommendedLayout);
-		const allowedDetails = object.isInstalled && S.Block.checkFlags(rootId, rootId, [ I.RestrictionObject.Details ]);
-		const allowedRelation = object.isInstalled && S.Block.checkFlags(rootId, rootId, [ I.RestrictionObject.Relation ]) && !U.Object.isParticipantLayout(object.recommendedLayout);
 		const allowedTemplate = object.isInstalled && allowedObject && showTemplates && canWrite && !isTemplate;
-		const allowedLayout = ![ I.ObjectLayout.Bookmark, I.ObjectLayout.Chat, I.ObjectLayout.Participant ].includes(object.recommendedLayout);
 		
 		const subIdObject = this.getSubIdObject();
 		const totalObject = S.Record.getMeta(subIdObject, '').total;
 		const totalTemplate = templates.length + (allowedTemplate ? 1 : 0);
-
-		if (!recommendedRelations.includes('rel-description')) {
-			recommendedRelations.push('rel-description');
-		};
-
-		const relations = recommendedRelations.map(id => S.Record.getRelationById(id)).filter(it => {
-			if (!it) {
-				return false;
-			};
-			return config.debug.hiddenObject ? true : !it.isHidden;
-		});
 
 		const isFileType = U.Object.isInFileLayouts(object.recommendedLayout);
 		const columns: any[] = [
@@ -97,25 +79,6 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 		if (!isFileType) {
 			columns.push({ relationKey: 'creator', name: translate('commonOwner'), isObject: true });
 		};
-
-		const ItemRelation = (item: any) => (
-			<div id={'item-' + item.id} className={[ 'item', (item.isHidden ? 'isHidden' : ''), 'canEdit' ].join(' ')}>
-				<div className="clickable" onClick={e => this.onRelationEdit(e, item.id)}>
-					<Icon className={[ 'relation', Relation.className(item.format) ].join(' ')} />
-					<div className="name">{item.name}</div>
-				</div>
-			</div>
-		);
-
-		const ItemAdd = () => (
-			<div id="item-add" className="item add" onClick={this.onRelationAdd}>
-				<div className="clickable">
-					<Icon className="plus" />
-					<div className="name">{translate('commonAddRelation')}</div>
-				</div>
-				<div className="value" />
-			</div>
-		);
 
 		return (
 			<div>
@@ -173,38 +136,6 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 							)}
 						</div>
 					) : ''}
-
-					{allowedLayout ? (
-						<div className="section layout">
-							<div className="title">{translate('pageMainTypeRecommendedLayout')}</div>
-							<div className="content">
-								{allowedDetails ? (
-									<Select 
-										id="recommendedLayout" 
-										value={object.recommendedLayout} 
-										options={U.Menu.turnLayouts()} 
-										arrowClassName="light" 
-										onChange={this.onLayout} 
-									/>
-								) : (
-									<React.Fragment>
-										<Icon className={layout.icon} />
-										<div className="name">{layout.name}</div>
-									</React.Fragment>
-								)}
-							</div>
-						</div>
-					) : ''}
-
-					<div className="section relation">
-						<div className="title">{relations.length} {U.Common.plural(relations.length, translate('pluralRelation'))}</div>
-						<div className="content">
-							{relations.map((item: any, i: number) => (
-								<ItemRelation key={i} {...item} />
-							))}
-							{allowedRelation ? <ItemAdd /> : ''}
-						</div>
-					</div>
 
 					{object.isInstalled && !object._empty_ ? (
 						<div className="section set">
@@ -442,71 +373,6 @@ const PageMainType = observer(class PageMainType extends React.Component<I.PageC
 				U.Object.openConfig(message.details);
 			};
 		});
-	};
-
-	onRelationAdd (e: any) {
-		const rootId = this.getRootId();
-		const object = S.Detail.get(rootId, rootId);
-		const skipSystemKeys = [ 'tag', 'description', 'source' ];
-		const recommendedKeys = object.recommendedRelations.map(id => S.Record.getRelationById(id)).map(it => it && it.relationKey);
-		const systemKeys = Relation.systemKeys().filter(it => !skipSystemKeys.includes(it));
-
-		S.Menu.open('relationSuggest', { 
-			element: '#page .section.relation #item-add',
-			offsetX: 32,
-			data: {
-				filter: '',
-				rootId,
-				ref: 'type',
-				menuIdEdit: 'blockRelationEdit',
-				skipKeys: recommendedKeys.concat(systemKeys),
-				addCommand: (rootId: string, blockId: string, relation: any, onChange: (message: any) => void) => {
-					C.ObjectTypeRelationAdd(rootId, [ relation.relationKey ], (message: any) => { 
-						S.Menu.close('relationSuggest'); 
-
-						if (onChange) {
-							onChange(message);
-						};
-					});
-				},
-			}
-		});
-	};
-
-	onRelationEdit (e: any, id: string) {
-		const rootId = this.getRootId();
-		const allowed = S.Block.checkFlags(rootId, rootId, [ I.RestrictionObject.Relation ]);
-		const relation = S.Record.getRelationById(id);
-		
-		S.Menu.open('blockRelationEdit', { 
-			element: `#page .section.relation #item-${id}`,
-			offsetX: 32,
-			data: {
-				rootId,
-				relationId: id,
-				readonly: !allowed,
-				ref: 'type',
-				addCommand: (rootId: string, blockId: string, relation: any, onChange?: (relation: any) => void) => {
-					C.ObjectTypeRelationAdd(rootId, [ relation.relationKey ], () => {
-						if (onChange) {
-							onChange(relation.relationKey);
-						};
-					});
-				},
-				deleteCommand: () => {
-					C.ObjectTypeRelationRemove(rootId, [ relation.relationKey ]);
-				},
-			}
-		});
-	};
-
-	onLayout (layout: string) {
-		const rootId = this.getRootId();
-
-		C.ObjectListSetDetails([ rootId ], [ 
-			{ key: 'recommendedLayout', value: Number(layout) || I.ObjectLayout.Page } 
-		]);
-		analytics.event('ChangeRecommendedLayout', { objectType: rootId, layout: layout });
 	};
 
 	onMenu (item: any) {
