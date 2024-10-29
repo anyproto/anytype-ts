4e0ba5158017761d568d813451ffaecd0ed8f8bd
Andrew Simachev <andrew.simachev@gmail.com>
2024-10-29 12:15:55 +0100

    JS-5693: relations section

--- src/ts/component/sidebar/section/type/relation.tsx
+++ src/ts/component/sidebar/section/type/relation.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Title, Label, Icon } from 'Component';
-import { I, translate } from 'Lib';
+import { Title, Label, Icon, ObjectName, IconObject } from 'Component';
+import { I, S, Relation, translate } from 'Lib';
 
 const SidebarSectionTypeRelation = observer(class SidebarSectionTypeRelation extends React.Component<I.SidebarSectionComponent> {
 	
     render () {
 		const { object } = this.props;
+		const recommendedRelations = Relation.getArrayValue(object.recommendedRelations);
+		const recommendedKeys = recommendedRelations.map(id => S.Record.getRelationById(id)).map(it => it && it.relationKey);
+
+		const Item = (item: any) => (
+			<div className="item">
+				<div className="side left">
+					<Icon className="dnd" />
+					<IconObject object={item} />
+					<ObjectName object={item} />
+				</div>
+				<div className="side right">
+					<Icon className="eye" />
+				</div>
+			</div>
+		);
 
         return (
 			<div className="wrap">
@@ -21,6 +36,11 @@ const SidebarSectionTypeRelation = observer(class SidebarSectionTypeRelation ext
 
 				<Label text={translate('sidebarTypeRelationSidebar')} />
 				<div className="items">
+					{recommendedKeys.map(key => {
+						const relation = S.Record.getRelationByKey(key);
+
+						return <Item key={`sidebar-${key}`} {...relation} />;
+					})}
 				</div>
 			</div>
 		);
