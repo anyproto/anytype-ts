de338d02d7cf1cb620cdece00e66ab6f81235208
Andrew Simachev <andrew.simachev@gmail.com>
2024-11-01 18:08:58 +0100

    revert bin button

--- src/ts/component/widget/buttons.tsx
+++ src/ts/component/widget/buttons.tsx
@@ -3,8 +3,6 @@ import { observer } from 'mobx-react';
 import { Icon } from 'Component';
 import { I, S, U, sidebar, translate } from 'Lib';
 
-const SUB_ID = 'widgetButtons';
-
 const WidgetButtons = observer(class WidgetSpace extends React.Component<I.WidgetComponent> {
 
 	isSubcribed = false;
@@ -58,13 +56,8 @@ const WidgetButtons = observer(class WidgetSpace extends React.Component<I.Widge
 		);
 	};
 
-	componentDidUpdate	(): void {
-		this.subscribeArchive();
-	};
-
 	getItems () {
 		const space = U.Space.getSpaceview();
-		const archived = S.Record.getRecordIds(SUB_ID, '');
 		const ret = [
 			{ id: 'all', name: translate('commonAllContent') },
 		];
@@ -75,10 +68,6 @@ const WidgetButtons = observer(class WidgetSpace extends React.Component<I.Widge
 
 		if (space.spaceMainChatId) {
 			ret.push({ id: 'chat', name: translate('commonMainChat') });
-		};
-
-		if (archived.length) {
-			ret.push({ id: 'bin', name: translate('commonBin') });
 		};
 
 		return ret;
@@ -105,32 +94,9 @@ const WidgetButtons = observer(class WidgetSpace extends React.Component<I.Widge
 				U.Object.openAuto({ id: space.spaceMainChatId, layout: I.ObjectLayout.Chat });
 				break;
 			};
-
-			case 'bin': {
-				U.Object.openAuto({ layout: I.ObjectLayout.Archive });
-				break;
-			};
 		};
 	};
 
-	subscribeArchive () {
-		if (this.isSubcribed) {
-			return;
-		};
-
-		this.isSubcribed = true;
-
-		U.Data.searchSubscribe({
-			subId: SUB_ID,
-			spaceId: S.Common.space,
-			withArchived: true,
-			filters: [
-				{ relationKey: 'isArchived', condition: I.FilterCondition.Equal, value: true },
-			],
-			limit: 1,
-		}, () => {});
-	};
-	
 });
 
 export default WidgetButtons;
