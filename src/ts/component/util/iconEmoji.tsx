830b8f8cce2b1a4df96df7909bdaa8edf5aa1639
Andrew Simachev <andrew.simachev@gmail.com>
2024-11-25 12:09:36 +0100

    iconEmoji as functional component

--- src/ts/component/util/iconEmoji.tsx
+++ src/ts/component/util/iconEmoji.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import { S, U, J } from 'Lib';
 
 interface Props {
@@ -9,58 +9,45 @@ interface Props {
 	asImage?: boolean;
 	className?: string;
 	canEdit?: boolean;
-	offsetX?: number;
-	offsetY?: number;
-}
-
-class IconEmoji extends React.Component<Props> {
-	
-	public static defaultProps = {
-		offsetX: 0,
-		offsetY: 0,
-		size: 18,
-		className: '',
-		asImage: true,
-	};
-	
-	render () {
-		const { id, size, icon, objectId, asImage, className, canEdit } = this.props;
-		const cn = [ 'iconEmoji' ];
-		const css = { lineHeight: size + 'px' };
-
-		if (className) {
-			cn.push(className);
-		};
-		if (canEdit) {
-			cn.push('canEdit');
-		};
-
-		let element = null;
-		if (icon) {
-			const code = icon.match(':') ? icon : U.Smile.getCode(icon);
-			if (code) {
-				if (asImage) {
-					element = <img src={U.Smile.srcFromColons(code)} className={[ 'smileImage', 'c' + size ].join(' ')} onDragStart={e=> e.preventDefault()} />;
-				} else {
-					element = <em-emoji shortcodes={code}></em-emoji>;
-				};
-			};
-		} else 
-		if (objectId) {
-			element = <img src={S.Common.imageUrl(objectId, J.Size.iconPage)} className={[ 'iconImage', 'c' + size ].join(' ')} onDragStart={e => e.preventDefault()} />;
-		};
-
-		if (!element) {
-			return null;
-		};
-		
-		return (
-			<div id={id} style={css} className={cn.join(' ')}>
-				{element}
-			</div>
-		);
-	};
-
 };
+
+const IconEmoji = forwardRef<HTMLDivElement, Props>(({
+	id = '',
+	icon = '',
+	objectId = '',
+	size = 18,
+	asImage = true,
+	className = '',
+	canEdit = false,
+}, ref) => {
+	const cn = [ 'iconEmoji', className ];
+	const css = { lineHeight: `${size}px` };
+
+	if (canEdit) {
+		cn.push('canEdit');
+	};
+
+	let element = null;
+	if (icon) {
+		const code = icon.match(':') ? icon : U.Smile.getCode(icon);
+		if (code) {
+			if (asImage) {
+				element = <img src={U.Smile.srcFromColons(code)} className={[ 'smileImage', 'c' + size ].join(' ')} onDragStart={e=> e.preventDefault()} />;
+			} else {
+				element = <em-emoji shortcodes={code}></em-emoji>;
+			};
+		};
+	} else 
+	if (objectId) {
+		element = <img src={S.Common.imageUrl(objectId, J.Size.iconPage)} className={[ 'iconImage', 'c' + size ].join(' ')} onDragStart={e => e.preventDefault()} />;
+	};
+
+	return element ? (
+		<div id={id} style={css} className={cn.join(' ')}>
+			{element}
+		</div>
+	) : null;
+
+});
 
 export default IconEmoji;
