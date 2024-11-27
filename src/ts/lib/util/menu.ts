2cc577da12497cbe8f6a56c85f212f0dca89cf02
Andrew Simachev <andrew.simachev@gmail.com>
2024-11-27 09:42:05 +0100

    fix widget layout options for date

--- src/ts/lib/util/menu.ts
+++ src/ts/lib/util/menu.ts
@@ -409,7 +409,7 @@ class UtilMenu {
 			if (!isSystem) {
 				const isSet = U.Object.isInSetLayouts(layout);
 				const setLayouts = U.Object.getSetLayouts();
-				const treeSkipLayouts = setLayouts.concat(U.Object.getFileAndSystemLayouts()).concat([ I.ObjectLayout.Participant ]);
+				const treeSkipLayouts = setLayouts.concat(U.Object.getFileAndSystemLayouts()).concat([ I.ObjectLayout.Participant, I.ObjectLayout.Date ]);
 
 				// Sets can only become Link and List layouts, non-sets can't become List
 				if (treeSkipLayouts.includes(layout)) {
