ae41412ee0ff09e830ca4ff54e7438f755f55eaa
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-04 01:17:46 +0100

    JS-4200: BlockIconPage as functional component

--- src/ts/component/block/iconPage.tsx
+++ src/ts/component/block/iconPage.tsx
@@ -1,23 +1,22 @@
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import { observer } from 'mobx-react';
 import { IconObject } from 'Component';
 import { I, S } from 'Lib';
 
-const BlockIconPage = observer(class BlockIconPage extends React.Component<I.BlockComponent> {
-
-	render (): any {
-		const { rootId, readonly } = this.props;
+const BlockIconPage = observer(forwardRef<{}, I.BlockComponent>(({
+	rootId = '',
+	readonly = false,
+}, ref) => {
 		
-		return (
-			<IconObject 
-				id={`block-icon-${rootId}`} 
-				canEdit={!readonly} 
-				getObject={() => S.Detail.get(rootId, rootId, [])} 
-				size={96} 
-			/>
-		);
-	};
+	return (
+		<IconObject 
+			id={`block-icon-${rootId}`} 
+			canEdit={!readonly} 
+			getObject={() => S.Detail.get(rootId, rootId, [])} 
+			size={96} 
+		/>
+	);
 
-});
+}));
 
 export default BlockIconPage;
