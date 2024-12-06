ac52dcdef97155dea12308c31d141e1111b1d05e
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-06 21:18:27 +0100

    JS-4200: FooterMainObject as functional component

--- src/ts/component/footer/main/object.tsx
+++ src/ts/component/footer/main/object.tsx
@@ -1,61 +1,60 @@
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import { observer } from 'mobx-react';
 import { PieChart } from 'react-minimal-pie-chart';
 import { Icon } from 'Component';
 import { I, J, S, Preview, translate } from 'Lib';
 
-const FooterMainEdit = observer(class FooterMainEdit extends React.Component<I.FooterComponent> {
-	
-	render () {
-		const { onHelp } = this.props;
-		const { show } = S.Progress;
-		const theme = S.Common.getThemeClass();
-		const current = S.Progress.getCurrent();
-		const total = S.Progress.getTotal();
-		const percent = Math.round((current / total) * 100);
-		const color = J.Theme[theme].progress;
+const FooterMainObject = observer(forwardRef<{}, I.FooterComponent>((props, ref) => {
 
-		return (
-			<div className="buttons">
-				{total ? (
-					<div 
-						id="button-progress"
-						className="iconWrap"
-						onClick={() => S.Progress.showSet(!show)}
-					>
-						<div className="inner">{percent}</div>
-						<PieChart
-							totalValue={100}
-							startAngle={270}
-							lengthAngle={-360}
-							data={[ 
-								{ title: '', value: 100 - percent, color: color.bg },
-								{ title: '', value: percent, color: color.fg },
-							]}
-						/>
-					</div>
-				) : ''}
-				<div 
-					id="button-help" 
-					className="iconWrap" 
-					onClick={onHelp}
-					onMouseEnter={e => this.onTooltipShow(e, translate('commonHelp'))}
-					onMouseLeave={() => Preview.tooltipHide(false)}
-				>
-					<Icon />
-					<div className="bg" />
-				</div>
-			</div>
-		);
-	};
+	const { onHelp } = props;
+	const { show } = S.Progress;
+	const theme = S.Common.getThemeClass();
+	const current = S.Progress.getCurrent();
+	const total = S.Progress.getTotal();
+	const percent = Math.round((current / total) * 100);
+	const color = J.Theme[theme].progress;
 
-	onTooltipShow (e: any, text: string, caption?: string) {
+	const onTooltipShow = (e: any, text: string, caption?: string) => {
 		const t = Preview.tooltipCaption(text, caption);
 		if (t) {
 			Preview.tooltipShow({ text: t, element: $(e.currentTarget), typeY: I.MenuDirection.Top });
 		};
 	};
 
-});
+	return (
+		<div className="buttons">
+			{total ? (
+				<div 
+					id="button-progress"
+					className="iconWrap"
+					onClick={() => S.Progress.showSet(!show)}
+				>
+					<div className="inner">{percent}</div>
+					<PieChart
+						totalValue={100}
+						startAngle={270}
+						lengthAngle={-360}
+						data={[ 
+							{ title: '', value: 100 - percent, color: color.bg },
+							{ title: '', value: percent, color: color.fg },
+						]}
+					/>
+				</div>
+			) : ''}
 
-export default FooterMainEdit;
+			<div 
+				id="button-help" 
+				className="iconWrap" 
+				onClick={onHelp}
+				onMouseEnter={e => onTooltipShow(e, translate('commonHelp'))}
+				onMouseLeave={() => Preview.tooltipHide(false)}
+			>
+				<Icon />
+				<div className="bg" />
+			</div>
+		</div>
+	);
+
+}));
+
+export default FooterMainObject;
