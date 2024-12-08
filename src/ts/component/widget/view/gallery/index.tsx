f3f737bdeaefcb7683f294ea4066d5b5b82bcf41
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-08 09:52:50 +0100

    JS-4200: WidgetViewGalleryIndex as functional component

--- src/ts/component/widget/view/gallery/index.tsx
+++ src/ts/component/widget/view/gallery/index.tsx
@@ -1,39 +1,30 @@
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import { observer } from 'mobx-react';
 import { I, S, U, J } from 'Lib';
 import Item from './item';
 
-const WidgetViewGallery = observer(class WidgetViewGallery extends React.Component<I.WidgetViewComponent> {
+const WidgetViewGallery = observer(forwardRef<{}, I.WidgetViewComponent>((props, ref) => {
+	
+	const { block, subId, getView, getRecordIds } = props;
+	const view = getView();
+	const items = getRecordIds().map(id => S.Detail.get(subId, id, J.Relation.sidebar));
 
-	node = null;
-
-	render (): React.ReactNode {
-		const { block, subId, getView } = this.props;
-		const view = getView();
-		const items = this.getItems();
-
-		return (
-			<div ref={ref => this.node = ref} className="body">
-				<div id="items" className="items">
-					{items.map(item => (
-						<Item 
-							{...this.props}
-							key={`widget-${block.id}-item-${item.id}`} 
-							subId={subId}
-							id={item.id} 
-							hideIcon={view.hideIcon}
-						/>
-					))}
-				</div>
+	return (
+		<div className="body">
+			<div id="items" className="items">
+				{items.map(item => (
+					<Item 
+						{...props}
+						key={`widget-${block.id}-item-${item.id}`} 
+						subId={subId}
+						id={item.id} 
+						hideIcon={view.hideIcon}
+					/>
+				))}
 			</div>
-		);
-	};
+		</div>
+	);
 
-	getItems () {
-		const { getRecordIds, subId } = this.props;
-		return getRecordIds().map(id => S.Detail.get(subId, id, J.Relation.sidebar));
-	};
-
-});
+}));
 
 export default WidgetViewGallery;
