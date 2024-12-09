831b044d65d6be06340727ce9652d74239ba0d8f
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-09 09:58:36 +0100

    JS-4200: PreviewLink as functional component

--- src/ts/component/preview/link.tsx
+++ src/ts/component/preview/link.tsx
@@ -1,112 +1,78 @@
-import * as React from 'react';
-import $ from 'jquery';
+import React, { forwardRef, useState, useEffect, useRef } from 'react';
 import { Loader } from 'Component';
 import { C, U } from 'Lib';
-import { observer } from 'mobx-react';
 
 interface Props {
 	url: string;
 	position?: () => void;
 };
 
-interface State {
-	loading: boolean;
+interface Info {
 	title: string;
 	description: string;
-	faviconUrl: string;
 	imageUrl: string;
 };
 
 const ALLOWED_SCHEME = [ 'http', 'https' ];
 
-const PreviewLink = observer(class PreviewLink extends React.Component<Props, State> {
-	
-	_isMounted = false;
-	node: any = null;
-	state = {
-		loading: false,
-		title: '',
-		description: '',
-		faviconUrl: '',
-		imageUrl: '',
+const PreviewLink = forwardRef<{}, Props>(({ url = '', position }, ref: any) => {
+
+	const [ object, setObject ] = useState({} as Info);
+	const [ isLoading, setIsLoading ] = useState(false);
+	const { title, description, imageUrl } = object;
+	const cn = [ 'previewLink' ];
+	const urlRef = useRef('');
+
+	if (imageUrl) {
+		cn.push('withImage');
 	};
-	url: string;
-	
-	render () {
-		const { url } = this.props;
-		const { loading, title, description } = this.state;
+
+	const load = () => {
+		if (urlRef.current == url) {
+			return;
+		};
 		
-		return (
-			<div ref={node => this.node = node} className="previewLink">
-				{loading ? <Loader /> : (
-					<React.Fragment>
-						<div className="info">
-							<div className="link">{U.Common.shortUrl(url)}</div>
-							{title ? <div className="name">{title}</div> : ''}
-							{description ? <div className="descr">{description}</div> : ''}
-						</div>
-					</React.Fragment>
-				)}
-			</div>
-		);
+		const scheme = U.Common.getScheme(url);
+
+		if (scheme && !ALLOWED_SCHEME.includes(scheme)) {
+			return;
+		};
+
+		urlRef.current = url;
+		setIsLoading(true);
+
+		C.LinkPreview(url, (message: any) => {
+			setIsLoading(false);
+
+			if (!message.error.code) {
+				setObject(message.previewLink);
+			} else {
+				urlRef.current = '';
+			};
+		});
 	};
-	
-	componentDidMount () {
-		this._isMounted = true;
-		this.load();
-	};
 
-	componentDidUpdate () {
-		const { position } = this.props;
-		const { imageUrl } = this.state;
-
-		$(this.node).toggleClass('withImage', !!imageUrl);
-
-		this.load();
+	useEffect(() => {
+		load();
 
 		if (position) {
 			position();
 		};
-	};
-
-	componentWillUnmount () {
-		this._isMounted = false;
-	};
-
-	load () {
-		const { url } = this.props;
-
-		if (this.url == url) {
-			return;
-		};
-		
-		this.url = url;
-		this.setState({ loading: true });
-
-		const scheme = U.Common.getScheme(url);
-		if (scheme && !ALLOWED_SCHEME.includes(scheme)) {
-			this.setState({ loading: false });
-			return;
-		};
-
-		C.LinkPreview(url, (message: any) => {
-			if (!this._isMounted) {
-				return;
-			};
-
-			let state: any = { loading: false };
-
-			if (!message.error.code) {
-				state = Object.assign(state, message.previewLink);
-			};
-
-			this.setState(state);
-
-			if (message.error.code) {
-				this.url = '';
-			};
-		});
-	};
+	});
+	
+	return (
+		<div className={cn.join(' ')}>
+			{isLoading ? <Loader /> : (
+				<>
+					<div className="info">
+						<div className="link">{U.Common.shortUrl(url)}</div>
+						{title ? <div className="name">{title}</div> : ''}
+						{description ? <div className="descr">{description}</div> : ''}
+					</div>
+				</>
+			)}
+		</div>
+	);
 
 });
 
