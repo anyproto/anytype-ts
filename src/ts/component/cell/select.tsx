a06c7550abb534a37fff45d99eac5df254899e7a
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-17 09:56:42 +0100

    JS-4200: CellSelect as functional component

--- src/ts/component/cell/select.tsx
+++ src/ts/component/cell/select.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { forwardRef, useRef, useState, useEffect, useImperativeHandle } from 'react';
 import $ from 'jquery';
 import arrayMove from 'array-move';
 import { observer } from 'mobx-react';
@@ -6,207 +6,47 @@ import { getRange, setRange } from 'selection-ranges';
 import { Tag, Icon, DragBox } from 'Component';
 import { I, S, U, J, Relation, keyboard } from 'Lib';
 
-interface State { 
-	isEditing: boolean; 
-};
+const CellSelect = observer(forwardRef<I.CellRef, I.Cell>((props, ref) => {
 
-const CellSelect = observer(class CellSelect extends React.Component<I.Cell, State> {
+	const { id, relation, recordId, getRecord, elementMapper, onChange, arrayLimit, canEdit, placeholder, menuClassName, menuClassNameWrap } = props;
+	const nodeRef = useRef(null);
+	const entryRef = useRef(null);
+	const listRef = useRef(null);
+	const placeholderRef = useRef(null);
+	const [ isEditing, setEditing ] = useState(false);
+	const record = getRecord(recordId);
+	const { maxCount } = relation;
+	const isSelect = relation.format == I.RelationType.Select;
+	const cn = [ 'wrap' ];
 
-	_isMounted = false;
-	node = null;
-	state = {
-		isEditing: false,
-	};
-
-	constructor (props: I.Cell) {
-		super(props);
-
-		this.onClear = this.onClear.bind(this);
-		this.onKeyPress = this.onKeyPress.bind(this);
-		this.onKeyDown = this.onKeyDown.bind(this);
-		this.onKeyUp = this.onKeyUp.bind(this);
-		this.onInput = this.onInput.bind(this);
-		this.onFocus = this.onFocus.bind(this);
-		this.onBlur = this.onBlur.bind(this);
-		this.onDragEnd = this.onDragEnd.bind(this);
-		this.focus = this.focus.bind(this);
-	};
-
-	render () {
-		const { relation, recordId, getRecord, elementMapper, arrayLimit, canEdit, placeholder } = this.props;
-		const { isEditing } = this.state;
-		const record = getRecord(recordId);
-		const isSelect = relation.format == I.RelationType.Select;
-		const cn = [ 'wrap' ];
-
-		if (!relation || !record) {
-			return null;
-		};
-
-		let value = this.getItems();
-		let content = null;
-
-		const length = value.length;
-
-		if (elementMapper) {
-			value = value.map(it => elementMapper(relation, it));
-		};
-
-		if (arrayLimit) {
-			value = value.slice(0, arrayLimit);
-			if (length > arrayLimit) {
-				cn.push('overLimit');
-			};
-		};
-
-		if (isEditing) {
-			const cni = [ 'itemWrap' ];
-
-			if (!isSelect) {
-				cni.push('isDraggable');
-			};
-
-			content = (
-				<div id="value" onClick={this.focus}>
-					<div id="placeholder" className="placeholder">{placeholder}</div>
-
-					<span id="list">
-						<DragBox onDragEnd={this.onDragEnd}>
-							{value.map((item: any, i: number) => (
-								<span 
-									key={i}
-									id={`item-${item.id}`}
-									className={cni.join(' ')}
-									draggable={canEdit && !isSelect}
-									onContextMenu={e => this.onContextMenu(e, item)}
-									{...U.Common.dataProps({ id: item.id, index: i })}
-								>
-									<Tag 
-										key={item.id}
-										text={item.name}
-										color={item.color}
-										canEdit={canEdit && !isSelect} 
-										className={Relation.selectClassName(relation.format)}
-										onClick={e => this.onClick(e, item.id)}
-										onRemove={() => this.onValueRemove(item.id)}
-									/>
-								</span>
-							))}
-						</DragBox>
-					</span>
-					
-					{canEdit ? (
-						<span 
-							id="entry" 
-							contentEditable={true}
-							suppressContentEditableWarning={true} 
-							onFocus={this.onFocus}
-							onBlur={this.onBlur}
-							onInput={this.onInput}
-							onKeyPress={this.onKeyPress}
-							onKeyDown={this.onKeyDown}
-							onKeyUp={this.onKeyUp}
-							onCompositionStart={() => keyboard.setComposition(true)}
-							onCompositionEnd={() => keyboard.setComposition(false)}
-							onClick={e => e.stopPropagation()}
-						>
-							{'\n'}
-						</span>
-					) : ''}
-
-					{isSelect ? <Icon className="clear" onMouseDown={this.onClear} /> : ''}
-				</div>
-			);
-		} else {
-			if (!value.length) {
-				content = <div className="empty">{placeholder}</div>;
-			} else {
-				content = (
-					<span className="over">
-						{value.map((item: any, i: number) => (
-							<Tag 
-								id={`item-${item.id}`}
-								key={item.id} 
-								text={item.name} 
-								color={item.color}
-								className={Relation.selectClassName(relation.format)}
-								onClick={e => this.onClick(e, item.id)}
-								onContextMenu={e => this.onContextMenu(e, item)}
-							/>
-						))}
-						{arrayLimit && (length > arrayLimit) ? <div className="more">+{length - arrayLimit}</div> : ''}
-					</span>
-				);
-			};
-		};
-
-		return (
-			<div 
-				ref={node => this.node = node} 
-				className={cn.join(' ')}
-			>
-				{content}
-			</div>
-		);
-	};
-
-	componentDidMount () {
-		this._isMounted = true;
-	};
-
-	componentDidUpdate () {
-		const { isEditing } = this.state;
-		const { id } = this.props;
-		const cell = $(`#${id}`);
-
-		if (isEditing) {
-			cell.addClass('isEditing');
-			
-			this.placeholderCheck();
-			this.focus();
-			this.resize();
-		} else {
-			cell.removeClass('isEditing');
-		};
-	};
-
-	componentWillUnmount () {
-		this._isMounted = false;
-	};
-
-	setEditing (v: boolean) {
-		const { canEdit } = this.props;
-		const { isEditing } = this.state;
-
+	const setEditingHandler = (v: boolean) => {
 		if (canEdit && (v != isEditing)) {
-			this.setState({ isEditing: v });
+			setEditing(v);
 			
 			if (v) {
-				window.setTimeout(() => this.focus(), 15);
+				window.setTimeout(() => focus(), 15);
 			};
 		};
 	}; 
 
-	onKeyPress (e: any) {
-		if (!this._isMounted || keyboard.isComposition) {
+	const onKeyPress = (e: any) => {
+		if (keyboard.isComposition) {
 			return;
 		};
 
-		const node = $(this.node);
-		const entry = node.find('#entry');
+		const entry = $(entryRef.current);
 
 		if (entry.length && (entry.text().length >= J.Constant.limit.cellEntry)) {
 			e.preventDefault();
 		};
 	};
 
-	onKeyDown (e: any) {
-		if (!this._isMounted || keyboard.isComposition) {
+	const onKeyDown = (e: any) => {
+		if (keyboard.isComposition) {
 			return;
 		};
 
-		const node = $(this.node);
-		const entry = node.find('#entry');
+		const entry = $(entryRef.current);
 
 		keyboard.shortcut('backspace', e, () => {
 			e.stopPropagation();
@@ -218,96 +58,79 @@ const CellSelect = observer(class CellSelect extends React.Component<I.Cell, Sta
 
 			e.preventDefault();
 			
-			const value = this.getValue();
+			const value = getValue();
 			value.existing.pop();
-			this.setValue(value.existing);
+			setValue(value.existing);
 		});
 
-		this.placeholderCheck();
-		this.resize();
-		this.scrollToBottom();
+		placeholderCheck();
+		resize();
+		scrollToBottom();
 	};
 
-	onKeyUp (e: any) {
-		if (!this._isMounted || keyboard.isComposition) {
+	const onKeyUp = (e: any) => {
+		if (keyboard.isComposition) {
 			return;
 		};
 
-		S.Menu.updateData('dataviewOptionList', { filter: this.getValue().new });
+		S.Menu.updateData('dataviewOptionList', { filter: getValue().new });
 
-		this.placeholderCheck();
-		this.resize();
-		this.scrollToBottom();
+		placeholderCheck();
+		resize();
+		scrollToBottom();
 	};
 
-	onInput () {
-		this.placeholderCheck();
+	const onInput = () => {
+		placeholderCheck();
 	};
 
-	onClick (e: any, id: string) {
+	const onClick = (e: any, id: string) => {
 	};
 
-	placeholderCheck () {
-		if (!this._isMounted) {
-			return;
-		};
-
-		const node = $(this.node);
-		const value = this.getValue();
-		const list = node.find('#list');
-		const placeholder = node.find('#placeholder');
+	const placeholderCheck = () => {
+		const value = getValue();
+		const list = $(listRef.current);
+		const placeholder = $(placeholderRef.current);
 
 		value.existing.length ? list.show() : list.hide();
 		value.new || value.existing.length ? placeholder.hide() : placeholder.show();
 	};
 
-	clear () {
-		if (!this._isMounted) {
-			return;
-		};
-
-		const node = $(this.node);
-		node.find('#entry').text(' ');
-
-		this.focus();
+	const clear = () => {
+		$(entryRef.current).text(' ');
+		focus();
 	};
 
-	onValueRemove (id: string) {
-		this.setValue(this.getItemIds().filter(it => it != id));
+	const onValueRemove = (id: string) => {
+		setValue(getItemIds().filter(it => it != id));
 	};
 
-	onDragEnd (oldIndex: number, newIndex: number) {
-		this.setValue(arrayMove(this.getItemIds(), oldIndex, newIndex));
+	const onDragEnd = (oldIndex: number, newIndex: number) => {
+		setValue(arrayMove(getItemIds(), oldIndex, newIndex));
 	};
 
-	onFocus () {
+	const onFocus = () => {
 		keyboard.setFocus(true);
 	};
 
-	onBlur () {
+	const onBlur = () => {
 		keyboard.setFocus(false);
 	};
 
-	focus () {
-		if (!this._isMounted) {
-			return;
-		};
-
-		const node = $(this.node);
-		const entry = node.find('#entry');
+	const focus = () => {
+		const entry = $(entryRef.current);
 
 		if (entry.length) {
 			window.setTimeout(() => {
 				entry.focus();
 				setRange(entry.get(0), { start: 0, end: 0 });
 
-				this.scrollToBottom();
+				scrollToBottom();
 			});
 		};
 	};
 
-	scrollToBottom () {
-		const { id } = this.props;
+	const scrollToBottom = () => {
 		const cell = $(`#${id}`);
 		const content = cell.hasClass('.cellContent') ? cell : cell.find('.cellContent');
 
@@ -316,16 +139,14 @@ const CellSelect = observer(class CellSelect extends React.Component<I.Cell, Sta
 		};
 	};
 
-	onClear (e: any) {
+	const onClear = (e: any) => {
 		e.preventDefault();
 		e.stopPropagation();
 
-		this.setValue([]);
+		setValue([]);
 	};
 
-	onContextMenu (e: React.MouseEvent, item: any) {
-		const { id, canEdit, menuClassName, menuClassNameWrap } = this.props;
-
+	const onContextMenu = (e: React.MouseEvent, item: any) => {
 		if (!canEdit) {
 			return;
 		};
@@ -344,26 +165,18 @@ const CellSelect = observer(class CellSelect extends React.Component<I.Cell, Sta
 		});
 	};
 
-	getItems (): any[] {
-		const { relation, recordId, getRecord } = this.props;
-		const record = getRecord(recordId);
-
+	const getItems = (): any[] => {
 		return relation && record ? Relation.getOptions(record[relation.relationKey]).filter(it => !it.isArchived && !it.isDeleted) : [];
 	};
 
-	getItemIds (): string[] {
-		return this.getItems().map(it => it.id);
+	const getItemIds = (): string[] => {
+		return getItems().map(it => it.id);
 	};
 
-	getValue () {
-		if (!this._isMounted) {
-			return;
-		};
-
-		const node = $(this.node);
-		const list = node.find('#list');
+	const getValue = () => {
+		const list = $(listRef.current);
 		const items = list.find('.itemWrap');
-		const entry = node.find('#entry');
+		const entry = $(entryRef.current);
 		const existing = [];
 
 		items.each((i: number, item: any) => {
@@ -377,10 +190,7 @@ const CellSelect = observer(class CellSelect extends React.Component<I.Cell, Sta
 		};
 	};
 
-	setValue (value: string[]) {
-		const { onChange, relation } = this.props;
-		const { maxCount } = relation;
-		
+	const setValue = (value: string[]) => {
 		value = U.Common.arrayUnique(value);
 
 		const length = value.length;
@@ -390,17 +200,141 @@ const CellSelect = observer(class CellSelect extends React.Component<I.Cell, Sta
 
 		if (onChange) {
 			onChange(value, () => {
-				this.clear();
+				clear();
 
 				S.Menu.updateData('dataviewOptionList', { value });
 			});
 		};
 	};
 
-	resize () {
+	const resize = () => {
 		$(window).trigger('resize.menuDataviewOptionList');
 	};
 
-});
+	let value = getItems();
+	let content = null;
+
+	const length = value.length;
+
+	if (elementMapper) {
+		value = value.map(it => elementMapper(relation, it));
+	};
+
+	if (arrayLimit) {
+		value = value.slice(0, arrayLimit);
+		if (length > arrayLimit) {
+			cn.push('overLimit');
+		};
+	};
+
+	if (isEditing) {
+		const cni = [ 'itemWrap' ];
+
+		if (!isSelect) {
+			cni.push('isDraggable');
+		};
+
+		content = (
+			<div id="value" onClick={focus}>
+				<div ref={placeholderRef} id="placeholder" className="placeholder">{placeholder}</div>
+
+				<span ref={listRef} id="list">
+					<DragBox onDragEnd={onDragEnd}>
+						{value.map((item: any, i: number) => (
+							<span 
+								key={i}
+								id={`item-${item.id}`}
+								className={cni.join(' ')}
+								draggable={canEdit && !isSelect}
+								onContextMenu={e => onContextMenu(e, item)}
+								{...U.Common.dataProps({ id: item.id, index: i })}
+							>
+								<Tag 
+									key={item.id}
+									text={item.name}
+									color={item.color}
+									canEdit={canEdit && !isSelect} 
+									className={Relation.selectClassName(relation.format)}
+									onClick={e => onClick(e, item.id)}
+									onRemove={() => onValueRemove(item.id)}
+								/>
+							</span>
+						))}
+					</DragBox>
+				</span>
+				
+				{canEdit ? (
+					<span 
+						id="entry" 
+						ref={entryRef}
+						contentEditable={true}
+						suppressContentEditableWarning={true} 
+						onFocus={onFocus}
+						onBlur={onBlur}
+						onInput={onInput}
+						onKeyPress={onKeyPress}
+						onKeyDown={onKeyDown}
+						onKeyUp={onKeyUp}
+						onCompositionStart={() => keyboard.setComposition(true)}
+						onCompositionEnd={() => keyboard.setComposition(false)}
+						onClick={e => e.stopPropagation()}
+					>
+						{'\n'}
+					</span>
+				) : ''}
+
+				{isSelect ? <Icon className="clear" onMouseDown={onClear} /> : ''}
+			</div>
+		);
+	} else {
+		if (!value.length) {
+			content = <div className="empty">{placeholder}</div>;
+		} else {
+			content = (
+				<span className="over">
+					{value.map((item: any, i: number) => (
+						<Tag 
+							id={`item-${item.id}`}
+							key={item.id} 
+							text={item.name} 
+							color={item.color}
+							className={Relation.selectClassName(relation.format)}
+							onClick={e => onClick(e, item.id)}
+							onContextMenu={e => onContextMenu(e, item)}
+						/>
+					))}
+					{arrayLimit && (length > arrayLimit) ? <div className="more">+{length - arrayLimit}</div> : ''}
+				</span>
+			);
+		};
+	};
+
+	useEffect(() => {
+		$(`#${id}`).toggleClass('isEditing', isEditing);
+
+		if (isEditing) {
+			placeholderCheck();
+			focus();
+			resize();
+		};
+	});
+
+	useImperativeHandle(ref, () => ({
+		clear,
+		setEditing: setEditingHandler,
+		getValue,
+		setValue,
+	}));
+
+	return (
+		<div 
+			ref={nodeRef} 
+			className={cn.join(' ')}
+		>
+			{content}
+		</div>
+	);
+
+}));
 
 export default CellSelect;
