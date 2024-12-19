7691c515f7965be1095f0e6bdd910d371e8fcdbf
Andrew Simachev <andrew.simachev@gmail.com>
2024-12-19 10:34:03 +0100

    JS-5594: fix

--- src/ts/component/block/chat/form.tsx
+++ src/ts/component/block/chat/form.tsx
@@ -465,9 +465,10 @@ const ChatForm = observer(class ChatForm extends React.Component<Props, State> {
 		const value = U.Common.stringInsert(this.getTextValue(), text, from, to);
 
 		this.range = { from: to, to: to + text.length };
-		this.refEditable.setValue(value);
+		this.refEditable.setValue(Mark.toHtml(value, this.marks));
 		this.refEditable.setRange(this.range);
 		this.refEditable.placeholderCheck();
+		this.renderMarkup();
 
 		if (list.length) {
 			this.addAttachments(list);
