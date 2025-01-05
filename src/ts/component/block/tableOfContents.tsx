57130c0e509e4a21a21a69be5ccb747a4fd57315
Andrew Simachev <andrew.simachev@gmail.com>
2025-01-05 20:31:58 +0100

    JS-4200: BlockTableOfContents as functional component

--- src/ts/component/block/tableOfContents.tsx
+++ src/ts/component/block/tableOfContents.tsx
@@ -1,81 +1,29 @@
-import * as React from 'react';
+import React, { forwardRef, KeyboardEvent } from 'react';
 import { I, S, U, J, focus, translate } from 'Lib';
 import { observer } from 'mobx-react';
 
-const BlockTableOfContents = observer(class BlockTableOfContents extends React.Component<I.BlockComponent> {
+const BlockTableOfContents = observer(forwardRef<{}, I.BlockComponent>((props, ref) => {
 
-	_isMounted = false;
+	const { rootId, block, isPopup, onKeyDown, onKeyUp } = props;
+	const cn = [ 'wrap', 'focusable', `c${block.id}` ];
 
-	constructor (props: I.BlockComponent) {
-		super(props);
-		
-		this.onKeyDown = this.onKeyDown.bind(this);
-		this.onKeyUp = this.onKeyUp.bind(this);
-		this.onFocus = this.onFocus.bind(this);
-	};
-
-	render () {
-		const { block } = this.props;
-		const cn = [ 'wrap', 'focusable', 'c' + block.id ];
-		const tree = this.getTree();
-
-		const Item = (item: any) => {
-			return (
-				<div 
-					className="item" 
-					onClick={e => this.onClick(e, item.id)}
-					style={{ paddingLeft: item.depth * 24 }}
-				>
-					<span>{item.text}</span>
-				</div>
-			);
-		};
-
-		return (
-			<div className={cn.join(' ')} tabIndex={0} onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp} onFocus={this.onFocus}>
-				{!tree.length ? (
-					<div className="empty">{translate('blockTableOfContentsAdd')}</div>
-				) : (
-					<React.Fragment>
-						{tree.map((item: any, i: number) => (
-							<Item key={i} {...item} />
-						))}
-					</React.Fragment>
-				)}
-			</div>
-		);
-	};
-	
-	componentDidMount () {
-		this._isMounted = true;
-	};
-	
-	componentWillUnmount () {
-		this._isMounted = false;
-	};
-	
-	onKeyDown (e: any) {
-		const { onKeyDown } = this.props;
-
+	const onKeyDownHandler = (e: KeyboardEvent) => {
 		if (onKeyDown) {
-			onKeyDown(e, '', [], { from: 0, to: 0 }, this.props);
+			onKeyDown(e, '', [], { from: 0, to: 0 }, props);
 		};
 	};
 	
-	onKeyUp (e: any) {
-		const { onKeyUp } = this.props;
-
+	const onKeyUpHandler = (e: KeyboardEvent) => {
 		if (onKeyUp) {
-			onKeyUp(e, '', [], { from: 0, to: 0 }, this.props);
+			onKeyUp(e, '', [], { from: 0, to: 0 }, props);
 		};
 	};
 
-	onFocus () {
-		focus.set(this.props.block.id, { from: 0, to: 0 });
+	const onFocus = () => {
+		focus.set(block.id, { from: 0, to: 0 });
 	};
 
-	getTree () {
-		const { rootId } = this.props;
+	const getTree = () => {
 		const blocks = S.Block.unwrapTree([ S.Block.wrapTree(rootId, rootId) ]).filter(it => it.isTextHeader());
 		const list: any[] = [];
 
@@ -107,12 +55,12 @@ const BlockTableOfContents = observer(class BlockTableOfContents extends React.C
 				text: String(block.content.text || translate('defaultNamePage')),
 			});
 		});
+
 		return list;
 	};
 
-	onClick (e: any, id: string) {
-		const { isPopup } = this.props;
-		const node = $('.focusable.c' + id);
+	const onClick = (e: any, id: string) => {
+		const node = $(`.focusable.c${id}`);
 
 		if (!node.length) {
 			return;
@@ -126,7 +74,39 @@ const BlockTableOfContents = observer(class BlockTableOfContents extends React.C
 
 		container.scrollTop(y);
 	};
-	
-});
+
+	const Item = (item: any) => (
+		<div 
+			className="item" 
+			onClick={e => onClick(e, item.id)}
+			style={{ paddingLeft: item.depth * 24 }}
+		>
+			<span>{item.text}</span>
+		</div>
+	);
+
+	const tree = getTree();
+
+	return (
+		<div 
+			className={cn.join(' ')} 
+			tabIndex={0} 
+			onKeyDown={onKeyDownHandler} 
+			onKeyUp={onKeyUpHandler} 
+			onFocus={onFocus}
+		>
+			{!tree.length ? (
+				<div className="empty">{translate('blockTableOfContentsAdd')}</div>
+			) : (
+				<>
+					{tree.map((item: any, i: number) => (
+						<Item key={i} {...item} />
+					))}
+				</>
+			)}
+		</div>
+	);
+
+}));
 
 export default BlockTableOfContents;
