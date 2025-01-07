6f8bec13a8ccef0053d7178389a1a15c47804c4b
Andrew Simachev <andrew.simachev@gmail.com>
2025-01-07 18:33:32 +0100

    fix auth flow

--- src/ts/component/page/auth/migrate.tsx
+++ src/ts/component/page/auth/migrate.tsx
@@ -1,16 +1,13 @@
 import React, { forwardRef, useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import { Frame, Error, ProgressBar } from 'Component';
-import { I, C, S, U, Animation, Storage } from 'Lib';
+import { I, C, S, U, Storage } from 'Lib';
 
 const PageAuthMigrate = observer(forwardRef<{}, I.PageComponent>((props, ref) => {
 
 	const { dataPath } = S.Common;
 	const accountId = Storage.get('accountId');
-	const spaceId = Storage.get('spaceId');
 	const [ error, setError ] = useState('');
-	const { networkConfig } = S.Auth;
-	const { mode, path } = networkConfig;
 	const types = [ I.ProgressType.Migrate ];
 	const list = S.Progress.getList(it => types.includes(it.type));
 	const progress = list.length ? list[0] : null;
@@ -29,27 +26,7 @@ const PageAuthMigrate = observer(forwardRef<{}, I.PageComponent>((props, ref) =>
 				return;
 			};
 
-			C.AccountSelect(accountId, dataPath, mode, path, (message: any) => {
-				const { account } = message;
-
-				if (!account) {
-					return;
-				};
-
-				S.Auth.accountSet(account);
-				S.Common.configSet(account.config, false);
-
-				const routeParam = { replace: true };
-
-				if (spaceId) {
-					U.Router.switchSpace(spaceId, '', false, routeParam);
-				} else {
-					U.Data.onAuthWithoutSpace(routeParam);
-				};
-
-				U.Data.onInfo(account.info);
-				U.Data.onAuthOnce(false);
-			});
+			U.Router.go('/auth/setup/init', { replace: true });
 		});
 	}, []);
 
