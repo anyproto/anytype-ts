7cef35687a5be62dcfd2f65119c159d97a46d0dd
Mike Mhlv <mhlv@anytype.io>
2025-01-16 11:40:34 +0000

    JS-6044: wip

--- src/ts/component/sidebar/section/type/conflict.tsx
+++ src/ts/component/sidebar/section/type/conflict.tsx
@@ -1,60 +1,74 @@
-import * as React from 'react';
+import React, { forwardRef, useState, useEffect, useImperativeHandle } from 'react';
 import { observer } from 'mobx-react';
 import { Title, Icon, ObjectName, IconObject } from 'Component';
 import { I, C, S, Relation, translate } from 'Lib';
 
-const SidebarSectionTypeConflict = observer(class SidebarSectionTypeConflict extends React.Component<I.SidebarSectionComponent> {
-	conflictIds: string[] = [];
+const SidebarSectionTypeConflict = observer(forwardRef<{}, I.SidebarSectionComponent>((props, ref) => {
+	const { space } = S.Common;
+	const { rootId, object, onChange } = props;
+	const [ conflictIds, setConflictIds ] = useState([]);
+	const [ dummy, setDummy ] = useState(0);
 
-	constructor (props: I.SidebarSectionComponent) {
-		super(props);
-	};
-
-    render () {
-		const items = this.getItems();
-
-        return (
-			<div className="wrap">
-				<div className="titleWrap">
-					<Title text={translate('sidebarRelationLocal')} />
-				</div>
-
-				<div className="items">
-					{items.map((item, i) => (
-						<div key={i} className="item">
-							<div className="side left">
-								<IconObject object={item} />
-								<ObjectName object={item} />
-							</div>
-							<div className="side right">
-								<Icon className="more" />
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		);
-    };
-
-	componentDidMount () {
-		this.load();
-	};
-
-	getItems () {
-		return Relation.getArrayValue(this.conflictIds).map(key => S.Record.getRelationById(key)).filter(it => it && !Relation.isSystem(it.relationKey));
-	};
-
-	load () {
-		const { space } = S.Common;
-		const { rootId } = this.props;
-
+	const load = () => {
 		C.ObjectTypeListConflictingRelations(rootId, space, (message) => {
 			if (!message.error.code) {
-				this.conflictIds = message.conflictRelationIds;
-				this.forceUpdate();
+				setConflictIds(message.conflictRelationIds);
 			};
 		});
 	};
-});
+
+	const getItems = () => {
+		return Relation.getArrayValue(conflictIds).map(key => S.Record.getRelationById(key)).filter(it => it && !Relation.isSystem(it.relationKey));
+	};
+
+	const onQuestion = () => {
+		S.Popup.open('confirm', {
+			data: {
+				title: translate('popupConfirmLocalFieldsTitle'),
+				text: translate('popupConfirmLocalFieldsText'),
+				textConfirm: translate('commonRemove'),
+				colorConfirm: 'red',
+				colorCancel: 'blank',
+				onConfirm: () => {
+					// need command?
+				},
+			},
+		});
+	};
+
+	useEffect(() => load());
+
+	useImperativeHandle(ref, () => ({
+		forceUpdate: () => setDummy(dummy + 1),
+		load,
+		getItems,
+		onQuestion,
+	}));
+
+	const items = getItems();
+
+	return (
+		<div className="wrap">
+			<div className="titleWrap">
+				<Title text={translate('sidebarRelationLocal')} />
+				<Icon className="question" onClick={onQuestion} />
+			</div>
+
+			<div className="items">
+				{items.map((item, i) => (
+					<div key={i} className="item">
+						<div className="side left">
+							<IconObject object={item} />
+							<ObjectName object={item} />
+						</div>
+						<div className="side right">
+							<Icon className="more" />
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}));
 
 export default SidebarSectionTypeConflict;
