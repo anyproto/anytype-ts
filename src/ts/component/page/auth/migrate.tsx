c2aa4c2fd1f74c03df7fb65d9853a6ed949b5300
Mike Mhlv <mhlv@anytype.io>
2025-02-13 14:45:22 +0000

    JS-6335: migration style

--- src/ts/component/page/auth/migrate.tsx
+++ src/ts/component/page/auth/migrate.tsx
@@ -1,48 +1,67 @@
 import React, { forwardRef, useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
-import { Frame, Error, ProgressBar, Button } from 'Component';
+import { Frame, Error, ProgressBar, Button, Icon, Title, Label } from 'Component';
 import { I, C, S, U, Storage, translate } from 'Lib';
 
 const PageAuthMigrate = observer(forwardRef<{}, I.PageComponent>((props, ref) => {
 
 	const { dataPath } = S.Common;
 	const accountId = Storage.get('accountId');
-	const [ error, setError ] = useState('');
+	const [ errorCode, setErrorCode ] = useState('');
 	const types = [ I.ProgressType.Migrate ];
 	const list = S.Progress.getList(it => types.includes(it.type));
 	const progress = list.length ? list[0] : null;
-	const segments = [];
+
+	let segments = [];
+	let title = translate('pageAuthMigrateTitle');
+	let text = translate('pageAuthMigrateText');
 
 	if (progress) {
 		segments.push({ name: '', caption: '', percent: progress.current / progress.total, isActive: true });
 	};
 
-	const onCancel = () => {
-		C.AccountMigrateCancel(accountId, (message: any) => {
-			if (message.error.code) {
-				setError(message.error.description);
-			};
-		});
+	if (errorCode) {
+		// TODO: change title and text accordingly to error code
+		title = translate('pageAuthMigrateErrorTitle');
+		text = translate('pageAuthMigrateErrorText');
 	};
 
-	useEffect(() => {
+	const onMigrate = () => {
 		S.Auth.clearAll();
+		segments = [];
 
 		C.AccountMigrate(accountId, dataPath, (message: any) => {
 			if (message.error.code) {
-				setError(message.error.description);
+				setErrorCode(message.error.description);
 				return;
 			};
 
 			U.Router.go('/auth/setup/init', { replace: true });
 		});
+	};
+
+	const onCancel = () => {
+		C.AccountMigrateCancel(accountId, (message: any) => {
+			if (message.error.code) {
+				setErrorCode(message.error.code);
+			};
+		});
+	};
+
+	useEffect(() => {
+		onMigrate();
 	}, []);
 
 	return (
 		<Frame>
-			<Error text={error} />
-			<ProgressBar segments={segments} />
-			<Button text={translate('commonCancel')} onClick={onCancel} />
+			<Icon className={errorCode ? 'error' : ''} />
+			<Title text={title} />
+			<Label text={text} />
+			{!errorCode ? (
+				<ProgressBar segments={segments} />
+			) : (
+				<Button text={translate('pageAuthMigrateTryAgain')} className="c36" color="none" />
+			)}
 		</Frame>
 	);
 
