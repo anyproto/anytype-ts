e1b370a49251189da7507c3d895e5e76d9e27896
Andrew Simachev <andrew.simachev@gmail.com>
2025-02-25 20:49:15 +0100

    JS-6479: fix

--- src/ts/component/page/index.tsx
+++ src/ts/component/page/index.tsx
@@ -210,6 +210,7 @@ const Page = observer(class Page extends React.Component<I.PageComponent> {
 		Preview.tooltipHide(true);
 		Preview.previewHide(true);
 		keyboard.setWindowTitle();
+		sidebar.rightPanelToggle(false, false, isPopup);
 
 		if (!Component) {
 			return;
