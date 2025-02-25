97d025096f48db7d351655260d1a48b205fd7384
Mike Mhlv <mhlv@anytype.io>
2025-02-25 14:25:49 +0000

    JS-6266: virtialized settings items

--- src/ts/component/sidebar/page/settings.tsx
+++ src/ts/component/sidebar/page/settings.tsx
@@ -3,10 +3,17 @@ import $ from 'jquery';
 import { observer } from 'mobx-react';
 import { I, J, S, sidebar, translate, U } from 'Lib';
 import { Icon, IconObject, ObjectName } from 'Component';
+import { AutoSizer, CellMeasurer, InfiniteLoader, List, CellMeasurerCache } from 'react-virtualized';
 
 interface Props extends React.Component {
 	page: string;
 };
+
+const LIMIT = 30;
+const HEIGHT_ITEM = 28;
+const HEIGHT_SECTION = 38;
+const HEIGHT_SECTION_FIRST = 34;
+const HEIGHT_ACCOUNT = 56;
 
 const SidebarSettings = observer(class SidebarSettings extends React.Component<Props, {}> {
 
@@ -16,6 +23,8 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 		contentModelTypes: false,
 		contentModelFields: false,
 	};
+
+	cache: any = {};
 
 	render () {
 		const space = U.Space.getSpaceview();
@@ -53,7 +62,7 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 				);
 			} else
 			if (item.isSection) {
-				return <div className="section"><div className="name">{item.name}</div></div>;
+				return <div className={[ 'section', item.isFirst ? 'isFirst' : '' ].join(' ')}><div className="name">{item.name}</div></div>;
 			};
 
 			if (item.id == param.id || (item.subPages && item.subPages.includes(param.id))) {
@@ -105,6 +114,20 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 			);
 		};
 
+		const rowRenderer = ({ index, key, parent, style }) => (
+			<CellMeasurer
+				key={key}
+				parent={parent}
+				cache={this.cache}
+				columnIndex={0}
+				rowIndex={index}
+			>
+				<div className="row" style={style}>
+					<Item {...items[index]} />
+				</div>
+			</CellMeasurer>
+		);
+
 		return (
 			<div 
 				ref={ref => this.node = ref} 
@@ -122,18 +145,39 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 							</div>
 						) : ''}
 						<div className="inner">
-							{items.map((item: any, i: number) => (
-								<Item key={i} {...item} />
-							))}
+							<InfiniteLoader
+								rowCount={items.length}
+								loadMoreRows={() => {}}
+								isRowLoaded={() => true}
+								threshold={LIMIT}
+							>
+								{({ onRowsRendered }) => (
+									<AutoSizer className="scrollArea">
+										{({ width, height }) => (
+											<List
+												width={width}
+												height={height}
+												deferredMeasurmentCache={this.cache}
+												rowCount={items.length}
+												rowHeight={({ index }) => this.getRowHeight(items[index])}
+												rowRenderer={rowRenderer}
+												onRowsRendered={onRowsRendered}
+												overscanRowCount={10}
+												scrollToAlignment="center"
+											/>
+										)}
+									</AutoSizer>
+								)}
+							</InfiniteLoader>
 						</div>
-					</div>
 
-					{!isSpace ? (
-						<div className="logout" onClick={() => S.Popup.open('logout', {})}>
-							<Icon />
-							{translate('commonLogout')}
-						</div>
-					) : ''}
+						{!isSpace ? (
+							<div className="logout" onClick={() => S.Popup.open('logout', {})}>
+								<Icon />
+								{translate('commonLogout')}
+							</div>
+						) : ''}
+					</div>
 				</div>
 			</div>
 		);
@@ -141,8 +185,18 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 
 	componentDidMount () {
 		const history = U.Router.history;
+		const items = this.getItems();
+
+		this.cache = new CellMeasurerCache({
+			fixedWidth: true,
+			defaultHeight: i => this.getRowHeight(items[i]),
+			keyMapper: i => (items[i] || {}).id,
+		});
 
 		this.routeBack = history.entries[history.index - 1];
+	};
+
+	componentDidUpdate () {
 	};
 
 	getSections (): any[] {
@@ -203,12 +257,21 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 		const sections = this.getSections();
 
 		let items: any[] = [];
+
 		for (const section of sections) {
+
+		};
+
+		sections.forEach((section, idx) => {
 			if (section.name) {
 				const item: any = { id: section.id, name: section.name, isSection: true };
 
 				if (section.isToggle) {
 					item.isToggle = true;
+				};
+
+				if (idx == 0) {
+					item.isFirst = true;
 				};
 
 				items.push(item);
@@ -220,8 +283,22 @@ const SidebarSettings = observer(class SidebarSettings extends React.Component<P
 			};
 
 			items = items.concat(children);
-		};
+		});
+
 		return items;
+	};
+
+	getRowHeight (item: any) {
+		if (item.isToggle) {
+			return HEIGHT_ITEM;
+		};
+		if (item.isSection) {
+			return item.isFirst ? HEIGHT_SECTION_FIRST : HEIGHT_SECTION;
+		};
+		if (item.id == 'account') {
+			return HEIGHT_ACCOUNT;
+		};
+		return HEIGHT_ITEM;
 	};
 
 	withMembership () {
