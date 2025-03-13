7f197fecee11f665cf725f19b48b58a7953086c3
Andrew Simachev <andrew.simachev@gmail.com>
2025-03-13 19:53:26 +0100

    fix all objects filter

--- src/ts/component/sidebar/page/allObject.tsx
+++ src/ts/component/sidebar/page/allObject.tsx
@@ -342,7 +342,7 @@ const SidebarPageObject = observer(class SidebarPageObject extends React.Compone
 			};
 
 			case I.ObjectContainerType.List: {
-				filters.push({ relationKey: 'resolvedLayout', condition: I.FilterCondition.In, value: U.Object.getSetLayouts() });
+				filters.push({ relationKey: 'resolvedLayout', condition: I.FilterCondition.In, value: U.Object.getSetLayouts().filter(it => !U.Object.isTypeLayout(it)) });
 				break;
 			};
 
