8a029eec0d25f9c765700654053ff9f118596d5d
Andrew Simachev <andrew.simachev@gmail.com>
2025-04-12 19:38:36 +0200

    https://community.anytype.io/t/resizing-layout-columns-bug-am-i-really-the-only-one-who-has-this-problem/27499/2 fix

--- src/ts/component/block/index.tsx
+++ src/ts/component/block/index.tsx
@@ -688,10 +688,6 @@ const Block = observer(class Block extends React.Component<Props> {
 		const currentBlockId = childrenIds[index];
 		const res = this.calcWidth(e.pageX - offset, index);
 
-		if (!res) {
-			return;
-		};
-		
 		this.unbind();
 		node.removeClass('isResizing');
 		$('body').removeClass('colResize');
@@ -700,11 +696,13 @@ const Block = observer(class Block extends React.Component<Props> {
 		keyboard.disableSelection(false);	
 		
 		node.find('.colResize.active').removeClass('active');
-		
-		C.BlockListSetFields(rootId, [
-			{ blockId: prevBlockId, fields: { width: res.percent * res.sum } },
-			{ blockId: currentBlockId, fields: { width: (1 - res.percent) * res.sum } },
-		]);
+
+		if (res) {
+			C.BlockListSetFields(rootId, [
+				{ blockId: prevBlockId, fields: { width: res.percent * res.sum } },
+				{ blockId: currentBlockId, fields: { width: (1 - res.percent) * res.sum } },
+			]);
+		};
 		
 		node.find('.resizable').trigger('resizeEnd', [ e ]);
 	};
@@ -741,7 +739,7 @@ const Block = observer(class Block extends React.Component<Props> {
 			};
 		};
 
-		return { sum: sum, percent: x };
+		return { sum, percent: x };
 	};
 	
 	onMouseMove (e: any) {
