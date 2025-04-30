f6a7e764adeb8e9b35a11f31b6388f65a3ab2832
Andrew Simachev <andrew.simachev@gmail.com>
2025-04-30 10:44:53 +0200

    JS-6989: fix

--- src/ts/model/block.ts
+++ src/ts/model/block.ts
@@ -124,7 +124,7 @@ class Block implements I.Block {
 	};
 
 	canCreateBlock (): boolean {
-		return !this.isSystem() && !this.isTextTitle() && !this.isTextDescription() && !this.isFeatured() && !this.isType() && !this.isTableRow();
+		return !this.isPage() && !this.isTextTitle() && !this.isTextDescription() && !this.isFeatured() && !this.isType() && !this.isTableRow();
 	};
 
 	canBecomeWidget (): boolean {
