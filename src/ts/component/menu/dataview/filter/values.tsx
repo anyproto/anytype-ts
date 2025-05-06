ae600c8366e4bb9997faa213ffee76dc4bb4eb69
Andrew Simachev <andrew.simachev@gmail.com>
2025-05-06 16:08:37 +0200

    fix styles

--- src/ts/component/menu/dataview/filter/values.tsx
+++ src/ts/component/menu/dataview/filter/values.tsx
@@ -113,7 +113,7 @@ const MenuDataviewFilterValues = observer(class MenuDataviewFilterValues extends
 								/>
 							</div>
 							<div className="buttons">
-								<Icon className="delete withBackground" onClick={e => this.onDelete(e, element)} />
+								<Icon className="delete" onClick={e => this.onDelete(e, element)} />
 							</div>
 						</div>
 					);
