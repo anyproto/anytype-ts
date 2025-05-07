991fa5a247ca2b6c640f4c2aef07628a772b0748
Andrew Simachev <andrew.simachev@gmail.com>
2025-05-07 21:39:40 +0200

    JS-7042: fix

--- src/ts/component/page/elements/head/simple.tsx
+++ src/ts/component/page/elements/head/simple.tsx
@@ -44,7 +44,7 @@ const HeadSimple = observer(class HeadSimple extends React.Component<Props> {
 		const { rootId, isContextMenuDisabled, readonly, noIcon, isPopup } = this.props;
 		const check = U.Data.checkDetails(rootId, '', []);
 		const object = S.Detail.get(rootId, rootId, [ 
-			'layout', 'spaceId', 'featuredRelations', 'recommendedLayout', 'pluralName', 'iconName', 'iconOption',
+			'layout', 'spaceId', 'featuredRelations', 'recommendedLayout', 'pluralName', 'iconName', 'iconOption', 'iconEmoji', 'iconImage',
 		], true);
 
 		if (object._empty_) {
