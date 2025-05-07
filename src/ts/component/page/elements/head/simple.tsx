44e51421e564045c2a9d73922f1993c8d8d0e9d8
Andrew Simachev <andrew.simachev@gmail.com>
2025-05-07 22:14:24 +0200

    fix all objects update

--- src/ts/component/page/elements/head/simple.tsx
+++ src/ts/component/page/elements/head/simple.tsx
@@ -45,6 +45,7 @@ const HeadSimple = observer(class HeadSimple extends React.Component<Props> {
 		const check = U.Data.checkDetails(rootId, '', []);
 		const object = S.Detail.get(rootId, rootId, [ 
 			'layout', 'spaceId', 'featuredRelations', 'recommendedLayout', 'pluralName', 'iconName', 'iconOption', 'iconEmoji', 'iconImage',
+			'done', 'fileExt', 'fileMimeType',
 		], true);
 
 		if (object._empty_) {
