663fb6738d701e4a3325e955a64cce29ad826e34
Shampra <16141040+Shampra@users.noreply.github.com>
2025-05-19 15:58:40 +0200

    Drawio support added to embed blocks

--- src/ts/lib/util/embed.ts
+++ src/ts/lib/util/embed.ts
@@ -13,6 +13,7 @@ DOMAINS[I.EmbedProcessor.Bilibili] = [ 'bilibili.com', 'b23.tv'];
 DOMAINS[I.EmbedProcessor.Kroki] = [ 'kroki.io' ];
 DOMAINS[I.EmbedProcessor.GithubGist] = [ 'gist.github.com' ];
 DOMAINS[I.EmbedProcessor.Sketchfab] = [ 'sketchfab.com' ];
+DOMAINS[I.EmbedProcessor.Drawio] = [ 'diagrams.net' ];
 
 const IFRAME_PARAM = 'frameborder="0" scrolling="no" allowfullscreen';
 
@@ -79,6 +80,10 @@ class UtilEmbed {
 	};
 
 	getSketchfabHtml (content: string): string {
+		return `<iframe src="${content}" ${IFRAME_PARAM}></iframe>`;
+	};
+
+	getDrawioHtml (content: string): string {
 		return `<iframe src="${content}" ${IFRAME_PARAM}></iframe>`;
 	};
 
@@ -229,6 +234,17 @@ class UtilEmbed {
 				break;
 			};
 
+			case I.EmbedProcessor.Drawio: {
+				const u = new URL(url);
+				const allowedHosts = ['viewer.diagrams.net', 'embed.diagrams.net', 'app.diagrams.net', 'draw.io'];
+				if (allowedHosts.includes(u.hostname)) {
+					// Edit mode cannot be opened at this time
+					u.searchParams.delete('edit');
+					url = u.toString();
+				}
+				break;
+			}
+
 			case I.EmbedProcessor.GithubGist: {
 				const a = url.split('#');
 				if (!a.length) {
@@ -341,6 +357,7 @@ class UtilEmbed {
 			I.EmbedProcessor.Bilibili,
 			I.EmbedProcessor.Kroki,
 			I.EmbedProcessor.Sketchfab,
+			I.EmbedProcessor.Drawio,
 			I.EmbedProcessor.Image,
 		].includes(p);
 	};
@@ -394,6 +411,7 @@ class UtilEmbed {
 			I.EmbedProcessor.Bilibili,
 			I.EmbedProcessor.Graphviz,
 			I.EmbedProcessor.Kroki,
+			I.EmbedProcessor.Drawio,
 			I.EmbedProcessor.Image,
 		].includes(p);
 	};
@@ -418,6 +436,7 @@ class UtilEmbed {
 			I.EmbedProcessor.Codepen,
 			I.EmbedProcessor.Kroki,
 			I.EmbedProcessor.Chart,
+			I.EmbedProcessor.Drawio,
 		].includes(p);
 	};
 
