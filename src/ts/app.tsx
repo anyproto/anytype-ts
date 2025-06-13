96a14e56b7cf1cba97a58bb0d907fd463db66e20
Mike Mhlv <mhlv@anytype.io>
2025-06-13 10:42:39 +0100

    JS-7273: redeem code button in settings

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -214,10 +214,6 @@ class App extends React.Component<object, State> {
 
 	componentDidMount () {
 		this.init();
-
-		window.setTimeout(() => {
-			U.Router.go('/main/settings/membership', {})
-		},1500)
 	};
 
 	init () {
