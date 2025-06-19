8babb5c79e510731e60bb4fbe8d882d142da17f4
Andrew Simachev <andrew.simachev@gmail.com>
2025-06-19 13:19:40 +0200

    JS-7315: Table of Contents

--- src/ts/component/page/elements/tableOfContents.tsx
+++ src/ts/component/page/elements/tableOfContents.tsx
@@ -1,7 +1,8 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useImperativeHandle, useRef, useEffect } from 'react';
 import $ from 'jquery';
+import raf from 'raf';
 import { observer } from 'mobx-react';
-import { I, S } from 'Lib';
+import { I, S, U, J } from 'Lib';
 
 interface TableOfContentsRefProps {
 	setBlock: (v: string) => void;
@@ -13,6 +14,15 @@ const TableOfContents = observer(forwardRef<TableOfContentsRefProps, I.BlockComp
 	const nodeRef = useRef(null);
 	const blocks = S.Block.getBlocks(rootId, it => it.isTextHeader());
 	const max = blocks.reduce((res, current) => Math.max(res, current.getLength()), 0);
+	
+	const rebind = () => {
+		unbind();
+		$(window).on('resize.tableOfContents', () => resize());
+	};
+
+	const unbind = () => {
+		$(window).off('resize.tableOfContents');
+	};
 
 	const setBlock = (id: string) => {
 		const node = $(nodeRef.current);
@@ -24,9 +34,13 @@ const TableOfContents = observer(forwardRef<TableOfContentsRefProps, I.BlockComp
 	};
 
 	const onClick = () => {
+		const node = $(nodeRef.current);
+
 		S.Menu.open('tableOfContents', {
-			element: $(nodeRef.current),
+			element: node,
+			horizontal: I.MenuDirection.Right,
 			vertical: I.MenuDirection.Center,
+			offsetX: node.width() + 4,
 			noFlipX: true,
 			noFlipY: true,
 			data: {
@@ -35,6 +49,38 @@ const TableOfContents = observer(forwardRef<TableOfContentsRefProps, I.BlockComp
 			}
 		});
 	};
+
+	const resize = () => {
+		raf(() => {
+			const node = $(nodeRef.current);
+			if (!node.length) {
+				return;
+			};
+
+			const container = U.Common.getScrollContainer(isPopup);
+			const width = container.width();
+			const height = container.height();
+			const o = isPopup && container.length ? container.offset().left : 0;
+
+			node.css({ left: o + width - node.width() - 22 });
+			console.log('resize', width, height);
+		});
+	};
+
+	useEffect(() => {
+		rebind();
+		resize();
+
+		if (isPopup) {
+			window.setTimeout(() => resize(), J.Constant.delay.popup);
+		};
+
+		return () => {
+			unbind();
+		};
+	}, []);
+
+	useEffect(() => resize());
 
 	useImperativeHandle(ref, () => ({
 		setBlock,
