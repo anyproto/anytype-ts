f8a0745b615b9125a9f9bc3dac48d9027e7269f5
Andrew Simachev <andrew.simachev@gmail.com>
2025-06-24 09:17:16 +0200

    SidebarSectionTypeLayoutFormatList as functional component

--- src/ts/component/sidebar/section/type/format/list.tsx
+++ src/ts/component/sidebar/section/type/format/list.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { forwardRef, useRef, useEffect } from 'react';
 import { observer } from 'mobx-react';
 import { Icon, Select } from 'Component';
 import { I, U, translate, S, J } from 'Lib';
@@ -7,79 +7,14 @@ interface Props extends I.SidebarSectionComponent {
 	layoutOptions?: any[];
 };
 
-const SidebarSectionTypeLayoutFormatList = observer(class SidebarSectionTypeLayoutFormatList extends React.Component<Props> {
+const SidebarSectionTypeLayoutFormatList = observer(forwardRef<{}, Props>((props, ref) => {
 
-	refDefaultView = null;
+	const viewRef = useRef(null);
+	const { object, onChange, readonly } = props;
+	const defaultViewOptions = U.Menu.getViews().filter(it => it.id != I.ViewType.Graph);
+	const type = S.Record.getTypeById(object.defaultTypeId);
 
-	constructor (props: Props) {
-		super(props);
-
-		this.onType = this.onType.bind(this);
-	};
-
-	render () {
-		const { object, onChange, readonly } = this.props;
-		const defaultViewOptions = U.Menu.getViews().filter(it => it.id != I.ViewType.Graph);
-		const type = S.Record.getTypeById(object.defaultTypeId);
-
-		return (
-			<div className="items">
-				<div className="item">
-					<div className="name">
-						{translate('sidebarSectionLayoutDefaultView')}
-					</div>
-
-					<div className="value">
-						<Select
-							ref={ref => this.refDefaultView = ref}
-							id={`sidebar-layout-default-view-${object.id}`}
-							options={defaultViewOptions}
-							value={object.defaultViewType}
-							arrowClassName="light"
-							onChange={id => onChange({ defaultViewType: id })}
-							readonly={readonly}
-							menuParam={{
-								className: 'fixed',
-								classNameWrap: 'fromSidebar',
-								horizontal: I.MenuDirection.Right,
-							}}
-						/>
-					</div>
-				</div>
-
-				<div className="item">
-					<div className="name">
-						{translate('sidebarSectionLayoutDefaultType')}
-					</div>
-
-					<div className="value">
-						<div 
-							id={`sidebar-layout-default-type-${object.id}`} 
-							className={[ 'select', (readonly ? 'isReadonly' : '') ].join(' ')} 
-							onClick={this.onType}
-						>
-							<div className="item">
-								<div className="name">{type?.name || translate('commonSelect')}</div>
-							</div>
-							<Icon className="arrow light" />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	};
-
-	componentDidMount (): void {
-		this.setValue();
-	};
-
-	componentDidUpdate (): void {
-		this.setValue();
-	};
-
-	onType () {
-		const { object, onChange, readonly } = this.props;
-		
+	const onType = () => {
 		if (readonly) {
 			return;
 		};
@@ -103,9 +38,58 @@ const SidebarSectionTypeLayoutFormatList = observer(class SidebarSectionTypeLayo
 		});
 	};
 
-	setValue () {
-		this.refDefaultView.setValue(String(this.props.object.defaultViewType));
+	const setValue = () => {
+		viewRef.current.setValue(String(object.defaultViewType));
 	};
-});
+
+	useEffect(() => setValue());
+
+	return (
+		<div className="items">
+			<div className="item">
+				<div className="name">
+					{translate('sidebarSectionLayoutDefaultView')}
+				</div>
+
+				<div className="value">
+					<Select
+						ref={viewRef}
+						id={`sidebar-layout-default-view-${object.id}`}
+						options={defaultViewOptions}
+						value={object.defaultViewType}
+						arrowClassName="light"
+						onChange={id => onChange({ defaultViewType: id })}
+						readonly={readonly}
+						menuParam={{
+							className: 'fixed',
+							classNameWrap: 'fromSidebar',
+							horizontal: I.MenuDirection.Right,
+						}}
+					/>
+				</div>
+			</div>
+
+			<div className="item">
+				<div className="name">
+					{translate('sidebarSectionLayoutDefaultType')}
+				</div>
+
+				<div className="value">
+					<div 
+						id={`sidebar-layout-default-type-${object.id}`} 
+						className={[ 'select', (readonly ? 'isReadonly' : '') ].join(' ')} 
+						onClick={onType}
+					>
+						<div className="item">
+							<div className="name">{type?.name || translate('commonSelect')}</div>
+						</div>
+						<Icon className="arrow light" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+
+}));
 
 export default SidebarSectionTypeLayoutFormatList;
