89dd904232ccd0873a7c497a4c11337806048a9b
Andrew Simachev <andrew.simachev@gmail.com>
2025-06-25 11:23:29 +0200

    update table of contents calculations

--- src/ts/component/editor/page.tsx
+++ src/ts/component/editor/page.tsx
@@ -31,6 +31,7 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 	winScrollTop = 0;
 	containerScrollTop = 0;
 	uiHidden = false;
+	tocBlockId = '';
 	width = 0;
 	refHeader: any = null;
 	refControls: any = null;
@@ -415,7 +416,7 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 		this.onScroll();
 
 		win.on(`resize.${ns}`, () => this.resizePage());
-		container.on(`scroll.${ns}`, e => this.onScroll());
+		container.on(`scroll.${ns}`, () => this.onScroll());
 
 		Renderer.on(`commandEditor`, (e: any, cmd: string, arg: any) => this.onCommand(cmd, arg));
 	};
@@ -1802,7 +1803,9 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 		const container = U.Common.getScrollContainer(isPopup);
 		const top = container.scrollTop();
 		const headers = S.Block.getBlocks(rootId, it => it.isTextHeader());
-		const co = (isPopup ? container.offset().top : 0) + J.Size.header;
+		const length = headers.length;
+		const co = isPopup ? container.offset().top : 0;
+		const ch = container.height() - J.Size.header;
 
 		this.containerScrollTop = top;
 		this.winScrollTop = win.scrollTop();
@@ -1812,26 +1815,27 @@ const EditorPage = observer(class EditorPage extends React.Component<Props, Stat
 			Storage.setScroll('editor', rootId, top, isPopup);
 		}, 50);
 
-		let blockId = '';
+		if (length) {
+			for (let i = 0; i < length; ++i) {
+				const block = headers[i];
+				const el = $(`#block-${block.id}`);
 
-		for (let i = 0; i < headers.length; ++i) {
-			const block = headers[i];
-			const el = $(`#block-${block.id}`);
+				if (!el.length) {
+					continue;
+				};
 
-			if (!el.length) {
-				continue;
-			};
+				const t = el.offset().top - co;
+				const h = el.outerHeight();
+				const check = isPopup ? 0 : top;
 
-			const offset = el.offset().top - co;
-			const check = isPopup ? 0 : top;
-
-			if (offset >= check) {
-				blockId = block.id;
-				break;
+				if ((t >= check) && (t + h <= check + ch)) {
+					this.tocBlockId = block.id;
+					break;
+				};
 			};
 		};
 
-		this.refToc?.setBlock(blockId);
+		this.refToc?.setBlock(this.tocBlockId);
 		Preview.previewHide(false);
 	};
 	
