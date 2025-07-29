66331eea3af9d7395c84a519e77e39b0babeaaef
Andrew Simachev <andrew.simachev@gmail.com>
2025-07-29 09:49:25 +0200

    JS-5965: PopupExport as functional component

--- src/ts/component/popup/export.tsx
+++ src/ts/component/popup/export.tsx
@@ -1,163 +1,99 @@
-import * as React from 'react';
+import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import { I, S, Action, keyboard, analytics, translate } from 'Lib';
 import { Title, Select, Button, Switch } from 'Component';
 import { observer } from 'mobx-react';
 
-const PopupExport = observer(class PopupExport extends React.Component<I.Popup> {
+const PopupExport = observer(forwardRef<{}, I.Popup>((props, ref) => {
 
-	data: any = {};
+	const { space } = S.Common;
+	const { param, close, position, storageGet, storageSet } = props;
+	const { data } = param;
+	const { allowHtml, objectIds, route } = data;
+	const [ dummy, setDummy ] = useState(0);
+	const theme = S.Common.getThemeClass();
+	const update = useRef<any>({});
 
-	constructor (props: I.Popup) {
-		super(props);
+	const pageSize = [
+		{ id: 'A3', name: 'A3'},
+		{ id: 'A4', name: 'A4'},
+		{ id: 'A5', name: 'A5'},
+		{ id: 'legal', name: 'Legal'},
+		{ id: 'letter', name: 'Letter'},
+		{ id: 'tabloid', name: 'Tabloid'},
+	];
 
-		this.onConfirm = this.onConfirm.bind(this);
-		this.onCancel = this.onCancel.bind(this);
-	};
+	const formatOptions = [
+		{ id: 'json', name: 'JSON'},
+		{ id: 'pb', name: 'Protobuf'},
+	];
 
-	render() {
-		const theme = S.Common.getThemeClass();
-		const formats = this.getFormats();
+	const Option = (item: any) => {
+		let control = null;
 
-		const pageSize = [
-			{ id: 'A3', name: 'A3'},
-			{ id: 'A4', name: 'A4'},
-			{ id: 'A5', name: 'A5'},
-			{ id: 'legal', name: 'Legal'},
-			{ id: 'letter', name: 'Letter'},
-			{ id: 'tabloid', name: 'Tabloid'},
-		];
-
-		const formatOptions = [
-			{ id: 'json', name: 'JSON'},
-			{ id: 'pb', name: 'Protobuf'},
-		];
-
-		const Option = (item: any) => {
-			let control = null;
-
-			switch (item.control) {
-				case 'switch': {
-					control = (
-						<Switch
-							className="big"
-							value={this.data[item.id]}
-							onChange={(e: any, v: boolean) => {
-								this.data[item.id] = v;
-								this.save();
-							}}
-						/>
-					);
-					break;
-				};
-
-				case 'select': {
-					let value = this.data[item.id];
-					if (item.id == 'json') {
-						value = value ? 'json' : 'pb';
-					};
-
-					control = (
-						<Select
-							id={item.id}
-							value={value}
-							options={item.options}
-							onChange={(v: any) => {
-								if (item.id == 'json') {
-									v = (v == 'json') ? true : false;
-								};
-
-								this.data[item.id] = v;
-								this.save();
-								this.forceUpdate();
-							}}
-							arrowClassName="light"
-							isMultiple={false}
-							menuParam={{ width: 300 }}
-						/>
-					);
-					break;
-				};
+		switch (item.control) {
+			case 'switch': {
+				control = (
+					<Switch
+						className="big"
+						value={update.current[item.id]}
+						onChange={(e: any, v: boolean) => {
+							update.current[item.id] = v;
+							save();
+						}}
+					/>
+				);
+				break;
 			};
 
-			return (
-				<div className="row">
-					<div className="name">{item.name}</div>
-					<div className="value">
-						{control}
-					</div>
-				</div>
-			);
-		};
-
-		this.init();
-
-		const { format } = this.data;
-
-		let items: any[] = [
-			{ id: 'format', name: translate('popupExportFormat'), control: 'select', options: formats },
-		];
-
-		switch (format) {
-			case I.ExportType.Markdown:
-			case I.ExportType.Protobuf:
-				if (format == I.ExportType.Protobuf) {
-					items.push({ id: 'json', name: translate('popupExportFileFormat'), control: 'select', options: formatOptions });
+			case 'select': {
+				let value = update.current[item.id];
+				if (item.id == 'json') {
+					value = value ? 'json' : 'pb';
 				};
 
-				items = items.concat([
-					{ id: 'zip', name: translate('popupExportZipArchive'), control: 'switch' },
-					{ id: 'nested', name: translate('popupExportIncludeLinkedObjects'), control: 'switch' },
-					{ id: 'files', name: translate('popupExportIncludeFiles'), control: 'switch' },
-					{ id: 'archived', name: translate('popupExportIncludeArchivedObjects'), control: 'switch' },
-				]);
-				break;
+				control = (
+					<Select
+						id={item.id}
+						value={value}
+						options={item.options}
+						onChange={(v: any) => {
+							if (item.id == 'json') {
+								v = (v == 'json') ? true : false;
+							};
 
-			case I.ExportType.Pdf:
-				items = items.concat([
-					{ id: 'pageSize', name: translate('popupExportPageSize'), control: 'select', options: pageSize },
-					{ id: 'landscape', name: translate('popupExportLandscape'), control: 'switch' },
-					(!theme ? { id: 'printBackground', name: translate('popupExportPrintBackground'), control: 'switch' } : null),
-				]);
+							update.current[item.id] = v;
+							save();
+							setDummy(dummy + 1);
+						}}
+						arrowClassName="light"
+						isMultiple={false}
+						menuParam={{ width: 300 }}
+					/>
+				);
 				break;
+			};
 		};
 
-		items = items.filter(it => it);
-
 		return (
-			<>
-				<Title text={translate('popupExportTitle')} />
-
-				{items.map((item: any, i: number) => (
-					<Option key={i} {...item} />
-				))}
-
-				<div className="buttons">
-					<Button text={translate('popupExportOk')} onClick={this.onConfirm} />
-					<Button color="blank" text={translate('commonCancel')} onClick={this.onCancel} />
+			<div className="row">
+				<div className="name">{item.name}</div>
+				<div className="value">
+					{control}
 				</div>
-			</>
+			</div>
 		);
 	};
 
-	componentDidMount () {
-		this.init();
-	};
-
-	componentDidUpdate () {
-		this.props.position();
-	};
-
-	init () {
-		const { storageGet } = this.props;
+	const init = () => {
 		const options = storageGet();
-		const formats = this.getFormats();
+		const formats = getFormats();
 
 		let format = Number(options.format);
 		if (!formats.map(it => it.id).includes(format)) {
 			format = formats[0].id;
 		};
 
-		this.data = {
+		update.current = {
 			format,
 			zip:				 Boolean(options.zip),
 			nested:				 Boolean(options.nested),
@@ -170,10 +106,7 @@ const PopupExport = observer(class PopupExport extends React.Component<I.Popup> 
 		};
 	};
 
-	getFormats () {
-		const { param } = this.props;
-		const { data } = param;
-		const { allowHtml } = data;
+	const getFormats = () => {
 		const { config } = S.Common;
 
 		return [
@@ -184,22 +117,18 @@ const PopupExport = observer(class PopupExport extends React.Component<I.Popup> 
 		].filter(it => it);
 	};
 
-	save () {
-		this.props.storageSet(this.data);
+	const save = () => {
+		storageSet(update.current);
 	};
 
-	onConfirm (e: any) {
-		const { space } = S.Common;
-		const { param, close } = this.props;
-		const { data } = param;
-		const { objectIds, route } = data;
-		const { format } = this.data;
+	const onConfirm = (e: any) => {
+		const { format } = update.current;
 
 		analytics.event('ClickExport', { type: format, route });
 
 		switch (format) {
 			default:
-				Action.export(space, objectIds, format, { ...this.data, route });
+				Action.export(space, objectIds, format, { ...update.current, route });
 				close();
 				break;
 
@@ -209,18 +138,72 @@ const PopupExport = observer(class PopupExport extends React.Component<I.Popup> 
 				break;
 
 			case I.ExportType.Pdf:
-				keyboard.onPrintToPDF({ ...this.data });
+				keyboard.onPrintToPDF({ ...update.current });
 				analytics.event('Export', { type: format, route });
 				break;
 		};
 		
-		this.save();
+		save();
 	};
 
-	onCancel (e: any) {
-		this.props.close();
+	init();
+
+	const { format } = update.current;
+	const formats = getFormats();
+
+	let items: any[] = [
+		{ id: 'format', name: translate('popupExportFormat'), control: 'select', options: formats },
+	];
+
+	switch (format) {
+		case I.ExportType.Markdown:
+		case I.ExportType.Protobuf:
+			if (format == I.ExportType.Protobuf) {
+				items.push({ id: 'json', name: translate('popupExportFileFormat'), control: 'select', options: formatOptions });
+			};
+
+			items = items.concat([
+				{ id: 'zip', name: translate('popupExportZipArchive'), control: 'switch' },
+				{ id: 'nested', name: translate('popupExportIncludeLinkedObjects'), control: 'switch' },
+				{ id: 'files', name: translate('popupExportIncludeFiles'), control: 'switch' },
+				{ id: 'archived', name: translate('popupExportIncludeArchivedObjects'), control: 'switch' },
+			]);
+			break;
+
+		case I.ExportType.Pdf:
+			items = items.concat([
+				{ id: 'pageSize', name: translate('popupExportPageSize'), control: 'select', options: pageSize },
+				{ id: 'landscape', name: translate('popupExportLandscape'), control: 'switch' },
+				(!theme ? { id: 'printBackground', name: translate('popupExportPrintBackground'), control: 'switch' } : null),
+			]);
+			break;
 	};
-	
-});
+
+	items = items.filter(it => it);
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	useEffect(() => {
+		position();
+	});
+
+	return (
+		<>
+			<Title text={translate('popupExportTitle')} />
+
+			{items.map((item: any, i: number) => (
+				<Option key={i} {...item} />
+			))}
+
+			<div className="buttons">
+				<Button text={translate('popupExportOk')} onClick={onConfirm} />
+				<Button color="blank" text={translate('commonCancel')} onClick={() => close()} />
+			</div>
+		</>
+	);
+
+}));
 
 export default PopupExport;
