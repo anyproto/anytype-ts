0721e48c2a031bc31d49822866a04b00aba9341d
Andrew Simachev <andrew.simachev@gmail.com>
2025-08-18 13:59:00 +0200

    refactoring

--- src/ts/component/widget/space.tsx
+++ src/ts/component/widget/space.tsx
@@ -1,52 +1,25 @@
-import React, { forwardRef, useRef, MouseEvent } from 'react';
+import React, { forwardRef } from 'react';
 import { observer } from 'mobx-react';
-import { Icon, IconObject, ObjectName } from 'Component';
-import { I, U, translate, sidebar, keyboard, analytics, Preview } from 'Lib';
+import { Icon } from 'Component';
+import { I, U, translate, sidebar, keyboard, analytics } from 'Lib';
 
 const WidgetSpace = observer(forwardRef<{}, I.WidgetComponent>((props, ref) => {
 
-	const { parent, sidebarDirection } = props;
+	const { sidebarDirection } = props;
 	const space = U.Space.getSpaceview();
-	const plusRef = useRef(null);
 	const participants = U.Space.getParticipantsList([ I.ParticipantStatus.Active, I.ParticipantStatus.Joining, I.ParticipantStatus.Removing ]);
 	const requestCnt = participants.filter(it => it.isJoining || it.isRemoving).length;
 	const isSpaceOwner = U.Space.isMyOwner();
 	const cn = [];
-	const cmd = keyboard.cmdSymbol();
-	const alt = keyboard.altSymbol();
 	const buttons = [
 		{ id: 'search', name: translate('commonSearch') },
 		{ id: 'all', name: translate('commonAllContent') },
 		!space.isPersonal ? { id: 'member', name: translate('pageSettingsSpaceIndexInviteMembers') } : null,
+		{ id: 'settings', name: translate('commonSettings') },
 	].filter(it => it);
 
 	if (isSpaceOwner && requestCnt) {
 		cn.push('withCnt');
-	};
-
-	const onSettings = (e: MouseEvent) => {
-		e.stopPropagation();
-		U.Object.openRoute({ id: 'spaceIndex', layout: I.ObjectLayout.Settings });
-	};
-
-	const onCreate = (e: MouseEvent) => {
-		e.stopPropagation();
-		keyboard.pageCreate({}, analytics.route.navigation, [ I.ObjectFlag.SelectTemplate, I.ObjectFlag.DeleteEmpty ]);
-	};
-
-	const onArrow = (e: MouseEvent) => {
-		e.stopPropagation();
-
-		U.Menu.typeSuggest({ 
-			element: `#widget-${parent.id}-arrow`,
-			offsetY: 2,
-			className: 'fixed',
-			classNameWrap: 'fromSidebar',
-		}, {}, { 
-			deleteEmpty: true,
-			selectTemplate: true,
-			withImport: true,
-		}, analytics.route.widget, object => U.Object.openAuto(object));
 	};
 
 	const onButtonClick = (e: any, item: any) => {
@@ -69,19 +42,17 @@ const WidgetSpace = observer(forwardRef<{}, I.WidgetComponent>((props, ref) => {
 				keyboard.onSearchPopup(analytics.route.widget);
 				break;
 			};
+
+			case 'settings': {
+				U.Object.openRoute({ id: 'spaceIndex', layout: I.ObjectLayout.Settings });
+				break;
+			};
 		};
-	};
-
-	const onPlusHover = (e: MouseEvent) => {
-		const t = Preview.tooltipCaption(translate('commonNew'), `${cmd} + N / ${cmd} + ${alt} + N`);
-
-		Preview.tooltipShow({ text: t, element: $(plusRef.current) });
 	};
 
 	return (
 		<div 
 			className={cn.join(' ')}
-			onClick={onSettings}
 		>
 			<div className="buttons">
 				{buttons.map((item, i) => {
