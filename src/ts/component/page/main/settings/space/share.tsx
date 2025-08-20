43de7dd93af9fd4db2842aede0c888f824c15a9d
Mike Mhlv <mhlv@anytype.io>
2025-08-20 14:17:35 +0100

    JS-7559: experimental flag removed

--- src/ts/component/page/main/settings/space/share.tsx
+++ src/ts/component/page/main/settings/space/share.tsx
@@ -129,17 +129,14 @@ const PageMainSettingsSpaceShare = observer(class PageMainSettingsSpaceShare ext
 			return;
 		};
 
-		const { config } = S.Common;
 		const { isOnline } = S.Common;
 		const isLocalNetwork = U.Data.isLocalNetwork();
 		const space = U.Space.getSpaceview();
-		const noApproveIds: I.InviteLinkType[] = [ I.InviteLinkType.Editor, I.InviteLinkType.Viewer ];
-
-		let ids: I.InviteLinkType[] = [ I.InviteLinkType.Manual ];
-		if (config.experimental) {
-			ids = noApproveIds.concat(ids);
-		};
-
+		const noApproveIds: I.InviteLinkType[] = [
+			I.InviteLinkType.Editor,
+			I.InviteLinkType.Viewer
+		];
+		const ids: I.InviteLinkType[] = noApproveIds.concat([ I.InviteLinkType.Manual ]);
 		const options: any[] = ids.map((id: I.InviteLinkType) => this.getOptionById(id));
 
 		if (isOnline && !isLocalNetwork) {
