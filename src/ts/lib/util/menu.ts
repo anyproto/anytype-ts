104cd7115510d69b62291136603e1e520cdda82a
Andrew Simachev <andrew.simachev@gmail.com>
2025-08-22 14:24:03 +0200

    JS-7719: disable chat in non-chat spaces

--- src/ts/lib/util/menu.ts
+++ src/ts/lib/util/menu.ts
@@ -925,7 +925,7 @@ class UtilMenu {
 
 		return [
 			{ id: J.Constant.widgetId.favorite, name: translate('widgetFavorite'), icon: 'widget-pin' },
-			{ id: J.Constant.widgetId.chat, name: translate('commonMainChat'), icon: `widget-chat${Number(!space?.isMuted)}` },
+			space.isChat ? { id: J.Constant.widgetId.chat, name: translate('commonMainChat'), icon: `widget-chat${Number(!space?.isMuted)}` } : null,
 			{ id: J.Constant.widgetId.allObject, name: translate('commonAllContent'), icon: 'widget-all' },
 			{ id: J.Constant.widgetId.recentEdit, name: translate('widgetRecent'), icon: 'widget-pencil' },
 			{ id: J.Constant.widgetId.recentOpen, name: translate('widgetRecentOpen'), icon: 'widget-eye', caption: translate('menuWidgetRecentOpenCaption') },
