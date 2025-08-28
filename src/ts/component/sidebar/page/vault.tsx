cc315a80abd85c5dd6b3e5246a7a878a251b4805
Andrew Simachev <andrew.simachev@gmail.com>
2025-08-28 12:21:07 +0200

    active space

--- src/ts/component/sidebar/page/vault.tsx
+++ src/ts/component/sidebar/page/vault.tsx
@@ -432,6 +432,10 @@ const SidebarPageVaultBase = observer(forwardRef<{}, I.SidebarPageComponent>((pr
 		};
 	}, []);
 
+	useEffect(() => {
+		raf(() => setActive(spaceview));
+	}, [ space ]);
+
 	return (
 		<>
 			<div id="head" className="head">
