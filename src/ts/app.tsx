44a23a0a7ab6b78d5790ec3f45794aad2d484a16
Mike Mhlv <mhlv@anytype.io>
2025-09-16 00:54:34 +0100

    JS-7836: phrase in onboarding

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -129,10 +129,6 @@ const App: FC = () => {
 	const init = () => {
 		const { version, arch, getGlobal } = electron;
 
-		window.setTimeout(() => {
-			U.Router.go('/auth/onboard', {})
-		}, 2500)
-
 		U.Router.init(history);
 		U.Smile.init();
 
