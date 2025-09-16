993c714280231a7c9bd1f49477e9663e2650e53e
Andrew Simachev <andrew.simachev@gmail.com>
2025-09-16 09:36:06 +0200

    fix import

--- electron/js/window.js
+++ electron/js/window.js
@@ -5,6 +5,7 @@ const windowStateKeeper = require('electron-window-state');
 const remote = require('@electron/remote/main');
 const port = process.env.SERVER_PORT;
 
+const ConfigManager = require('./config.js');
 const UpdateManager = require('./update.js');
 const MenuManager = require('./menu.js');
 const Util = require('./util.js');
