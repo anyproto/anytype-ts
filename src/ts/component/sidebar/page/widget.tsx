4d0af676b93c5c316cd9ed95fda180773ed37ae1
Andrew Simachev <andrew.simachev@gmail.com>
2025-09-16 17:24:27 +0200

    fix crash

--- src/ts/component/sidebar/page/widget.tsx
+++ src/ts/component/sidebar/page/widget.tsx
@@ -910,7 +910,7 @@ const SidebarPageWidget = observer(class SidebarPageWidget extends React.Compone
 
 			const object = this.getObject(block, target);
 
-			if (object._empty_ || object.isArchived || object.isDeleted) {
+			if (!object || object._empty_ || object.isArchived || object.isDeleted) {
 				return false;
 			};
 
