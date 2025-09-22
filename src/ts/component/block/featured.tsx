035132aae6581b875cc8548398178925f3a5175f
Andrew Simachev <andrew.simachev@gmail.com>
2025-09-22 21:31:35 +0200

    JS-7550: fix

--- src/ts/component/block/featured.tsx
+++ src/ts/component/block/featured.tsx
@@ -366,7 +366,7 @@ const BlockFeatured = observer(class BlockFeatured extends React.Component<Props
 	checkSource () {
 		const { rootId, isPopup } = this.props;
 		const storeId = this.getStoreId();
-		const object = S.Detail.get(rootId, storeId, [ 'layout', 'setOf', 'featuredRelations' ]);
+		const object = S.Detail.get(rootId, storeId, [ 'layout', 'setOf' ]);
 		const keys = this.getItems().map(it => it.relationKey);
 
 		if (object._empty_ || !U.Object.isSetLayout(object.layout) || !keys.includes('setOf')) {
