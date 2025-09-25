870ed06c2c9d7c9cd4b8b3e224d1f0d3f01dbb7e
Andrew Simachev <andrew.simachev@gmail.com>
2025-09-25 18:52:49 +0200

    fix formatting

--- src/ts/component/page/auth/onboard.tsx
+++ src/ts/component/page/auth/onboard.tsx
@@ -61,6 +61,7 @@ const PageAuthOnboard = observer(forwardRef<{}, I.PageComponent>((props, ref) =>
 		const routeParam = { replace: true };
 
 		S.Common.showRelativeDatesSet(true);
+
 		Storage.set('isNewUser', true);
 		Storage.set('chatsOnboarding', true);
 		Storage.setOnboarding('objectDescriptionButton');
