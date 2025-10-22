722bc65ca62b3f2a943666e862011ed91597c89a
Andrew Simachev <andrew.simachev@gmail.com>
2025-10-22 15:36:43 +0200

    PageMainSettingsImportObsidian as functional component

--- src/ts/component/page/main/settings/import/obsidian.tsx
+++ src/ts/component/page/main/settings/import/obsidian.tsx
@@ -1,73 +1,57 @@
-import * as React from 'react';
-import { Title, Button, Label, Icon } from 'Component';
+import React, { forwardRef, useState } from 'react';
+import { observer } from 'mobx-react';
+import { Title, Button, Label, Icon, Error } from 'Component';
 import { I, U, J, translate, Action } from 'Lib';
 
-interface State {
-	error: string;
-};
+const PageMainSettingsImportObsidian = observer(forwardRef<I.PageRef, I.PageSettingsComponent>((props, ref) => {
 
-class PageMainSettingsImportObsidian extends React.Component<I.PageSettingsComponent, State> {
+	const [ error, setError ] = useState<string>('');
 
-	ref = null;
-	state: State = {
-		error: '',
-	};
-
-	constructor (props: I.PageSettingsComponent) {
-		super(props);
-
-		this.onImport = this.onImport.bind(this);
-	};
-
-	render () {
-		const { onPage } = this.props;
-		const { error } = this.state;
-
-		return (
-			<>
-				<Icon className="logo" />
-				<Title text={U.Menu.getImportNames()[I.ImportType.Obsidian]} />
-				<Label className="description" text={translate('popupSettingsImportObsidianDescription')} />
-
-				<div className="inputWrapper flex">
-					<Button text={translate('popupSettingsImportFiles')} className="c36" onClick={this.onImport} />
-				</div>
-
-				<div className="line" />
-
-				<div className="helpWrapper flex">
-					<Title text={translate('popupSettingsImportObsidianHowTo')} />
-				</div>
-
-				<ol className="list">
-					<li>
-						<Label text={translate('popupSettingsImportObsidianList11')} />
-						<Label className="grey" text={translate('popupSettingsImportObsidianList12')} />
-					</li>
-					<li>
-						<Label text={translate('popupSettingsImportObsidianList21')} />
-						<Label className="grey" text={translate('popupSettingsImportObsidianList22')} />
-					</li>
-					<li>
-						<Label text={translate('popupSettingsImportObsidianList31')} />
-						<Label className="grey" text={translate('popupSettingsImportObsidianList32')} />
-					</li>
-				</ol>
-			</>
-		);
-	};
-	
-	onImport (): void {
+	const onImport = () => {
 		Action.import(I.ImportType.Obsidian, J.Constant.fileExtension.import[I.ImportType.Markdown], {}, (message: any) => {
 			if (message.error.code) {
-				this.setState({ error: message.error.description });
-				return;
+				setError(message.error.description);
+			} else {
+				U.Space.openDashboard();
 			};
-
-			U.Space.openDashboard();
 		});
 	};
 
-};
+	return (
+		<>
+			<Icon className="logo" />
+			<Title text={U.Menu.getImportNames()[I.ImportType.Obsidian]} />
+			<Label className="description" text={translate('popupSettingsImportObsidianDescription')} />
+
+			<div className="inputWrapper flex">
+				<Button text={translate('popupSettingsImportFiles')} className="c36" onClick={onImport} />
+			</div>
+
+			<Error text={error} />
+
+			<div className="line" />
+
+			<div className="helpWrapper flex">
+				<Title text={translate('popupSettingsImportObsidianHowTo')} />
+			</div>
+
+			<ol className="list">
+				<li>
+					<Label text={translate('popupSettingsImportObsidianList11')} />
+					<Label className="grey" text={translate('popupSettingsImportObsidianList12')} />
+				</li>
+				<li>
+					<Label text={translate('popupSettingsImportObsidianList21')} />
+					<Label className="grey" text={translate('popupSettingsImportObsidianList22')} />
+				</li>
+				<li>
+					<Label text={translate('popupSettingsImportObsidianList31')} />
+					<Label className="grey" text={translate('popupSettingsImportObsidianList32')} />
+				</li>
+			</ol>
+		</>
+	);
+	
+}));
 
 export default PageMainSettingsImportObsidian;
