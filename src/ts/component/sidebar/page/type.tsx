c6ea710be2fd96123f60c13102aba2630e9ed89f
Andrew Simachev <andrew.simachev@gmail.com>
2025-10-23 11:19:55 +0200

    SidebarSectionTypeLayout as functional component

--- src/ts/component/sidebar/page/type.tsx
+++ src/ts/component/sidebar/page/type.tsx
@@ -193,7 +193,9 @@ const SidebarPageType = observer(class SidebarPageType extends React.Component<I
 		this.updateSections();
 		this.disableButton(!U.Common.objectLength(this.update) || (!this.object.name && !this.object.pluralName));
 
-		C.BlockDataviewRelationSet(this.object.id, J.Constant.blockId.dataview, [ 'name', 'description' ].concat(U.Object.getTypeRelationKeys(this.object.id)));
+		if (this.object.id) {
+			C.BlockDataviewRelationSet(this.object.id, J.Constant.blockId.dataview, [ 'name', 'description' ].concat(U.Object.getTypeRelationKeys(this.object.id)));
+		};
 
 		// analytics
 		let eventId = '';
