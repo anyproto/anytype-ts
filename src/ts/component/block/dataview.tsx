a61d7d4b03f78bfc07054696194fe7785683df44
Andrew Simachev <andrew.simachev@gmail.com>
2025-11-21 21:10:29 +0100

    JS-8362: drop records into view and change relations

--- src/ts/component/block/dataview.tsx
+++ src/ts/component/block/dataview.tsx
@@ -60,7 +60,6 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 		this.getDefaultTemplateId = this.getDefaultTemplateId.bind(this);
 		this.getSubId = this.getSubId.bind(this);
 		this.getEmptyView = this.getEmptyView.bind(this);
-		this.onRecordAdd = this.onRecordAdd.bind(this);
 		this.onCellClick = this.onCellClick.bind(this);
 		this.onCellChange = this.onCellChange.bind(this);
 		this.onContext = this.onContext.bind(this);
@@ -71,7 +70,9 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 		this.onSourceTypeSelect = this.onSourceTypeSelect.bind(this);
 		this.onEmpty = this.onEmpty.bind(this);
 		this.onDragRecordStart = this.onDragRecordStart.bind(this);
+		this.onRecordAdd = this.onRecordAdd.bind(this);
 		this.onRecordDrop = this.onRecordDrop.bind(this);
+		this.onViewDrop = this.onViewDrop.bind(this);
 		this.onTemplateMenu = this.onTemplateMenu.bind(this);
 		this.onTemplateAdd = this.onTemplateAdd.bind(this);
 		this.onSelectEnd = this.onSelectEnd.bind(this);
@@ -1160,11 +1161,13 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 	onDragRecordStart (e: any, recordId: string) {
 		e.stopPropagation();
 
-		const { block } = this.props;
+		const { block, isPopup } = this.props;
 		const dragProvider = S.Common.getRef('dragProvider');
 		const selection = S.Common.getRef('selectionProvider');
 		const record = this.getRecord(recordId);
 		const ids = selection?.get(I.SelectType.Record) || [];
+		const con = $(this.refControls.getNode());
+		const sel = $(this.refSelect.getNode());
 
 		if (!ids.length) {
 			ids.push(record.id);
@@ -1181,7 +1184,11 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 		dragProvider?.onDragStart(e, I.DropType.Record, ids, {
 			getNode: () => this.node,
 			onRecordDrop: this.onRecordDrop,
+			onViewDrop: this.onViewDrop,
 		});
+
+		con.show();
+		sel.hide();
 	};
 
 	onRecordDrop (targetId: string, ids: string[]) {
@@ -1228,6 +1235,23 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 		} else {
 			cb();
 		};
+	};
+
+	onViewDrop (targetId: string, ids: string[]) {
+		if (!targetId || !ids.length) {
+			return;
+		};
+
+		const { rootId, block } = this.props;
+		const details = Dataview.getDetails(rootId, block.id, this.getObjectId(), targetId);
+		const map = [];
+
+		for (const key in details) {
+			map.push({ key, value: details[key] });
+		};
+
+		C.ObjectListSetDetails(ids, map);
+		this.selectionCheck();
 	};
 
 	onSortAdd (item: any, callBack?: () => void) {
@@ -1499,9 +1523,8 @@ const BlockDataview = observer(class BlockDataview extends React.Component<Props
 
 	selectionCheck () {
 		const selection = S.Common.getRef('selectionProvider');
-		const node = $(this.node);
-		const con = node.find('#dataviewControls');
-		const sel = node.find('#dataviewSelection');
+		const con = $(this.refControls.getNode());
+		const sel = $(this.refSelect.getNode());
 		const ids = selection?.get(I.SelectType.Record) || [];
 		const length = ids.length;
 
