1d8cb7aaa5135583b0374d3b23a0da51419b43ef
Andrew Simachev <andrew.simachev@gmail.com>
2025-11-26 18:16:01 +0100

    JS-8396: fix

--- src/ts/component/block/chat/message/date.tsx
+++ src/ts/component/block/chat/message/date.tsx
@@ -22,7 +22,10 @@ const SectionDate = observer(forwardRef<{}, Props>((props, ref) => {
 				ref={nodeRef} 
 				className="sectionDate"
 				{...U.Common.animationProps({
-					transition: { duration: 0.2 },
+					initial: { y: 20 }, 
+					animate: { y: 0 }, 
+					exit: { y: -20 },
+					transition: { duration: 0.3, delay: 0.1 },
 				})}
 			>
 				<Label text={text} />
