ed436d2c30cbe84644aef86c99a13e4c8351ee8d
Andrew Simachev <andrew.simachev@gmail.com>
2025-12-01 15:30:21 +0100

    add one-on-one deeplink

--- src/ts/component/page/main/oneToOne.tsx
+++ src/ts/component/page/main/oneToOne.tsx
@@ -1,17 +1,59 @@
-import React, { forwardRef } from 'react';
-import { I, keyboard } from 'Lib';
+import React, { forwardRef, useRef, useEffect, useImperativeHandle, useState } from 'react';
+import { Loader, Error, Frame, Button, Footer } from 'Component';
+import { U, I, keyboard, translate } from 'Lib';
 
 const PageMainOneToOne = forwardRef<I.PageRef, I.PageComponent>((props, ref) => {
 
 	const { isPopup } = props;
+	const [ error, setError ] = useState('');
 	const param = keyboard.getMatch(isPopup).params;
+	const nodeRef = useRef(null);
+	const frameRef = useRef(null);
+
+	const init = () => {
+	};
+
+	const resize = () => {
+		const win = $(window);
+		const obj = U.Common.getPageFlexContainer(isPopup);
+		const node = $(nodeRef.current);
+		const oh = obj.height();
+		const wh = isPopup ? oh : win.height();
+
+		node.css({ height: wh });
+		frameRef.current?.resize();
+	};
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	useImperativeHandle(ref, () => ({ resize }));
 
 	return (
-		<div>
-			One to One Page
-			{JSON.stringify(param, null, 3)}
+		<div 
+			ref={nodeRef}
+			className="wrapper"
+		>
+			<Frame ref={frameRef}>
+				<Error text={error} />
+
+				{error ? (
+					<div className="buttons">
+						<Button 
+							text={translate('commonBack')} 
+							color="blank" 
+							className="c36" 
+							onClick={() => U.Space.openDashboardOrVoid()} 
+						/>
+					</div>
+				) : <Loader />}
+			</Frame>
+
+			<Footer component="mainObject" {...props} />
 		</div>
 	);
+
 
 });
 
