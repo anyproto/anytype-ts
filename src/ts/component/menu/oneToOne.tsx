02d202de2abec78052a02e66d65ffe066b32eeca
Mike Mhlv <mhlv@anytype.io>
2025-12-03 11:57:09 +0000

    JS-8446: rename

---
+++ src/ts/component/menu/oneToOne.tsx
@@ -0,0 +1,53 @@
+import React, { forwardRef } from 'react';
+import { observer } from 'mobx-react';
+import { ObjectName, Button, QR } from 'Component';
+import { I, U, S, translate, J, Renderer } from 'Lib';
+import $ from 'jquery';
+
+const MenuOneToOne = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, ref: any) => {
+
+	const { getId } = props;
+	const profile = U.Space.getProfile();
+	const { account } = S.Auth;
+	const { id, info } = account;
+	const { metadataKey } = info;
+	const deeplink = `${J.Constant.protocol}://hi/?id=${id}&key=${metadataKey}`;
+
+	const onDownload = () => {
+		const canvas = $(`#${getId()}`).find('canvas').get(0);
+		if (!canvas) {
+			return;
+		};
+
+		const image = canvas.toDataURL('image/png');
+		if (!image) {
+			return;
+		};
+
+		Renderer.send('download', image, { saveAs: true });
+	};
+
+	const onCopy = () => {
+		U.Common.copyToast(translate('commonLink'), deeplink)
+	};
+
+	return (
+		<>
+			<div className="nameWrapper">
+				<ObjectName object={profile} withBadge={true} />
+			</div>
+
+			<div className="qrWrapper">
+				<QR size={200} value={deeplink} withLogo={true} />
+			</div>
+
+			<div className="buttonsWrapper">
+				<Button color="accent" className="c40" text={translate('commonCopyLink')} onClick={onCopy} />
+				<Button color="blank" className="c40" text={translate('menuHiDownloadCode')} onClick={onDownload} />
+			</div>
+		</>
+	);
+
+}));
+
+export default MenuOneToOne;
