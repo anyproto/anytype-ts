b0e2473aeb605882ac45bf3072d973498d618ec8
Andrew Simachev <andrew.simachev@gmail.com>
2025-12-03 10:47:10 +0100

    JS-8458: fix

--- src/ts/component/block/chat/form.tsx
+++ src/ts/component/block/chat/form.tsx
@@ -69,6 +69,7 @@ const ChatForm = observer(forwardRef<RefProps, Props>((props, ref) => {
 	const menuContext = useRef(null);
 	const namespace = U.Common.getEventNamespace(isPopup);
 	const attachmentsSubId = subId + namespace;
+	const spaceview = U.Space.getSpaceview();
 	
 	let attachments = S.Chat.getAttachments(attachmentsSubId);
 
@@ -276,7 +277,7 @@ const ChatForm = observer(forwardRef<RefProps, Props>((props, ref) => {
 		const oneSymbolBefore = range.current ? value[range.current.from - 1] : '';
 		const twoSymbolBefore = range.current ? value[range.current.from - 2] : '';
 		const menuOpenMention = S.Menu.isOpen('blockMention');
-		const canOpenMenuMention = !menuOpenMention && (oneSymbolBefore == '@') && (!twoSymbolBefore || [ ' ', '\n', '(', '[', '"', '\'' ].includes(twoSymbolBefore));
+		const canOpenMenuMention = !spaceview.isOneToOne && !menuOpenMention && (oneSymbolBefore == '@') && (!twoSymbolBefore || [ ' ', '\n', '(', '[', '"', '\'' ].includes(twoSymbolBefore));
 
 		setMarks(parsed.marks);
 
