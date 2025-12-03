3ea374840273f965d0e311d6d477204f444560bf
Andrew Simachev <andrew.simachev@gmail.com>
2025-12-03 10:42:25 +0100

    JS-8457: fix

--- src/ts/component/menu/participant.tsx
+++ src/ts/component/menu/participant.tsx
@@ -13,6 +13,7 @@ const MenuParticipant = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, r
 	const oneToOne = U.Space.getList().filter(it => it.isOneToOne && (it.oneToOneIdentity == object.identity))[0];
 	const text = oneToOne ? translate('menuParticipantMessage') : translate('menuParticipantConnect');
 	const showButton = ((oneToOne && oneToOne.targetSpaceId != space) || !oneToOne) && (object.identity != account.id);
+	const globalName = object.globalName || U.Common.shortMask(object.identity, 6);
 
 	const load = () => {
 		U.Object.getById(object.id, { keys: U.Subscription.participantRelationKeys() }, (object: any) => {
@@ -60,21 +61,23 @@ const MenuParticipant = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, r
 
 	return object ? (
 		<>
-			<IconObject object={object} size={96} />
+			<div className="head">
+				<Label
+					text={globalName}
+					onClick={() => {
+						U.Common.copyToast(translate('blockFeaturedIdentity'), object.identity);
+					}}
+				/>
+			</div>
+			<IconObject object={object} size={128} />
 			<div className="nameWrapper">
 				<ObjectName object={object} withBadge={true} />
 			</div>
-			<Label
-				text={U.Common.shorten(object.resolvedName, 150)}
-				onClick={() => {
-					U.Common.copyToast(translate('blockFeaturedIdentity'), object.identity);
-				}}
-			/>
 			<ObjectDescription object={object} />
 
 			{showButton ? (
 				<div className="buttonsWrapper">
-					<Button color="accent" className="c32" text={text} onClick={onClick} />
+					<Button color="accent" text={text} onClick={onClick} />
 				</div>
 			) : ''}
 		</>
