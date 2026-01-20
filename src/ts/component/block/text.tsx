73da44fb7848d1323667e9afeaa825b94b376283
Andrew Simachev <andrew.simachev@gmail.com>
2026-01-20 15:35:44 +0100

    add trace to debug jumping cursor

--- src/ts/component/block/text.tsx
+++ src/ts/component/block/text.tsx
@@ -56,6 +56,8 @@ const BlockText = observer(forwardRef<I.BlockRef, Props>((props, ref) => {
 	const preventMenu = useRef(false);
 	const clickCnt = useRef(0);
 
+	trace();
+
 	useEffect(() => {
 		setValue(text);
 		renderLatex();
