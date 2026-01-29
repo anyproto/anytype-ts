7a80af613287a7feadb7654bf2e99234ac2a3d52
Andrew Simachev <andrew.simachev@gmail.com>
2026-01-29 11:35:28 +0100

    JS-8733: fix

--- src/ts/lib/util/object.ts
+++ src/ts/lib/util/object.ts
@@ -206,13 +206,22 @@ class UtilObject {
 			return;
 		};
 
-		const tabs = objects.filter(it => it).map(object => ({
+		const filtered = objects.filter(it => it);
+		if (!filtered.length) {
+			return;
+		};
+
+		const tabs = filtered.map(object => ({
 			route: this.route(object),
 			data: this.getTabData(object),
 		}));
 
 		if (tabs.length) {
 			Renderer.send('openTabs', tabs);
+
+			for (const object of filtered) {
+				analytics.event('AddTab', { objectType: object.type });
+			};
 		};
 	};
 
