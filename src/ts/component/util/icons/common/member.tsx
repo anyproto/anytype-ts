3e9468ab0f766d0d19b94f8b8b1c0246c25060c1
Andrew Simachev <andrew.simachev@gmail.com>
2026-03-28 00:22:03 +0100

    remove redundant icon

--- src/ts/component/util/icons/common/member.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Member = (props: React.SVGProps<SVGSVGElement>) => (
-	<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-		<path d="M10 10C12.4853 10 14.5 12.0147 14.5 14.5V15.25C14.5 15.6642 14.1642 16 13.75 16H6.25C5.83579 16 5.5 15.6642 5.5 15.25V14.5C5.5 12.0147 7.51472 10 10 10ZM10 4C11.2426 4 12.25 5.11929 12.25 6.5C12.25 7.88071 11.2426 9 10 9C8.75736 9 7.75 7.88071 7.75 6.5C7.75 5.11929 8.75736 4 10 4Z" fill="currentColor" />
-	</svg>
-);
-
-export default Member;
