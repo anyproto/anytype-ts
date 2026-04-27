44cbd2324134a7d7d45a02c28dcc607922e6daa8
Andrew Simachev <andrew.simachev@gmail.com>
2026-04-27 19:20:24 +0200

    fix widget icon

--- src/ts/component/util/icons/settings/sidebarView.tsx
+++ src/ts/component/util/icons/settings/sidebarView.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const SidebarView = (props: React.SVGProps<SVGSVGElement>) => (
 	<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-		<rect x="3" y="5" width="14" height="2" rx="1" fill="currentColor" />
-		<rect x="3" y="9" width="14" height="2" rx="1" fill="currentColor" />
-		<rect x="3" y="13" width="14" height="2" rx="1" fill="currentColor" />
+		<rect x="2" y="8" width="16" height="4" rx="2" fill="currentColor"/>
+		<rect x="2" y="3" width="16" height="4" rx="2" fill="currentColor"/>
+		<rect x="2" y="13" width="16" height="4" rx="2" fill="currentColor"/>
 	</svg>
+
 );
 
 export default SidebarView;
