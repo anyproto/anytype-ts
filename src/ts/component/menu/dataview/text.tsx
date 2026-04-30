anyproto/anytype-ts

commit 7cb447cfd43408afe815ef13609c96949548b415

JS-8216: menuDataviewText actions logic

src/ts/component/menu/dataview/text.tsx
@@ -1,35 +1,90 @@
-import React, { forwardRef, useRef, useEffect } from 'react';
+import React, { forwardRef, useRef, useEffect, useImperativeHandle } from 'react';
 import $ from 'jquery';
 import raf from 'raf';
 import { observer } from 'mobx-react';
-import { Editable } from 'Component';
-import { I, J, U } from 'Lib';
+import { Editable, MenuItemVertical, Icon } from 'Component';
+import { I, J, U, S, keyboard, Action, Relation, analytics } from 'Lib';
 
 const MenuDataviewText = observer(forwardRef<I.MenuRef, I.Menu>((props, ref) => {
 	
-	const { param, getId, position } = props;
+	const { param, getId, position, setActive, onKeyDown, setHover, close } = props;
 	const { data } = param;
-	const { value, placeholder, canEdit, noResize, cellId, onChange } = data;
-	const nodeRef = useRef(null);
+	const { value, placeholder, canEdit, noResize, cellId, onChange, relationKey, actions = [] } = data;
+	const relation = S.Record.getRelationByKey(relationKey);
+	const { relationFormat } = relation;
+	const isSingleLine = [ I.RelationType.Url, I.RelationType.Email, I.RelationType.Phone ].includes(relationFormat);
+	const inputWrapper = useRef(null);
+	const inputRef = useRef(null);
+	const n = useRef(-1);
 	const length = value.length;
 	const valueRef = useRef(value);
 
 	useEffect(() => {
-		if (nodeRef.current) {
-			nodeRef.current.setValue(U.String.htmlSpecialChars(value));
-			nodeRef.current.setRange({ from: length, to: length });
-			nodeRef.current.placeholderCheck();
-		};
-
-		resize();
+		rebind();
 
 		return () => {
 			save();
+			unbind();
 		};
 	});
 
+	const rebind = () => {
+		unbind();
+		if (inputRef.current) {
+			inputRef.current.setValue(U.String.htmlSpecialChars(value));
+			inputRef.current.setRange({ from: length, to: length });
+			inputRef.current.placeholderCheck();
+			inputRef.current.setFocus();
+		};
+		resize();
+
+		window.setTimeout(() => {
+			setActive();
+			$(window).on('keydown.menu', e => onKeyDownHandler(e));
+		}, 15);
+	};
+
+	const unbind = () => {
+		$(window).off('keydown.menu');
+	};
+
 	const getValue = () => {
-		return String(nodeRef.current?.getTextValue() || '').trim();
+		return String(inputRef.current?.getTextValue() || '').trim();
+	};
+
+	const onKeyDownHandler = (e: any) => {
+		let ret = false;
+
+		if (inputRef.current?.isFocused()) {
+			keyboard.shortcut('arrowdown', e, () => {
+				inputRef.current?.setBlur();
+			});
+
+			if (isSingleLine) {
+				keyboard.shortcut(`enter`, e, () => {
+					e.preventDefault();
+					ret = true;
+					save();
+				});
+			};
+		} else
+		if (!n.current) {
+			keyboard.shortcut('arrowup', e, () => {
+				inputRef.current?.setFocus();
+				n.current = -1;
+			});
+			keyboard.shortcut(`enter`, e, () => {
+				e.preventDefault();
+				ret = true;
+				onClick(e, actions[n.current]);
+			});
+		};
+
+		if (ret) {
+			return;
+		};
+
+		onKeyDown(e);
 	};
 
 	const onInput = () => {
@@ -61,15 +116,102 @@ const MenuDataviewText = observer(forwardRef<I.MenuRef, I.Menu>((props, ref) => 
 		});
 	};
 
+	const onClick = (e: any, action: any) => {
+		switch (action.id) {
+			case 'openInBrowser':
+			case 'sendEmail':
+			case 'call': {
+				Action.openUrl(Relation.checkUrlScheme(relation.format, value));
+				analytics.event('RelationUrlOpen');
+				break;
+			};
+
+			case 'copy': {
+				U.Common.clipboardCopy({ text: value, html: value });
+				analytics.event('RelationUrlCopy');
+				break;
+			};
+		};
+
+		close();
+	};
+
+	const onMouseEnter = (e: any, item: any) => {
+		if (!keyboard.isMouseDisabled) {
+			setActive(item, false);
+		};
+	};
+
+	const onMouseLeave = (e: any, item: any) => {
+		if (!keyboard.isMouseDisabled) {
+			setHover(null, false);
+		};
+		if (inputRef.current?.isFocused()) {
+			n.current = -1;
+		};
+	};
+
+	const onFocus = () => {
+		setHover(null, false);
+		$(inputWrapper.current).addClass('focused');
+	};
+
+	const onBlur = () => {
+		$(inputWrapper.current).removeClass('focused');
+	};
+
+	const onClear = () => {
+		inputRef.current.setValue('');
+		inputRef.current.placeholderCheck();
+		inputRef.current.setFocus();
+	};
+
+	let menuItems: any = null;
+	if (actions.length) {
+		menuItems = (
+			<div className="items">
+				{actions.map((action: any, i: number) => (
+					<MenuItemVertical
+						key={i}
+						{...action}
+						icon={action.icon}
+						onClick={e => onClick(e, action)}
+						onMouseEnter={e => onMouseEnter(e, action)}
+						onMouseLeave={e => onMouseLeave(e, action)}
+					/>
+				))}
+			</div>
+		);
+	};
+
+	useImperativeHandle(ref, () => ({
+		getItems: () => actions,
+		getIndex: () => n.current,
+		setIndex: (i: number) => n.current = i,
+	}), []);
+
 	return (
-		<Editable
-			ref={nodeRef}
-			id="input"
-			placeholder={placeholder}
-			readonly={!canEdit}
-			onInput={onInput}
-			onPaste={onInput}
-		/>
+		<div className="innerWrapper">
+			<div
+				ref={inputWrapper}
+				className="inputWrapper"
+				onMouseEnter={() => setHover(null, false)}
+			>
+				<Editable
+					ref={inputRef}
+					id="input"
+					placeholder={placeholder}
+					readonly={!canEdit}
+					onInput={onInput}
+					onPaste={onInput}
+					onFocus={onFocus}
+					onBlur={onBlur}
+				/>
+
+				<Icon className="clear" onClick={onClear} />
+			</div>
+			{menuItems}
+		</div>
 	);
 
 }));

src/img/icon/menu/action/email0.svg
@@ -1,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M2.59375 6.56989L3.40748 5.43066L10.0006 10.14L16.5938 5.43066L17.4075 6.56989L10.0006 11.8605L2.59375 6.56989Z" fill="#9B9B9B"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M15 5H5C4.17157 5 3.5 5.67157 3.5 6.5V13.5C3.5 14.3284 4.17157 15 5 15H15C15.8284 15 16.5 14.3284 16.5 13.5V6.5C16.5 5.67157 15.8284 5 15 5ZM5 3.5C3.34315 3.5 2 4.84315 2 6.5V13.5C2 15.1569 3.34315 16.5 5 16.5H15C16.6569 16.5 18 15.1569 18 13.5V6.5C18 4.84315 16.6569 3.5 15 3.5H5Z" fill="#9B9B9B"/>
+</svg>

src/img/icon/menu/action/phone0.svg
@@ -1,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path d="M8.19082 6.93454C8.47399 6.65137 8.49141 6.19792 8.23079 5.89386L5.97973 3.26763C5.6892 2.92868 5.1716 2.9088 4.85593 3.22447L3.27345 4.80695L6.79593 8.32943L8.19082 6.93454Z" fill="#9B9B9B"/>
+    <path d="M13.0681 11.8118C13.3513 11.5287 13.8047 11.5112 14.1088 11.7719L16.735 14.0229C17.0739 14.3134 17.0938 14.831 16.7781 15.1467L15.2795 16.6451L11.6732 13.2067L13.0681 11.8118Z" fill="#9B9B9B"/>
+    <path d="M3.00249 5.07791C3.00249 5.07791 2.75965 8.64001 7.06684 12.9357C11.3965 17.2769 14.9375 16.9871 14.9375 16.9871L15.2795 16.6451L11.6732 13.2067C11.6732 13.2067 10.7039 13.3212 8.6926 11.3099C6.68128 9.29861 6.79593 8.32943 6.79593 8.32943L3.27345 4.80695L3.00249 5.07791Z" fill="#9B9B9B"/>
+</svg>

src/img/icon/menu/clear.svg
@@ -1,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+    <path d="M8.88672 0.191406C9.14133 -0.0628657 9.55399 -0.0628796 9.80859 0.191406C10.0633 0.446096 10.0633 0.859568 9.80859 1.11426L5.92188 5L9.80859 8.8877C10.0633 9.14239 10.0633 9.55489 9.80859 9.80957C9.55391 10.0643 9.14141 10.0642 8.88672 9.80957L4.99902 5.92285L1.11328 9.80957C0.858592 10.0643 0.445119 10.0643 0.19043 9.80957C-0.0638566 9.55496 -0.0638427 9.14231 0.19043 8.8877L4.07715 5L0.19043 1.11426C-0.0641317 0.859558 -0.0642171 0.446053 0.19043 0.191406C0.445077 -0.0632407 0.858581 -0.0631551 1.11328 0.191406L4.99902 4.07812L8.88672 0.191406Z" fill="#9B9B9B"/>
+</svg>

src/scss/menu/dataview/text.scss
@@ -31,7 +31,19 @@
 	}
 
 	.menu.menuDataviewText.fromBlockFeatured {
-		.content { padding: 8px 14px; }
+		.inputWrapper { margin: 0px 8px; padding: 3px 8px; border-radius: 6px; position: relative; }
+		.inputWrapper {
+			.icon.clear { display: none; width: 20px; height: 20px; background: url('~img/icon/menu/clear.svg') 50% 50% / 10px 10px no-repeat; position: absolute; right: 8px; top: 4px; }
+		}
+		.inputWrapper:not(.focused) { cursor: default; padding-right: 28px; }
+		.inputWrapper:not(.focused):hover { background: var(--color-shape-highlight-medium); }
+		.inputWrapper:not(.focused):hover {
+			.icon.clear { display: block; }
+		}
+
+		.items {
+			&:before { content: ''; display: block; height: 1px; background: var(--color-shape-secondary); margin: 8px 16px;  }
+		}
 	}
 
 	.menu.menuDataviewText.fromSidebar {

src/scss/menu/icon.scss
@@ -86,6 +86,8 @@
 		.icon.reset { background-image: url('~img/icon/menu/action/reset.svg'); }
 		.icon.hide { background-image: url('~img/icon/menu/relation/hide.svg'); }
 		.icon.browse { background-image: url('~img/icon/menu/action/browse0.svg'); }
+		.icon.email { background-image: url('~img/icon/menu/action/email0.svg'); }
+		.icon.phone { background-image: url('~img/icon/menu/action/phone0.svg'); }
 		.icon.unlink { background-image: url('~img/icon/menu/action/unlink0.svg'); }
 		.icon.store { background-image: url('~img/icon/menu/action/store0.svg'); }
 		.icon.pencil { background-image: url('~img/icon/menu/action/pencil0.svg'); }

src/ts/component/cell/index.tsx
@@ -315,7 +315,10 @@ const Cell = observer(forwardRef<I.CellRef, Props>((props, ref) => {
 			case I.RelationType.Phone: {
 				if (noInplace) {
 					param = Object.assign(param, { width: J.Size.menu.value });
-					param.data = Object.assign(param.data, { noResize: true });
+					param.data = Object.assign(param.data, {
+						noResize: true,
+						actions: value ? U.Menu.relationTextActions(relation.format) : [],
+					});
 					menuId = 'dataviewText';
 					closeIfOpen = false;
 

commit 2f557ed65cc06996d971ed68e778d04dae791ff6

fix: keep arrow keys native in long-text property menu
The dataview text menu's window keydown handler intercepted arrowdown
to blur the editable and forwarded all arrow keys to the parent menu's
onKeyDown, which preventDefaults arrowup/down for menu navigation. For
long-text properties (no actions list) this blocked native caret
movement inside the multi-line editable: arrowup did nothing, arrowdown
made the cursor disappear and stopped further input. Gate the blur and
parent onKeyDown propagation on actions.length > 0 so multi-line text
property editing works again. Reported via community forum, regression
since 7cb447c (JS-8216).

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>

src/ts/component/menu/dataview/text.tsx
@@ -56,11 +56,14 @@ const MenuDataviewText = forwardRef<I.MenuRef, I.Menu>((props, ref) => {
 
 	const onKeyDownHandler = (e: any) => {
 		let ret = false;
+		const hasActions = actions.length > 0;
 
 		if (inputRef.current?.isFocused()) {
-			keyboard.shortcut('arrowdown', e, () => {
-				inputRef.current?.setBlur();
-			});
+			if (hasActions) {
+				keyboard.shortcut('arrowdown', e, () => {
+					inputRef.current?.setBlur();
+				});
+			};
 
 			if (isSingleLine) {
 				keyboard.shortcut(`enter`, e, () => {
@@ -82,7 +85,7 @@ const MenuDataviewText = forwardRef<I.MenuRef, I.Menu>((props, ref) => {
 			});
 		};
 
-		if (!ret) {
+		if (!ret && hasActions) {
 			onKeyDown(e);
 		};
 	};