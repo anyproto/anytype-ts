5841ff452c5589ee8af1aa7114d2598710578978
Anatolii Smolianinov <zarkonesmall@gmail.com>
2026-05-20 17:20:08 +0200

    bump flake

--- flake.nix
+++ flake.nix
@@ -31,7 +31,6 @@
         nodejs_22
         bun
         # anytype-heart
-        grpc-tools
         protobuf
 
         # keytar build fails on npm install because python312 has distutils removed
